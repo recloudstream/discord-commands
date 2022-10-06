8a1a628b40bcd1fbe7519d508e58bbde33e0bc68
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-06 23:48:44 +0300

    Update info.js

--- embeds/info.js
+++ embeds/info.js
@@ -1,1 +1,8 @@
-
+const getFiles = require("../functions/getFiles.js")
+module.exports = (async()=> {
+    return {
+        "title": "Extensions - Embeds",
+        "description": (await getFiles("embeds")).map(json=> json.name).join("\n"),
+        "color": 3407616
+    }
+})()
