c15b53c4bed8a748adbd04423777958dbd0ec443
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-25 19:18:56 +0300

    Create multi.js

---
+++ embeds/multi.js
@@ -0,0 +1,23 @@
+module.exports = (async()=> {
+    return {
+      "description": "Links",
+      "color": null,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1031508285393748008/1031569491177521152/1_links.png?width=1024&height=256"
+      }
+    },
+    {
+      "description": "Repos",
+      "color": null,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1031508285393748008/1031569610975232010/2_repos.png?width=1024&height=256"
+      }
+    },
+    {
+      "description": "Guides",
+      "color": null,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1031508285393748008/1031569691174502400/3_md.png?width=1024&height=256"
+      }
+    }
+})()
