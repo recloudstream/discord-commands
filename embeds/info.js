634299de45b2b44338250a9971ee8da25d25f158
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-26 18:02:33 +0300

    ..

--- embeds/info.js
+++ embeds/info.js
@@ -1,8 +1,12 @@
 const getFiles = require("../functions/getFiles.js")
 module.exports = (async()=> {
     return [{
-        "title": "Embeds Extensions",
+        "title": "Github Embeds",
         "description": (await getFiles("embeds")).map(json=> json.name).join("\n"),
+        "color": 3407616
+    },{
+        "title": "Server Embeds",
+        "description": (await getFiles("embeds", false)).map(json=> json.name).join("\n"),
         "color": 3407616
     }]
 })()
