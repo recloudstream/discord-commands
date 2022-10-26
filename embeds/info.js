895adcc37bad189d07a23185af95ee7e0d60e2f7
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-26 21:54:25 +0300

    a

--- embeds/info.js
+++ embeds/info.js
@@ -1,12 +1,18 @@
 const getFiles = require("../functions/getFiles.js")
-module.exports = new function main() {
-    this.embeds = [{
-        "title": "Github Embeds",
-        "description": (await getFiles("embeds")).map(json=> json.name).join("\n"),
-        "color": 3407616
-    },{
-        "title": "Server Embeds",
-        "description": (await getFiles("embeds", false)).map(json=> json.name).join("\n"),
-        "color": 3407616
-    }]
+class AsyncConstructor {
+    constructor(value) {
+    return (async () => {
+        this.embeds = [{
+            "title": "Github Embeds",
+            "description": (await getFiles("embeds")).map(json=> json.name).join("\n"),
+            "color": 3407616
+        },{
+            "title": "Server Embeds",
+            "description": (await getFiles("embeds", false)).map(json=> json.name).join("\n"),
+            "color": 3407616
+        }]
+        return this;
+        })();
+    }
 }
+module.exports = new AsyncConstructor()
--- embeds/repos.js
+++ embeds/repos.js
@@ -1,4 +1,18 @@
 module.exports = new function main() {
+  this.components = [
+    {
+        "type": 1,
+        "components": [
+            {
+                "type": 2,
+                "label": "Click me!",
+                "url": "https://discord.com/developers/docs/interactions/message-components#action-rows",
+                "style": 5,
+                "emoji": "😀"
+            }
+        ]
+    }
+  ]
   this.embeds = [
     {
       "description": "For direct install, click the link\n english: https://cutt.ly/diren\n multi: https://cutt.ly/dirmulti\n arabic: https://cutt.ly/dirar\n hexated: https://cutt.ly/dirhex\n darkdemon: https://cutt.ly/dirdem\n LikDev:  https://cutt.ly/dirlik\n nsfw: https://cutt.ly/dir18\n\n For manual install, copy/type the link in the repo box (cs3 > Settings > Extensions > Add repository)\n english: https://cutt.ly/engrepo\n multi: https://cutt.ly/multirepo\n arabic: https://cutt.ly/arabrepo\n hexated: https://cutt.ly/hexarepo\n darkdemon: https://cutt.ly/drepo\n LikDev:  https://cutt.ly/likrepo\n nsfw: https://cutt.ly/horni\n\n Put the repo name in the name section. Also don't skip https://.\n\n Recommended EN sources: https://rentry.org/enrec",
@@ -7,5 +21,5 @@ module.exports = new function main() {
         "url": "https://media.discordapp.net/attachments/1031508285393748008/1031569610975232010/2_repos.png?width=1024&height=256"
       }
     }
-]
+  ]
 }
