48d2396833e56eeb487427d78fe7e965796a2873
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-29 21:12:04 +0200

    Create setup.js

---
+++ embeds/setup.js
@@ -0,0 +1,15 @@
+module.exports = new function main() {
+  this.files = ["https://cutt.ly/cs3start"]
+  the this.embeds = [{
+      "description": "
+**Cloudstream setup process: **
+
+**Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease.apk).
+**Step 2:** Install the [repositories](https://cloudstream.cf/repos).
+
+If you need more guides, go to #links.
+
+[Video guide](https://cutt.ly/cs3start) of the setup process ⬇️
+",
+    }]
+}
