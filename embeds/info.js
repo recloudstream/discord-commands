a7721b0a6aac36d3d552cb1941fddcd4364a0002
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-29 16:48:45 +0200

    Update info.js

--- embeds/info.js
+++ embeds/info.js
@@ -7,6 +7,20 @@ class AsyncConstructor {
             "description": (await getFiles("embeds")).map(json=> json.name).join("\n"),
             "color": 3407616
         }]
+        this.components = [
+            {
+                "type": 1,
+                "components": [
+                    {
+                        "type": 2,
+                        "label": "Edit on Github.",
+                        "url": "https://github.com/ImZaw/discord-extensions/tree/master/embeds",
+                        "style": 5,
+                        "emoji": "✍️"
+                    }
+                ]
+            }
+          ]
         return this;
         })();
     }
