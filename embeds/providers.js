5cf889291a0869bdd6a5992119f88e677f875e9c
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-29 19:48:42 +0200

    Add files via upload

---
+++ embeds/providers.js
@@ -0,0 +1,35 @@
+const axios = require("axios")
+class AsyncConstructor {
+    constructor(value) {
+    return (async () => {
+        const chunk = (arr, size) => Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+		arr.slice(i * size, i * size + size)
+		);
+		var allList = []
+		var repo_db = (await axios.get("https://raw.githubusercontent.com/recloudstream/cs-repos/master/repos-db.json")).data
+		for (const repo in repo_db) {
+		  var repoUrl = repo_db[repo].url ?? repo_db[repo]
+		  var pluginsResponseUrls = (await axios.get(repoUrl)).data
+		  for (const pluginUrl in pluginsResponseUrls.pluginLists) {
+			  (await axios.get(pluginsResponseUrls.pluginLists[pluginUrl])).data
+			  .forEach(data=> 
+			  {
+				allList.push(data)  
+			  })
+		  }
+		}
+		var allEmbeds = []
+		chunk(allList, 30).forEach(data=> {
+			console.log(data.map(it=> `${it.internalName.replace("Provider", "")}`).join(", ").length)
+			allEmbeds.push({
+			"description": data.map(it=> `${it.internalName.replace("Provider", "")}`).join(", "),
+			"color": null
+		})
+		})
+        this.embeds = allEmbeds
+		this.onlyiDs = ["326466810150912000"]
+        return this;
+        })();
+    }
+}
+module.exports = new AsyncConstructor()
