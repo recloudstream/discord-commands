f1a1df00d6eff6ad86e5de875f71c7b03c302ecf
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 16:14:45 +0600

    Create details.js

---
+++ embeds/details.js
@@ -0,0 +1,8 @@
+module.exports = new function main()
+
+{
+  this.files = ["https://cdn.discordapp.com/attachments/1013281939026890793/1036220525556551760/details.gif"]
+  this.embeds = [{
+      "description": `If the <#737725084881387652> channel can't solve your issue, post this in <#1019975050104950865> channel.`,
+    }]
+}
