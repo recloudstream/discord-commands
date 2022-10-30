fae70272f4e0dc5db20d82983b797ca2a9f7f600
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-31 02:17:49 +0600

    Update ursite.js

--- embeds/ursite.js
+++ embeds/ursite.js
@@ -1,4 +1,4 @@
 module.exports = new function main()
 {
-  this.files = ["https://cdn.discordapp.com/attachments/737724143570780260/1036372278344175727/Lv_0_20220901203257-1.mp4"]
+  this.files = ["https://cdn.discordapp.com/attachments/1036144578690031726/1036373272805245008/8mb.video-dGR-0rwNza0W.mp4"]
 }
