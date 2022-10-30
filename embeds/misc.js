df33db096797a8cded022af886e739d06a84c676
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 14:07:45 +0600

    Update misc.js

--- embeds/misc.js
+++ embeds/misc.js
@@ -3,10 +3,10 @@
       "title": "Miscellaneous softwares for cloudstream 3",
       "description": `
       
-      External media player: [**MPV**](https://play.google.com/store/apps/details?id=is.xyz.mpv&hl=en&gl=US) \n
-External download manager: [**Navi**](https://play.google.com/store/apps/details?id=com.tachibana.downloader) \n
-External casting: [**Web Video Cast**](https://play.google.com/store/apps/details?id=com.instantbits.cast.webvideo) \n
-Emulator for linux: [**Waydroid**](https://waydro.id/) \n
+      :small_blue_diamond: External media player: [**MPV**](https://play.google.com/store/apps/details?id=is.xyz.mpv&hl=en&gl=US) \n
+      :small_blue_diamond: External download manager: [**Navi**](https://play.google.com/store/apps/details?id=com.tachibana.downloader) \n
+      :small_blue_diamond: External casting: [**Web Video Cast**](https://play.google.com/store/apps/details?id=com.instantbits.cast.webvideo) \n
+      :small_blue_diamond: Emulator for linux: [**Waydroid**](https://waydro.id/) \n
       
       `,
     }]
