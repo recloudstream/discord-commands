960101c8449a98840598e64345ab802d0b0d8598
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 13:29:26 +0600

    Update enrec.js

--- embeds/enrec.js
+++ embeds/enrec.js
@@ -1,8 +1,8 @@
 module.exports = new function main()
 {
-  this.content = "https://rentry.org/enrec"
   this.embeds = [{
       "title": "Recommended English Sources",
       "description": `BODY`,
     }]
+  this.content = "https://rentry.org/enrec"
 }
