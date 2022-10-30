473ae2209a09b398745c6dce243495d5d79089b9
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 11:08:23 +0600

    Update setup.js

--- embeds/setup.js
+++ embeds/setup.js
@@ -3,9 +3,8 @@ module.exports = new function main()
 {
   this.files = ["https://cdn.discordapp.com/attachments/1008207706143871067/1030755127189065748/cs3start.mp4"]
   this.embeds = [{
+      "title": "Cloudstream setup process: ",
       "description": `
-**Cloudstream setup process: **
-
 **Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease.apk).
 **Step 2:** Install the [repositories](https://cloudstream.cf/repos).
 
