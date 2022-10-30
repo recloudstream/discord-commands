ba72553bbf414709d4df95167c7c0a6fed70a2b0
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-30 22:13:49 +0200

    Update ursite.js

--- embeds/ursite.js
+++ embeds/ursite.js
@@ -1,4 +1,4 @@
 module.exports = new function main()
 {
-  this.content = "https://cdn.discordapp.com/attachments/835622152270118953/1014914075957919754/lv_0_20220901203257.mp4"
+  this.files = ["https://cdn.discordapp.com/attachments/737724143570780260/1036372278344175727/Lv_0_20220901203257-1.mp4"]
 }
