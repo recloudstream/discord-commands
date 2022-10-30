4a3713bf6d9dfd53d7d60cd4fce5e3ec22460c18
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 15:53:44 +0600

    Update enrec.js

--- embeds/enrec.js
+++ embeds/enrec.js
@@ -1,4 +1,42 @@
 module.exports = new function main()
+
 {
-  this.content = "https://rentry.org/enrec"
+  this.embeds = [{
+      "title": "Reccomended English Sources",
+      "description": `
+      
+      
+**Movies & Series** \n
+1. Super stream [English](https://cutt.ly/diren) \n
+2. Sora stream [Hexated](https://cutt.ly/dirhex) \n
+3. Loklok [Hexated](https://cutt.ly/dirhex) \n
+4. Ask4movies [English](https://cutt.ly/diren) \n
+5. Olgply [English](https://cutt.ly/diren) \n
+ \n \n
+**Anime** \n
+1. Animixplay [Hexated](https://cutt.ly/dirhex) \n
+2. Crunchyroll **(disabled)** [English](https://cutt.ly/diren) \n
+3. Zoro [English](https://cutt.ly/diren) \n
+4. Tenshi [English](https://cutt.ly/diren) \n
+5. 9anime **(disabled)** [English](https://cutt.ly/diren) \n
+6. Allanime **(disabled)** [English](https://cutt.ly/diren) \n
+7. Gogo anime [English](https://cutt.ly/diren) \n
+ \n \n
+**Asian** \n
+1. Loklok [Hexated](https://cutt.ly/dirhex) \n
+2. Kisskh [Hexated](https://cutt.ly/dirhex) \n
+3. Kissasian [Hexated](https://cutt.ly/dirhex) \n
+ \n \n
+**TV & Sports** \n
+1. IPTV-org **(TV)**[Multi](https://cutt.ly/dirmulti) \n
+2. Time4TV **(Sports)**[Hexated](https://cutt.ly/dirhex) \n
+3. TV247 [Hexated](https://cutt.ly/dirhex) \n
+4. 123TV [Hexated](https://cutt.ly/dirhex) \n
+ \n \n
+💡For more details, click [here](https://rentry.org/enrec).
+
+      `,
+    }]
 }
+
+
