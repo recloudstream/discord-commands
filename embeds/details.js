bff67c17356d1aac550a42ecf47c77c47de5ca1f
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 20:15:58 +0600

    Update details.js

--- embeds/details.js
+++ embeds/details.js
@@ -1,8 +1,22 @@
 module.exports = new function main()
 
 {
-  this.files = ["https://cdn.discordapp.com/attachments/1013281939026890793/1036220525556551760/details.gif"]
   this.embeds = [{
-      "description": `If the <#737725084881387652> channel can't solve your issue, post this in <#1019975050104950865> channel.`,
+      "description": "1. Don't put your issue in <#737724143570780260>\n2. Don't spam your issue. If somebody knows any solution, they will help you. Otherwise, wait for the devs' response.\n3. Post your issue at <#737725084881387652>",
+      "color": 10617089
+    },
+    {
+      "title": "Before posting your issue",
+      "description": "1. Are you using the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease-release.apk)?\n2. Have you read <#922482568979431475> and <#737729080492818543>?\n3. Have you read the [Fix](https://rentry.org/cs3fix) page?",
+      "color": 12430848
+    },
+    {
+      "title": "Post your issue with these details",
+      "description": "1. The app version (example: 3.1.56 · 65b5efb)\n2. Source(s) names\n3. Full description of your issue.\n4. Screen shots or screen recordings or photos of the issue.\n5. Post the logcat (cs3 > settings > backup & updates > show logcat) (Optional)",
+      "color": 1687
+    },
+    {
+      "description": "If you dont get any solution, put this in <#1019975050104950865>",
+      "color": null
     }]
 }
