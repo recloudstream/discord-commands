9421ef15812fe1888ec051b9f0ef2fcb903aaf94
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-30 12:37:40 +0200

    Create pre.js

---
+++ embeds/pre.js
@@ -0,0 +1,12 @@
+module.exports = new function main()
+{
+  var downloadLink = "https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease.apk"
+  this.embeds = [
+    {
+      "description": `Download the [pre release](${downloadLink}) to fix all your issues`,
+      "color": 11341326,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/737724143570780260/993578886044590210/sticker.png?width=472&height=472"
+      }
+    }
+  ]
