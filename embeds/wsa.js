e7a5bb1de598da9f63772a0d61bd5206b763bce6
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 13:12:22 +0600

    Update wsa.js

--- embeds/wsa.js
+++ embeds/wsa.js
@@ -1,5 +1,5 @@
 module.exports = new function main()
 {
-  this.files = ["https://cutt.ly/cs3onwin11"]
+  this.files = ["https://cdn.discordapp.com/attachments/737724143570780260/1031100838212538388/wsa.mp4"]
   this.content = "https://rentry.org/wsa"
 }
