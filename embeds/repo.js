29375f84e614fd03b1a6ffaafa9a6e08f3ce2125
Duckling <57977673+sfaresee@users.noreply.github.com>
2022-10-30 08:49:51 +0600

    Create repo.js

---
+++ embeds/repo.js
@@ -0,0 +1,23 @@
+const axios = require("axios")
+class AsyncConstructor {
+    constructor(value) {
+    return (async () => {
+        var repos = (await axios.get("https://raw.githubusercontent.com/recloudstream/cs-repos/master/repos-db.json")).data
+        .map(json=> {
+            if(json.verified) return "  <:verified:1027693463573114903> " + json.url.split("/")[4] + " :\n[Install](" + json.url + ")"
+            else return json.split("/")[4] + ":\n[Install](" + json + ")"
+        })
+        this.embeds = [{
+            "title": "Cloudstream Repositories",
+            "description": `
+			
+			🧩 Repositories:\n 
+      ${repos.join("\n\n")} \n\n`,
+			
+            "color": 1381909
+        }]
+        return this;
+        })();
+    }
+}
+module.exports = new AsyncConstructor()
