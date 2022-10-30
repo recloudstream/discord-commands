dc0996467a9d1b7c860301069cb17f4a64313f89
Duckling <57977673+sfaresee@users.noreply.github.com>
2022-10-30 08:46:43 +0600

    Update setup.js

--- embeds/setup.js
+++ embeds/setup.js
@@ -1,5 +1,6 @@
-module.exports = new function main() {
-  this.content = "https://cdn.discordapp.com/attachments/1008207706143871067/1030755127189065748/cs3start.mp4"
+module.exports = new function main()
+
+{
   this.files = ["https://cdn.discordapp.com/attachments/1008207706143871067/1030755127189065748/cs3start.mp4"]
   this.embeds = [{
       "description": `
@@ -8,8 +9,6 @@ module.exports = new function main() {
 **Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease.apk).
 **Step 2:** Install the [repositories](https://cloudstream.cf/repos).
 
-If you need more guides, go to <#1031508285393748008>.
-
-[Video guide](https://cutt.ly/cs3start) of the setup process ⬇️ `,
+If you need more guides, go to <#1031508285393748008>. `,
     }]
 }
