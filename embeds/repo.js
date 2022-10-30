b61645d5f06045ec58a6dd6ce1011a649d32c230
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-30 12:43:58 +0200

    Update repo.js

--- embeds/repo.js
+++ embeds/repo.js
@@ -3,9 +3,10 @@ class AsyncConstructor {
     constructor(value) {
     return (async () => {
         var repos = (await axios.get("https://raw.githubusercontent.com/recloudstream/cs-repos/master/repos-db.json")).data
-        .map(json=> {
-            if(json.verified) return "  <:verified:1027693463573114903> " + json.url.split("/")[4] + " :\n[Install](" + json.url + ")"
-            else return json.split("/")[4] + ":\n[Install](" + json + ")"
+        .map(async json=> {
+	    var name = (await axios.get(json.url)).data.name
+            if(json.verified) return "  <:verified:1027693463573114903> " + name + " :\n[Install](" + json.url + ")"
+            else return name + ":\n[Install](" + json + ")"
         })
         this.embeds = [{
             "title": "Cloudstream Repositories",
