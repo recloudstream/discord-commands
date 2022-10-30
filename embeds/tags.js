84edd539af4d0aec849c8f412310542753275537
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-31 01:48:11 +0600

    Update tags.js

--- embeds/tags.js
+++ embeds/tags.js
@@ -1,8 +1,24 @@
 {
   this.embeds = [{
     
-    "description": "https://discord.com/channels/737724143126052974/1031508285393748008/1036346889467076638",
+    "title": "Links",
+      "description": ":small_blue_diamond:.**site **: for the official site.\n:small_blue_diamond:.**mirror **: for the mirror rentry side\n:small_blue_diamond:.**repo **: for the repo list\n:small_blue_diamond:.**paste **: for the paste-able links\n:small_blue_diamond:.**github **: for the github page of the app\n:small_blue_diamond:.**pre **: for the direct pre link\n:small_blue_diamond:.**enrec **: for english reccomendation",
       "color": null
-
+    },
+    {
+      "title": "Features",
+      "description": ":small_blue_diamond:.**sub **: for the subtitle faq page\n:small_blue_diamond:.**opensub **: for the opensub faq\n:small_blue_diamond:.**sync **: for the sync guide\n:small_blue_diamond:.**track **: for anime tracking guide\n:small_blue_diamond:.**stremio **: for a stremio catalogue guide\n:small_blue_diamond:.**trakt **: for getting trakt list",
+      "color": null
+    },
+    {
+      "title": "How to",
+      "description": ":small_blue_diamond:.**setup **: for the setup guide\n:small_blue_diamond:.**fix **: for the fixing guide\n:small_blue_diamond:.**tv**: for the tv installation guide\n:small_blue_diamond:.**wsa **: for the installation guide on wsa\n:small_blue_diamond:.**nsfw **: for the nsfw extension guide",
+      "color": null
+    },
+    {
+      "title": "Others",
+      "description": ":small_blue_diamond:.**sun **: for sunstream link\n:small_blue_diamond:.**aqua **: for aquastream link\n:small_blue_diamond:.**scrape **: for the scraping guide\n:small_blue_diamond:.**none** : for the **none** button location\n:small_blue_diamond:.**v **: for the app version location\n:small_blue_diamond:.**misc **: miscellaneous apps related to cs3\n:small_blue_diamond:.**vpn **: for the vpn guide\n:small_blue_diamond:.**egy **: for the egy meme\n:small_blue_diamond:.**details **: to ask about the details of the issue",
+      "color": null
+      
     }]
 }
