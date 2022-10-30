ab6dbffa97bc38a3b4221b28dcb7bd74dae1910e
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 13:21:28 +0600

    Update stremio.js

--- embeds/stremio.js
+++ embeds/stremio.js
@@ -1,5 +1,5 @@
 module.exports = new function main()
 {
-  this.files = ["https://cdn-cf-east.streamable.com/video/mp4/9avubx.mp4?Expires=1667373300&Signature=LSnjp-mM9jarJhRnNl3hPQTqmVSTbKGk3Z9daxDWScMPHkts60KucO~f28n-CQn76GHZzUPArha5p5jymGQr8bfL5o1JO6y6gqNk9nmT-RpFdRiQQ~NhwCWMc24ljmIHWWtphhkaiJp-PFaW73Qr8NuZWRyuQbotYXjohPpALvc6SmGxag1aLxAtkh1eBqinTkThN~dVZf5Fe-Vpik9Mh38outW8cO3Ky4eYI-qEWWKYqmjrVP2oPbcHF7lY2pQQveLyh4z2Q6PD5L3H74CliqZ8h9bO3qG-oEFe06KSm0U2OX0NS7t59Ow7eSFxLt1yDhO1VA2FNNuS9xC3LvgONg__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ"]
+  //this.files = ["https://cdn-cf-east.streamable.com/video/mp4/9avubx.mp4?Expires=1667373300&Signature=LSnjp-mM9jarJhRnNl3hPQTqmVSTbKGk3Z9daxDWScMPHkts60KucO~f28n-CQn76GHZzUPArha5p5jymGQr8bfL5o1JO6y6gqNk9nmT-RpFdRiQQ~NhwCWMc24ljmIHWWtphhkaiJp-PFaW73Qr8NuZWRyuQbotYXjohPpALvc6SmGxag1aLxAtkh1eBqinTkThN~dVZf5Fe-Vpik9Mh38outW8cO3Ky4eYI-qEWWKYqmjrVP2oPbcHF7lY2pQQveLyh4z2Q6PD5L3H74CliqZ8h9bO3qG-oEFe06KSm0U2OX0NS7t59Ow7eSFxLt1yDhO1VA2FNNuS9xC3LvgONg__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ"]
   this.content = "https://rentry.org/stremio"
 }
