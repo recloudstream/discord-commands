a2f9e6d98b4aec58c4c2f0a7933e7f30d31c8b1f
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 16:30:05 +0600

    Update enrec.js

--- embeds/enrec.js
+++ embeds/enrec.js
@@ -6,31 +6,32 @@ module.exports = new function main()
       "description": `
            
 **Movies & Series**
-1. Super stream [English](cloudstreamrepo://raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)
-2. Sora stream [Hexated](cloudstreamrepo://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)
-3. Loklok [Hexated](cloudstreamrepo://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)
-4. Ask4movies [English](cloudstreamrepo://raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)
-5. Olgply [English](cloudstreamrepo://raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)
+1. Super stream [English](https://cutt.ly/diren)
+2. Sora stream [Hexated](https://cutt.ly/dirhex)
+3. Loklok [Hexated](https://cutt.ly/dirhex)
+4. Ask4movies [English](https://cutt.ly/diren)
+5. Olgply [English](https://cutt.ly/diren)
 
 **Anime**
-1. Animixplay [Hexated](cloudstreamrepo://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)
-2. Crunchyroll **(disabled)** [English](cloudstreamrepo://raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)
-3. Zoro [English](cloudstreamrepo://raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)
-4. Tenshi [English](cloudstreamrepo://raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)
-5. 9anime **(disabled)** [English](cloudstreamrepo://raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)
-6. Allanime **(disabled)** [English](cloudstreamrepo://raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)
-7. Gogo anime [English](cloudstreamrepo://raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)
+1. Animixplay [Hexated](https://cutt.ly/dirhex)
+2. Crunchyroll **(disabled)** [English](https://cutt.ly/diren)
+3. Zoro [English](https://cutt.ly/diren)
+4. Tenshi [English](https://cutt.ly/diren)
+5. 9anime **(disabled)** [English](https://cutt.ly/diren)
+6. Allanime **(disabled)** [English](https://cutt.ly/diren)
+7. Gogo anime [English](https://cutt.ly/diren)
  
 **Asian**
-1. Loklok [Hexated](cloudstreamrepo://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)
-2. Kisskh [Hexated](cloudstreamrepo://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)
-3. Kissasian [Hexated](cloudstreamrepo://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)
+1. Loklok [Hexated](https://cutt.ly/dirhex)
+2. Kisskh [Hexated](https://cutt.ly/dirhex)
+3. Kissasian [Hexated](https://cutt.ly/dirhex)
  
 **TV & Sports**
-1. IPTV-org **(TV)**[Multi](cloudstreamrepo://raw.githubusercontent.com/recloudstream/cloudstream-extensions-multilingual/builds/repo.json)
-2. Time4TV **(Sports)**[Hexated](cloudstreamrepo://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)
-3. TV247 [Hexated](cloudstreamrepo://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)
-4. 123TV [Hexated](cloudstreamrepo://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)
+1. IPTV-org **(TV)**[Multi](https://cutt.ly/dirmulti)
+2. Time4TV **(Sports)**[Hexated](https://cutt.ly/dirhex)
+3. TV247 [Hexated](https://cutt.ly/dirhex)
+4. 123TV [Hexated](https://cutt.ly/dirhex)
+
 
 
 💡For more details, click [here](https://rentry.org/enrec).
