0b061a5f0caf9c3aabe104d5b8779c91629b24c6
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 16:44:53 +0600

    Update none.js

--- embeds/none.js
+++ embeds/none.js
@@ -1,4 +1,6 @@
 module.exports = new function main()
 {
   this.files = ["https://cdn.discordapp.com/attachments/737729263221997619/1034390187372982343/none.png"]
+    this.embeds = [{
+      "description": `Click none and select your source.`,
 }
