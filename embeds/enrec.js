8e43f851dd90af305a1a10173561dfb178f004dc
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 13:28:38 +0600

    Create enrec.js

---
+++ embeds/enrec.js
@@ -0,0 +1,8 @@
+module.exports = new function main()
+{
+  this.content = "https://rentry.org/enrec"
+  this.embeds = [{
+      "title": "Recommended English Sources",
+      "description": `BODY`,
+    }]
+}
