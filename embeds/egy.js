8ae20115c476fd6c174bea5398cf3d29d4652bb1
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 16:22:52 +0600

    Create egy.js

---
+++ embeds/egy.js
@@ -0,0 +1,10 @@
+https://cdn.discordapp.com/attachments/737724143570780260/1035879212411797565/sketch-1667043229468.png
+
+module.exports = new function main()
+
+{
+  this.files = ["https://cdn.discordapp.com/attachments/737724143570780260/1035879212411797565/sketch-1667043229468.png"]
+  this.embeds = [{
+      "description": `Egybest is always broken. Use other sources.`,
+    }]
+}
