3e3a78c1a3d323fb31d76d3b1fbc2b515e3eb773
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-30 17:48:45 +0600

    Update none.js

--- embeds/none.js
+++ embeds/none.js
@@ -9,7 +9,7 @@ module.exports = new function main()
       "image": {
         "url": "https://cdn.discordapp.com/attachments/737729263221997619/1034390187372982343/none.png"
       }
-      }
+      
 
     }]
 }
