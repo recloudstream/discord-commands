cadd8d55e1c81b914a6c08b27cc26c9b66b43965
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-10-30 13:25:20 +0200

    Update misc.js

--- embeds/misc.js
+++ embeds/misc.js
@@ -1,13 +1,8 @@
 {
-  this.embeds = [{
-      "title": "Miscellaneous softwares for cloudstream 3",
-      "description": `
-      
-      :small_blue_diamond: External media player: [**MPV**](https://play.google.com/store/apps/details?id=is.xyz.mpv&hl=en&gl=US)
-      :small_blue_diamond: External download manager: [**Navi**](https://play.google.com/store/apps/details?id=com.tachibana.downloader)
-      :small_blue_diamond: External casting: [**Web Video Cast**](https://play.google.com/store/apps/details?id=com.instantbits.cast.webvideo)
-      :small_blue_diamond: Emulator for linux: [**Waydroid**](https://waydro.id/)
-      :small_blue_diamond: Free vpn for android: [**1.1.1.1**](https://rentry.org/askvpn#cloudflare)
-      `,
-    }]
+  this.embeds = [
+    {
+      "description": ":small_blue_diamond: External media player: [**MPV**](https://play.google.com/store/apps/details?id=is.xyz.mpv&hl=en&gl=US)\n:small_blue_diamond: External download manager: [**Navi**](https://play.google.com/store/apps/details?id=com.tachibana.downloader)\n:small_blue_diamond: External casting: [**Web Video Cast**](https://play.google.com/store/apps/details?id=com.instantbits.cast.webvideo)\n:small_blue_diamond: Emulator for linux: [**Waydroid**](https://waydro.id/)\n:small_blue_diamond: Free vpn for android: [**1.1.1.1**](https://rentry.org/askvpn#cloudflare)",
+      "color": null
+    }
+  ]
 }
