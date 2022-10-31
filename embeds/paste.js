e159a3d0954936f3d924a796f6ecf7a12c410634
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-31 18:58:40 +0600

    Update paste.js

--- embeds/paste.js
+++ embeds/paste.js
@@ -5,15 +5,14 @@
       "title": "For manual install, copy/type the link in the repo box (`cs3 > Settings > Extensions > Add repository`)",
       "description": `
       
-:small_blue_diamond: english: https://cutt.ly/engrepo \n
-:small_blue_diamond: multi: https://cutt.ly/multirepo \n
-:small_blue_diamond: arabic: https://cutt.ly/arabrepo \n
-:small_blue_diamond: hexated: https://cutt.ly/hexarepo \n
-:small_blue_diamond: darkdemon: https://cutt.ly/drepo \n
-:small_blue_diamond: LikDev:  https://cutt.ly/likrepo \n
-:small_blue_diamond: nsfw: https://cutt.ly/horni \n
+:small_blue_diamond: english: **eng**
+:small_blue_diamond: multi: **multi**
+:small_blue_diamond: arabic: **arab**
+:small_blue_diamond: hexated: **hexa**
+:small_blue_diamond: darkdemon: **drepo**
+:small_blue_diamond: LikDev:  **likrepo**
+:small_blue_diamond: nsfw: **nsfw**
 
-:warning: Put the repo name in the name section. Also don't skip "https://".  \n
       
       `,
     }]
