29a07bd3078e41dc12bcd2dc1b273154fd2a2b69
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-31 19:28:37 +0600

    Update stable.js

--- embeds/stable.js
+++ embeds/stable.js
@@ -1,7 +1,6 @@
 module.exports = new function main()
 
 {
-  this.files = ["https://cdn.discordapp.com/attachments/1008207706143871067/1030755127189065748/cs3start.mp4"]
   this.embeds = [{
 "title": "But why?",
       "color": 16711680,
