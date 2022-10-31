532eca0f1173fa976bf92cd1c9013fe3b15dd69d
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-01 01:54:37 +0600

    Update store.js

--- embeds/store.js
+++ embeds/store.js
@@ -3,7 +3,7 @@ module.exports = new function main()
 {
   this.embeds = [{
 "title": "Cloudstream Beta - Miscrosoft Store Apps",
-      "description": "Cloudstream is a windows app for streaming and downloading Movies, TV-Series, Asian, Anime and IPTV. The app provides all the contents without any sign-up and subscription.\n\nFeatures include:\n- 4K movies\n- Anime tracking\n- Subtitle downloads\n- Chromecast support",
+      "description": "Cloudstream is a windows app for streaming and downloading Movies, TV-Series, Asian dramas, Anime and IPTV. The app provides all the contents without any sign-up and subscription.\n\nFeatures include:\n- 4K movies\n- Anime tracking\n- Subtitle downloads\n- Chromecast support",
       "url": "https://rentry.org/wsa",
       "image": {
         "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036639726255480853/cover.png"
