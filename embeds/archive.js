1bbd0eaaa511a6c03c2023bc73b7776b588064e3
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-10-31 22:41:49 +0100

    fix archive.js

--- embeds/archive.js
+++ embeds/archive.js
@@ -5,7 +5,7 @@ class AsyncConstructor {
 		var data = (await axios.get("https://api.github.com/repos/recloudstream/cloudstream-archive/git/trees/master"))?.data?.tree || []
 		var currentAvailableCommits = data.filter(x=>x.path.endsWith(".apk")).map(x => x.path)
 		var commits = (await axios.get("https://api.github.com/repos/recloudstream/cloudstream/commits"))?.data?.slice(0, 15) || []
-		var fields = commits.filter(x => currentAvailableCommits.includes(x.sha.slice(0, 7))).map((commit, idx) => {
+		var fields = commits.filter(x => currentAvailableCommits.includes(`${x.sha.slice(0, 7)}.apk`)).map((commit, idx) => {
 			var name = commit.sha.slice(0, 7)
 			if(idx === 0) name = name + " **(Latest)**"
 			return {
