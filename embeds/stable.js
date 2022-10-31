c5fae39a2a2eed8f72cfea440bb5314d834dd77c
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-31 19:27:06 +0600

    Create stable.js

---
+++ embeds/stable.js
@@ -0,0 +1,12 @@
+module.exports = new function main()
+
+{
+  this.files = ["https://cdn.discordapp.com/attachments/1008207706143871067/1030755127189065748/cs3start.mp4"]
+  this.embeds = [{
+"title": "But why?",
+      "color": 16711680,
+      "image": {
+        "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036631289534619680/okand.png"
+      }
+    }]
+}
