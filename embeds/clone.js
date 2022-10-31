40571c2053f0b20f200b680c29adf1c1c520404e
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-10-31 13:47:11 +0600

    Create clone.js

---
+++ embeds/clone.js
@@ -0,0 +1,32 @@
+
+      
+module.exports = new function main()
+
+{
+  this.embeds = [{
+      "title": "It's a clone site",
+      "description": "The site you have requested has one of these server combinations.",
+      "color": null
+    },
+    {
+      "title": "sflix.to",
+      "color": null,
+      "image": {
+        "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1036544539273470023/unknown.png"
+      }
+    },
+    {
+      "title": "sflix.pro",
+      "color": null,
+      "image": {
+        "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1036544644600836096/unknown.png"
+      }
+    },
+    {
+      "title": "2embde.org",
+      "color": null,
+      "image": {
+        "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1036544900126220348/unknown.png"
+      }
+    }]
+}
