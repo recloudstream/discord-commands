fca6111015fb61447aea7b82daeb558877c4a279
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-01 21:15:04 +0200

    kek

--- embeds/kek.js
+++ embeds/kek.js
@@ -1,18 +1,16 @@
 
 
-module.exports = new function main()
-
-{
+module.exports = function main() {
   this.embeds = [{
 
-      "title": "Cloudstream Repositories",
-      "description": "<:verified:1036237892604936232> **English**: [Install](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n\n<:verified:1036237892604936232> **Multilingual**: [Install](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions-multilingual/builds/repo.json)\n\n**Hexated**: [Install](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n\n**Arabic**: [Install](https://cs.repo/?raw.githubusercontent.com/ImZaw/cloudstream-extensions-arabic/builds/repo.json)\n\n**NSFW**: [Install](https://cs.repo/?raw.githubusercontent.com/Jacekun/cs3xxx-repo/main/repo.json)\n\n**Darkdemon**: [Install](https://cs.repo/?raw.githubusercontent.com/daarkdemon/cs-darkdemon-extensions/builds/repo.json)\n\n**LikDev-256**: [Install](https://cs.repo/?raw.githubusercontent.com/LikDev-256/likdev256-tamil-providers/builds/repo.json)\n\nDon't install all the plugins from a repo without knowing.",
-      "url": "https://cloudstream.cf/repos",
-      "color": 16711680,
-      "image": {
-        "url": "https://cdn.discordapp.com/attachments/1036209801803333632/1036609641636962334/unknown.png"
-      }
+    "title": "Cloudstream Repositories",
+    "description": "<:verified:1036237892604936232> **English**: [Install](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n\n<:verified:1036237892604936232> **Multilingual**: [Install](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions-multilingual/builds/repo.json)\n\n**Hexated**: [Install](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n\n**Arabic**: [Install](https://cs.repo/?raw.githubusercontent.com/ImZaw/cloudstream-extensions-arabic/builds/repo.json)\n\n**NSFW**: [Install](https://cs.repo/?raw.githubusercontent.com/Jacekun/cs3xxx-repo/main/repo.json)\n\n**Darkdemon**: [Install](https://cs.repo/?raw.githubusercontent.com/daarkdemon/cs-darkdemon-extensions/builds/repo.json)\n\n**LikDev-256**: [Install](https://cs.repo/?raw.githubusercontent.com/LikDev-256/likdev256-tamil-providers/builds/repo.json)\n\nDon't install all the plugins from a repo without knowing.",
+    "url": "https://cloudstream.cf/repos",
+    "color": 16711680,
+    "image": {
+      "url": "https://cdn.discordapp.com/attachments/1036209801803333632/1036609641636962334/unknown.png"
+    }
 
 
-    }]
+  }]
 }
