5104f69f9b9da60fcbc31d1f37e976e59b3d63d6
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-01 22:27:52 +0600

    Create logcat.js

---
+++ embeds/logcat.js
@@ -0,0 +1,14 @@
+
+module.exports = new function main()
+
+{
+  this.embeds = [{
+      "title": "Where is the logcat?",
+      "description": "To get logcat go to **cs3 > settings > updates and backup > show logcat**",
+      "color": 16711680,
+      "image": {
+        "url": "https://cdn.discordapp.com/attachments/1015131233824538624/1037037596318105690/unknown.png"
+      }
+
+    }]
+}
