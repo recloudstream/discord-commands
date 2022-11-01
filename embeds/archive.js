3bced025f5b9cabb299328a8ae91a165b7390633
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-01 13:29:14 +0200

    Update archive.js

--- embeds/archive.js
+++ embeds/archive.js
@@ -1,4 +1,8 @@
 const axios = require("axios");
+function timestamp(year, month, day, hour, minute, second){
+ var datum = new Date(Date.UTC(year, month, day, hour, minute, second))
+ return datum.getTime()/1000
+}
 class AsyncConstructor {
 	constructor(value) {
 		return (async () => {
@@ -7,9 +11,11 @@ class AsyncConstructor {
 			var commits = (await axios.get("https://api.github.com/repos/recloudstream/cloudstream/commits"))?.data?.slice(0, 15) || []
 			var fields = commits.filter(x => currentAvailableCommits.includes(`${x.sha.slice(0, 7)}.apk`)).map((commit, idx) => {
 				var name = commit.sha.slice(0, 7)
+				var time = new Date(commit.commit.author.date)
+				var timestamp = timestamp(time.getUTCFullYear(), time.getUTCMonth(), time.getUTCDate(), time.getUTCHours(), time.getUTCMinutes(), time.getUTCSeconds())
 				if (idx === 0) name = name + " **(Latest)**"
 				return {
-					"name": name,
+					"name": name + ` <t:${timestamp}:R>`,
 					"value": `[${commit.sha.slice(0, 7) + ".apk"}](https://github.com/recloudstream/cloudstream-archive/raw/master/${commit.sha.slice(0, 7)}.apk)\n${commit.commit.message}`,
 					"inline": true
 				}
