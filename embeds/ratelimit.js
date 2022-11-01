c9a274acb627d652578fd8a963d7e61d58231a36
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-01 15:49:07 +0100

    add .ratelimit

---
+++ embeds/ratelimit.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+class AsyncConstructor {
+	constructor(value) {
+		return (async () => {
+			let data = (await axios.get("https://api.github.com/rate_limit"))?.data
+            var fields = data.resources.map(it => {return { // js grammar is bad 
+                name: it.resource,
+                value: `${it.remaining}/${it.limit} (reset @ <t:${it.reset}:R>)`,
+                inline: false
+            }})
+			
+			this.embeds = [
+				{
+					"title": "Gitub API Rate limit remaining",
+					"color": 16746328,
+					"fields": fields
+				}
+			]
+			return this;
+		})();
+	}
+}
+module.exports = new AsyncConstructor()
