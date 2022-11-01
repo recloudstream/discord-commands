e70798b3da2313d61763e81266d78c0837d9695c
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-01 23:23:13 +0600

    Update stable.js

--- embeds/stable.js
+++ embeds/stable.js
@@ -3,7 +3,7 @@ module.exports = new function main() {
     "title": "But why?",
     "color": 16711680,
     "image": {
-      "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036631289534619680/okand.png"
+      "url": "https://cdn.discordapp.com/attachments/737729263221997619/1037053471846846554/download-2.jpeg"
     }
   }]
 }
