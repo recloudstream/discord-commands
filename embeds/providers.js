65a1b34fcc88a3b6e1dfcc265fd79b9c8926394b
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-01 20:39:52 +0100

    Update providers.js

--- embeds/providers.js
+++ embeds/providers.js
@@ -1,9 +1,14 @@
 const axios = require("axios")
 class AsyncConstructor {
 	constructor(args) {
-		return (async () => {
+		return (async (inputs) => {
 			var allEmbeds = []
-			let repo_db = (await axios.get("https://raw.githubusercontent.com/recloudstream/cs-repos/master/repos-db.json")).data
+			var repo_db = []
+			if (inputs?.length > 0 && inputs[0].startsWith("http")) {
+				repo_db = [inputs[0]]
+			} else {
+				repo_db = (await axios.get("https://raw.githubusercontent.com/recloudstream/cs-repos/master/repos-db.json")).data
+			}
 			for (const repo in repo_db) {
 				var repoPlugins = []
 				var RepoResponse = (await axios.get(repo_db[repo].url ?? repo_db[repo])).data
