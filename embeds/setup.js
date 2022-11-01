f25084f413cc94fb6a4c97eb64879e5050f77ec1
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-01 15:57:00 +0600

    Update setup.js

--- embeds/setup.js
+++ embeds/setup.js
@@ -4,13 +4,10 @@ module.exports = new function main()
   this.files = ["https://cdn.discordapp.com/attachments/1008207706143871067/1030755127189065748/cs3start.mp4"]
   this.embeds = [{
       "title": "Cloudstream setup process",
-      "description": "**Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease-release.apk).\n**Step 2:** Install the [repositories](https://cloudstream.cf/repos).\n\nIf you need more guides, go to <#1031508285393748008>.\n\n**Don't install all the plugins from a repo without knowing.**",
+      "description": "**Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease-release.apk).\n**Step 2:** Install the [repositories](https://cloudstream.cf/repos).\n\nIf you need more guides, go to <#1031508285393748008>.\n\n**Don't install all the plugins from a repo without knowing**",
       "color": 16711680,
       "image": {
         "url": "https://cdn.discordapp.com/attachments/1036209801803333632/1036609641636962334/unknown.png"
-      },
-      "thumbnail": {
-        "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
       }
       
     }]
