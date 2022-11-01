44f74d91535b21d8c69af352b5db08d1994168a3
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-01 22:50:53 +0300

    Update providers.js

--- embeds/providers.js
+++ embeds/providers.js
@@ -3,13 +3,16 @@ class AsyncConstructor {
 	constructor(args) {
 		this.args = args
 		return (async (inputs) => {
+			console.log(inputs)
 			var allEmbeds = []
 			var repo_db = []
 			if (inputs?.length > 0 && inputs[0].startsWith("http")) {
 				repo_db = [inputs[0]]
+				console.log(inputs[0])
 			} else {
 				repo_db = (await axios.get("https://raw.githubusercontent.com/recloudstream/cs-repos/master/repos-db.json")).data
 			}
+			console.log(repo_db)
 			for (const repo in repo_db) {
 				var repoPlugins = []
 				var RepoResponse = (await axios.get(repo_db[repo].url ?? repo_db[repo])).data
