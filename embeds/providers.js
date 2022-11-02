1457aad36ca7bed4f37b1f318b3d2e092bfb10f4
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-02 14:10:33 +0200

    added votes

--- embeds/providers.js
+++ embeds/providers.js
@@ -1,18 +1,19 @@
 const axios = require("axios")
+const { createHash } = require('crypto');
+function hash(url) {
+	return createHash('sha256').update(url + "#funny-salt").digest('hex');
+}
 class AsyncConstructor {
 	constructor(args) {
 		this.args = args
 		return (async (inputs) => {
-			console.log(inputs)
 			var allEmbeds = []
 			var repo_db = []
 			if (inputs?.length > 0 && inputs[0].startsWith("http")) {
 				repo_db = [inputs[0]]
-				console.log(inputs[0])
 			} else {
 				repo_db = (await axios.get("https://raw.githubusercontent.com/recloudstream/cs-repos/master/repos-db.json")).data
 			}
-			console.log(repo_db)
 			for (const repo of repo_db) {
 				if (!repo) continue;
 				var repoPlugins = []
@@ -24,13 +25,25 @@ class AsyncConstructor {
 							repoPlugins.push(data)
 						})
 				}
+				var pluginsList = []
+				for (const plugin in repoPlugins) {
+					try {
+						var it = repoPlugins[plugin]
+						var voteUrl = "https://api.countapi.xyz/get/cs3-votes/" + hash(it.url)
+						var voteCount = (await axios.get(voteUrl))?.data?.value;
+						if (voteCount >= 0) voteCount = voteCount + " <:upvote:1037335398759809094>"; else voteCount = voteCount + " <:downvote:1037335394787790908>"
+						var status;
+						if (it.status == 1) status = "🟢"; else if (it.status == 2) status = "🟡"; else if (it.status == 3) status = "🟠"; else status = "🔴"
+						pluginsList.push(`**${status} ${it.internalName.replace("Provider", "")} ( ${voteCount} )**`)
+					} catch (err) {
+						var status;
+						if (it.status == 1) status = "🟢"; else if (it.status == 2) status = "🟡"; else if (it.status == 3) status = "🟠"; else status = "🔴"
+						pluginsList.push(`**${status} ${it.internalName.replace("Provider", "")}**`)
+					}
+				}
 				var repoEmbed = {
 					"title": RepoResponse.name,
-					"description": repoPlugins.map(it => {
-						var status;
-						if(it.status == 1) status = "\\🟢"; else if(it.status == 2) status = "\\🟡"; else if(it.status == 3) status = "\\🟠"; else status = "\\🔴"
-						return `**${status} ${it.internalName.replace("Provider", "")}**`
-					}).join("\n"),
+					"description": pluginsList.join("\n"),
 					"url": repo.url || repo,
 					"color": 7232090,
 					"footer": {
