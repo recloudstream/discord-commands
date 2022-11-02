d451ea2135c0dbacae81bebc7f26bd36790ce024
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-02 16:32:54 +0600

    Update pr.js

--- embeds/pr.js
+++ embeds/pr.js
@@ -3,7 +3,7 @@ module.exports = new function main()
 {
   this.embeds = [{
       
-"title": "Pull request feature is free for all <:verycoo:888951267873923093>",
+"title": "Pull request feature is free for all",
       "color": 16711680,
       "image": {
         "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1037307920817324152/pr.png"
