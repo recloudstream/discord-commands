a36dec1537f25bd34338c94e8be52419af87f524
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-02 19:17:49 +0600

    Create k.js

---
+++ embeds/k.js
@@ -0,0 +1,14 @@
+module.exports = new function main()
+
+{
+  this.embeds = [{
+      
+"title": "Known issue.",
+      "color": 16711680,
+      "image": {
+        "url": "https://www.meme-arsenal.com/memes/e37e86d33dbdc95cbd233f36d8a1a0b2.jpg"
+      }
+
+
+    }]
+}
