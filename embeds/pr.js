4aec16493ab698ab1276f25375060893700b0952
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-02 18:36:31 +0600

    Update pr.js

--- embeds/pr.js
+++ embeds/pr.js
@@ -3,7 +3,7 @@ module.exports = function main() {
     "title": "Pull request feature is free for all <:verycoo:888951267873923093>",
     "color": 16711680,
     "image": {
-      "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1037307920817324152/pr.png"
+      "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1037344275681247372/pr.png"
     }
   }]
 }
