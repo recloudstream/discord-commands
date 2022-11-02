71311cd86e180535f500ef7b36b3cd57c0ec9856
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-02 13:00:19 +0200

    ..

--- embeds/pr.js
+++ embeds/pr.js
@@ -1,14 +1,10 @@
-module.exports = function main()
-
-{
+module.exports = function main() {
+  console.log("PR")
   this.embeds = [{
-      
-"title": "Pull request feature is free for all",
-      "color": 16711680,
-      "image": {
-        "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1037307920817324152/pr.png"
-      }
-
-
-    }]
+    "title": "Pull request feature is free for all",
+    "color": 16711680,
+    "image": {
+      "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1037307920817324152/pr.png"
+    }
+  }]
 }
