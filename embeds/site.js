68f9b64d117496ca04d1db7f53c640b82b8e2872
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-03 22:47:37 +0600

    Update site.js

--- embeds/site.js
+++ embeds/site.js
@@ -1,3 +1,16 @@
-module.exports = function main() {
-  this.content = "https://cloudstream.cf/"
+module.exports = function main()
+
+{
+  this.embeds = [{
+
+      "title": "Cloudstream",
+      "description": "Cloudstream is an Android app for streaming and downloading Movies, TV-Series, Anime, Asian dramas and TV channels.",
+      "url": "https://cloudstream.cf/",
+      "color": 16711680,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
+      }
+
+
+    }]
 }
