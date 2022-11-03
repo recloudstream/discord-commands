cfec5d12017630dd3afda5fe73f986ec3a27490a
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-03 22:16:15 +0600

    Update fix.js

--- embeds/fix.js
+++ embeds/fix.js
@@ -6,7 +6,7 @@ module.exports = function main()
 "title": "Troublshooting",
       "description": "You can read this page to get the idea how to fix your posted issue.",
       "url": "https://rentry.org/cs3fix",
-      "color": null,
+      "color": 16711680,
       "thumbnail": {
         "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
       }
