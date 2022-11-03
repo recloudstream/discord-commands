faf72a60a722fad18a00c60e2406267006bcd248
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-03 23:32:48 +0600

    Update links.js

--- embeds/links.js
+++ embeds/links.js
@@ -1,3 +1,11 @@
+
 module.exports = function main() {
-  this.messageUrl = "https://discord.com/channels/1003664099483336736/1035986709902536814/1037773592009658419"
+  this.embeds = [{
+"title": "Cloudstream Links",
+      "description": "<:7R:1037622920811384903>  **Site**: https://cloudstream.cf/\n:jigsaw:  **Repos**: https://cloudstream.cf/repos\n<:rentry:1037775510324916326> **Mirror**: https://rentry.org/cs3/\n<:github:1037775478234292285> **Github **: https://github.com/recloudstream/cloudstream/releases\n:open_file_folder: **Archive **: https://github.com/recloudstream/cloudstream-archive\n<:gitdab:1037775447708160091> **Gitdab **: https://gitdab.com/recloudstream\n<:matrix:1037775510324916326> **Matrix**: https://matrix.to/#/#lagrapps:matrix.org",
+      "url": "https://cloudstream.cf/",
+      "color": 16711680
+
+
+    }]
 }
