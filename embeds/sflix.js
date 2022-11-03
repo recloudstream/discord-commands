d3af1092e3746e8e727cfba1cf012a73293ee836
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-03 19:02:35 +0600

    Create sflix.js

---
+++ embeds/sflix.js
@@ -0,0 +1,21 @@
+module.exports = function main()
+
+{
+  this.embeds = [{
+      
+"color": 16711680,
+      "image": {
+        "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1037710730260131881/SFLIX.png"
+      }
+    },
+    {
+      "title": "Try these sources",
+      "description": "1. Super stream [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n2. Sora stream [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n3. Loklok [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n4. Ask4movies [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n5. Olgply [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)",
+      "color": 16711680,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/emojis/888951267873923093.webp?size=128&quality=lossless"
+      }
+
+
+    }]
+}
