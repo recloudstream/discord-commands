5461d5ed0c3def005ab4f83de28d96a8ea05c597
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-03 22:20:57 +0600

    Update opensub.js

--- embeds/opensub.js
+++ embeds/opensub.js
@@ -1,3 +1,16 @@
-module.exports = function main() {
-  this.content = "https://rentry.org/opensub"
+module.exports = function main()
+
+{
+  this.embeds = [{
+      
+"title": "Opensubtitles guide",
+      "description": "You can read this page to get the idea how to login, use and add your own subtitles in that site.",
+      "url": "https://rentry.org/opensub",
+      "color": 16711680,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
+      }
+
+
+    }]
 }
