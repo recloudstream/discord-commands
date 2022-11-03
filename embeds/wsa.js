29ef631166b51868dd8b03139c697a2636572e40
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-03 22:35:59 +0600

    Update wsa.js

--- embeds/wsa.js
+++ embeds/wsa.js
@@ -1,4 +1,13 @@
 module.exports = function main() {
   this.files = ["https://cdn.discordapp.com/attachments/737724143570780260/1031100838212538388/wsa.mp4"]
-  this.content = "https://rentry.org/wsa"
+ this.embeds = [{
+      "title": "CS3 on Windows 11 installation guide",
+      "description": "You can read this page to get the idea how to install cs3 app on windows 11 using WSA.",
+      "url": "https://cutt.ly/cs3onwin11",
+      "color": 16711680,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
+      }
+
+    }]
 }
