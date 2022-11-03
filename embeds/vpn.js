ef18b610ce33ec45237ffa5d2216adcdb72cc127
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-03 22:43:12 +0600

    Update vpn.js

--- embeds/vpn.js
+++ embeds/vpn.js
@@ -1,3 +1,16 @@
-module.exports = function main() {
-  this.content = "https://rentry.org/askvpn"
+module.exports = function main()
+
+{
+  this.embeds = [{
+      
+
+"title": "VPN related article",
+      "description": "In this page you will get the basic ideas and other FAQs.",
+      "url": "https://rentry.org/askvpn",
+      "color": 16711680,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
+      }
+
+    }]
 }
