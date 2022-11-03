e942e9b5e82e3ec426b5922faeb4db41e18f1df4
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-03 23:34:09 +0600

    Update links.js

--- embeds/links.js
+++ embeds/links.js
@@ -2,7 +2,7 @@
 module.exports = function main() {
   this.embeds = [{
 "title": "Cloudstream Links",
-      "description": "<:7R:1037622920811384903>  **Site**: https://cloudstream.cf/\n:jigsaw:  **Repos**: https://cloudstream.cf/repos\n<:rentry:1037775510324916326> **Mirror**: https://rentry.org/cs3/\n<:github:1037775478234292285> **Github **: https://github.com/recloudstream/cloudstream/releases\n:open_file_folder: **Archive **: https://github.com/recloudstream/cloudstream-archive\n<:gitdab:1037775447708160091> **Gitdab **: https://gitdab.com/recloudstream\n<:matrix:1037775510324916326> **Matrix**: https://matrix.to/#/#lagrapps:matrix.org",
+      "description": "<:7R:1037622920811384903>  **Site**: https://cloudstream.cf/\n:jigsaw:  **Repos**: https://cloudstream.cf/repos\n<:rentry:1037775510324916326> **Mirror**: https://rentry.org/cs3/\n<:github:1037775478234292285> **Github **: https://github.com/recloudstream/cloudstream/releases\n:open_file_folder: **Archive **: https://github.com/recloudstream/cloudstream-archive\n<:gitdab:1037775447708160091> **Gitdab **: https://gitdab.com/recloudstream\n<:matrix:1037776230759551017> **Matrix**: https://matrix.to/#/#lagrapps:matrix.org",
       "url": "https://cloudstream.cf/",
       "color": 16711680
 
