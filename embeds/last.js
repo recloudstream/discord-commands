acfa1ebf14857b3506c52e4ae5a254bf5a8bc3e1
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-04 15:13:24 +0200

    Update last.js

--- embeds/last.js
+++ embeds/last.js
@@ -1,6 +1,7 @@
 module.exports = function main(args, message) {
     var deletedMessage = require("./../events/lastDeletedMessage.js").deletedMessagesArray
     .filter(value=> value.message.channelId == message.channelId).at(-1)
+    if(deletedMessage == null) return this.content = "No message got deleted"
     this.embeds = [
         {
             "description": `**${deletedMessage.message.content}**`,
