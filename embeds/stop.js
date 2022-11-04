7731edc1545d7885a5a5eab32b17dbe9f73303cb
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-04 17:40:27 +0600

    Create stop.js

---
+++ embeds/stop.js
@@ -0,0 +1,10 @@
+module.exports = function main() {
+  this.embeds = [{
+    "description": "💀 for the second time, I dont want any UI requests. This app is not a free disney/hbo/netflix/hulu and sending UI design on how you want me to copy smth from another app is useless, because I dont care.  the **ONLY** useful feature requests are when you reference an open source app that has a feature, that way I can look how they do it. Just saying: \"make free netflix\" will never be acknowledged and you are wasting everyones time",
+      "color": 16711680,
+      "author": {
+        "name": "Blue",
+        "icon_url": "https://cdn.discordapp.com/avatars/476316493143867403/e2c697b3de3baac74c865f4126f84ac6.png?size=1024"
+      }
+  }]
+}
