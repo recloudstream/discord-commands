e864ad34ee7d971f92aa4e52d391d154638f9bad
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-05 20:21:10 +0100

    Update imagine.js

--- embeds/imagine.js
+++ embeds/imagine.js
@@ -3,15 +3,16 @@ const cheerio = require("cheerio");
 
 class AsyncConstructor {
 	constructor(args) {
-        this.args = args
-		return (async (inputs) => {
-            let prompt = inputs.join(" ")
-            let data = (await axios.post("https://stable-diffusion-nft-minter.glitch.me/", "prompt="+encodeURIComponent(prompt).replace(/%20/g, "+"))).data
-            let $ = cheerio.load(data)
-            let img = $('.content img').first().attr('src')
-            this.content = img || "Couldn't generate the image <:cri:906496612749168661>. Please wait like a few seconds."
-            return this
-        })(args);
+            this.args = args
+            let url = Buffer.from('aHR0cHM6Ly9zdGFibGUtZGlmZnVzaW9uLW5mdC1taW50ZXIuZ2xpdGNoLm1lLw==', 'base64').toString('binary')
+            return (async (inputs) => {
+                let prompt = inputs.join(" ")
+                let data = (await axios.post(url, "prompt="+encodeURIComponent(prompt).replace(/%20/g, "+"))).data
+                let $ = cheerio.load(data)
+                let img = $('.content img').first().attr('src')
+                this.content = img || "Couldn't generate the image <:cri:906496612749168661>. Please wait like a few seconds."
+                return this
+            })(args);
 	}
 }
 
