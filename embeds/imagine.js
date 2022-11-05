d9dd80d552bd3d96fd6c62d9eb70c0f3429a2433
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-05 16:14:40 +0100

    Update imagine.js

--- embeds/imagine.js
+++ embeds/imagine.js
@@ -9,9 +9,9 @@ class AsyncConstructor {
             let data = (await axios.post("https://stable-diffusion-nft-minter.glitch.me/", "prompt="+encodeURIComponent(prompt).replace(/%20/g, "+"))).data
             let $ = cheerio.load(data)
             let img = $('.content img').first().attr('src')
-            this.content = img
+            this.content = img || "Couldn't generate the image <:cri:906496612749168661>. Please wait like a few seconds."
             return this
-		})(args);
+        })(args);
 	}
 }
 
