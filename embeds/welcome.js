feecddaf7b38a828114aea8b1e95d1e448338f49
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-06 21:27:39 +0600

    Create welcome.js

---
+++ embeds/welcome.js
@@ -0,0 +1,27 @@
+module.exports = function main(){
+  this.embeds = [{
+      "title": "Welcome to the Lagrapps server!",
+      "color": 16711680
+    },
+    {
+      "title": "Cloudstream setup process",
+      "description": "**Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease-release.apk).\n**Step 2:** Install the [repositories](https://cloudstream.cf/repos).\n\n**GIF guide of the process 🔽**",
+      "color": 16711680,
+      "image": {
+        "url": "https://cdn.discordapp.com/attachments/1038362748049965056/1038832812989632542/setup.gif"
+      }
+    },
+    {
+      "title": "If direct repo install doesn't work",
+      "description": "**Copy/type the link in the repo box (`cs3 > Settings > Extensions > Add repository`)**\n\n:small_blue_diamond: english: **eng**\n:small_blue_diamond: multi: **multi**\n:small_blue_diamond: arabic: **arab**\n:small_blue_diamond: hexated: **hexa**\n:small_blue_diamond: darkdemon: **drepo**\n:small_blue_diamond: LikDev:  **likrepo**\n:small_blue_diamond: nsfw: **nsfw**\n\n**GIF guide of the process 🔽**",
+      "color": 16711680,
+      "image": {
+        "url": "https://cdn.discordapp.com/attachments/1038362748049965056/1038835771706196039/manual.gif"
+      }
+    },
+    {
+      "description": "If you need more guides, go to <#1031508285393748008>.",
+      "color": 16711680
+
+    }]
+}
