34caee6e4fd2f211593eeaa42ef8ff3069f41fbf
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-06 11:23:09 +0100

    fetch latest release automatically

--- embeds/stable.js
+++ embeds/stable.js
@@ -1,9 +1,22 @@
-module.exports = function main() {
-  this.embeds = [{
-    "description": "Download the [stable release](https://github.com/recloudstream/cloudstream/releases/download/v3.2.1/3.2.1.apk) **but you will not get any quick bug fix or server support <:monkeshrug:853332276486209596>**\n\n⚠️ Some new sources may not work with this stable release.",
-    "color": 2815,
-    "thumbnail": {
-      "url": "https://cdn.discordapp.com/attachments/1008207706143871067/1038448568589094982/7R.png"
-    }
-  }]
+const axios = require("axios");
+
+class AsyncConstructor {
+	constructor(args) {
+            this.args = args
+            return (async () => {
+                let data = (await axios.get("https://api.github.com/repos/recloudstream/cloudstream/releases")).data
+                let stable = data.find(it => it && !it.prerelease && it.tag_name !== "pre-release")
+                let apk = stable.assets.find(it => it && it.content_type === "application/vnd.android.package-archive")
+                this.embeds = [{
+                    "description": `Download the [stable release ${stable.tag_name}](${apk.browser_download_url}) **but you will not get any quick bug fix or server support <:monkeshrug:853332276486209596>**\n\n⚠️ Some new sources may not work with this stable release.`,
+                    "color": 2815,
+                    "thumbnail": {
+                        "url": "https://cdn.discordapp.com/attachments/1008207706143871067/1038448568589094982/7R.png"
+                    }
+                }]
+                return this
+            })(args);
+	}
 }
+
+module.exports = AsyncConstructor
