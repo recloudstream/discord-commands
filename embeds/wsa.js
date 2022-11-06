365d89f7501d8c2306d6ef9688afc6efb85c1aad
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-06 21:51:39 +0600

    Update wsa.js

--- embeds/wsa.js
+++ embeds/wsa.js
@@ -3,7 +3,7 @@ module.exports = function main() {
   this.embeds = [{
     "title": "CS3 on Windows 11 installation guide",
     "description": "You can read this page to get the idea how to install cs3 app on windows 11 using WSA.",
-    "url": "https://cutt.ly/cs3onwin11",
+    "url": "https://rentry.org/wsa",
     "color": 16711680,
     "thumbnail": {
       "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
