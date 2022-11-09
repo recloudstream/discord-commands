b74d7f88337e502daad188b5945b1ccc17683d00
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-09 09:20:48 +0600

    Create linux.js

---
+++ embeds/linux.js
@@ -0,0 +1,11 @@
+module.exports = function main() {
+  this.embeds = [{
+"title": "CS3 on Linux installation guide",
+      "description": "You can use [**Waydroid**](https://waydro.id/) to run android on linux.\n🔹[Installing Waydroid](https://docs.waydro.id/usage/install-on-desktops)\n🔹[Install and Run Android Applications](https://docs.waydro.id/usage/install-and-run-android-applications)\n🔹[Full documentation](https://docs.waydro.id/)\n🔹[Video guide](https://www.youtube.com/watch?v=FKnkIf9jb7U)",
+      "color": 16711680,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1015131233824538624/1038363839298805771/l9_1.png"
+      }
+    
+  }]
+}
