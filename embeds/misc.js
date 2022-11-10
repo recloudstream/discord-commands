8a7a492287476b82048521dad5afea7f6fc56547
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-10 17:00:57 +0600

    Update misc.js

--- embeds/misc.js
+++ embeds/misc.js
@@ -1,8 +1,30 @@
 module.exports = function main() {
   this.embeds = [
     {
-      "description": ":small_blue_diamond: External media player: [**MPV**](https://play.google.com/store/apps/details?id=is.xyz.mpv&hl=en&gl=US)\n:small_blue_diamond: External download manager: [**Navi**](https://play.google.com/store/apps/details?id=com.tachibana.downloader)\n:small_blue_diamond: External casting: [**Web Video Cast**](https://play.google.com/store/apps/details?id=com.instantbits.cast.webvideo)\n:small_blue_diamond: Emulator for linux: [**Waydroid**](https://waydro.id/)\n:small_blue_diamond: Free vpn for android: [**1.1.1.1**](https://rentry.org/askvpn#cloudflare)",
-      "color": null
+      "title": "mpv-android",
+      "description": "mpv-android is a video player for Android based on libmpv.\n\n**Features:**\n🔹Hardware and software video decoding\n🔹Gesture-based seeking, volume/brightness control and more\n🔹Advanced video settings (interpolation, debanding, scalers, ...)\n🔹Play network streams with the \"Open URL\" function\n🔹Background playback, Picture-in-Picture, keyboard input supported",
+      "url": "https://play.google.com/store/apps/details?id=is.xyz.mpv&hl=en&gl=US",
+      "color": 6361200,
+      "thumbnail": {
+        "url": "https://upload.wikimedia.org/wikipedia/commons/7/73/Mpv_logo_%28official%29.png"
+      }
+    },
+    {
+      "title": "Download Navi",
+      "description": "Download Navi is a free and open-source download manager with no ads.\n\n**Features:**\n🔹Pause and resume downloads\n🔹Browsers support\n🔹Simultaneous downloads (default 3)\n🔹Multi part downloading (max 16)",
+      "url": "https://play.google.com/store/apps/details?id=com.tachibana.downloader",
+      "color": 1412488,
+      "thumbnail": {
+        "url": "https://f-droid.org/repo/com.tachibana.downloader/en-US/icon_Hyw-WJaW3eM4qTBAiCpeIfImHRPTpvxEUaPSHvDUuIY=.png"
+      }
+    },
+    {
+      "title": "Web Video Caster",
+      "description": "Web Video Caster allows you to watch on your TV videos from your favorite websites.\n\n**Fetures:**\n🔹Shows, live streams, photos, audio files and IPTV.\n🔹Cast local videos stored on your phone.\n🔹Subtitles from the web page, local storage or OpenSubtitles\n🔹Supports Chromecast, Roku, DLNA receivers, Fire tv and sticks, Smart tv and playstations.",
+      "color": 14644241,
+      "thumbnail": {
+        "url": "https://is4-ssl.mzstatic.com/image/thumb/Purple112/v4/ad/1d/f1/ad1df1a7-870b-af66-d5c9-edf7423208bc/AppIcon-0-1x_U007emarketing-0-10-0-85-220.png/230x0w.webp"
+      }
     }
   ]
 }
