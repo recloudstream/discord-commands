83f878cb6867763a17378199ed83897f588c78c7
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-10 12:43:55 +0600

    Update stremio.js

--- embeds/stremio.js
+++ embeds/stremio.js
@@ -4,9 +4,9 @@ this.files = ["https://cdn.discordapp.com/attachments/1016716040648015972/103795
     "title": "Adding stremio addon for catalogue",
     "description": "The Stremio extension is still experimental. Don't expect a stremio like experience. You can add sources in cs3 using this method, but it'll be only for catalogue (most of the time). So, when you will try to play the eps, it will show No link found.",
     "url": "https://rentry.org/stremio",
-    "color": 16711680,
+    "color": 7084972,
     "thumbnail": {
-      "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
+      "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1040154389358329856/stremio.png"
     }
   }]
 }
