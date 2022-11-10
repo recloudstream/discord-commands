ebe4379758c8e1879c60859cee416bfb2c0c78f2
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-10 19:28:43 +0600

    Update warp.js

--- embeds/warp.js
+++ embeds/warp.js
@@ -2,7 +2,7 @@ module.exports = function main() {
   this.embeds = [{
     
       "title": "Use warp to bypass geo restriction",
-      "description": "You can use **warp** if DOH is not bypassing the geo restriction. WARP is a hassle-free alternative which is free. This vpn is for bypassing geo restriction. not for privacy (1.1.1.1 will show your real ip to the site that has Cloudflare).\n\nIf you want privacy as well as geo restriction free experience, you can use [**Winscribe**](https://play.google.com/store/apps/details?id=com.windscribe.vpn) (free) or [**Mullvad**](https://play.google.com/store/apps/details?id=net.mullvad.mullvadvpn) (paid).",
+      "description": "You can use **warp** if DOH is not bypassing the geo restriction. WARP is a hassle-free alternative which is free. This vpn is for bypassing geo restriction. not for privacy (1.1.1.1 will show your real ip to the site that has Cloudflare).\n\nIf you want a vpn that respects your privacy, you can use [**Winscribe**](https://play.google.com/store/apps/details?id=com.windscribe.vpn) (free) or [**Mullvad**](https://play.google.com/store/apps/details?id=net.mullvad.mullvadvpn) (paid).",
       "url": "https://rentry.org/warpplus",
       "color": 16740352,
       "thumbnail": {
