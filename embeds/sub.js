9988d49910eb1d64baf771deb031c92386163df8
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-10 13:40:35 +0600

    Update sub.js

--- embeds/sub.js
+++ embeds/sub.js
@@ -5,7 +5,7 @@ module.exports = function main() {
     "url": "https://rentry.org/cs3sub",
     "color": 16711680,
     "thumbnail": {
-      "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
+      "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1040168844318216202/subtitles.png"
     }
   }]
 }
