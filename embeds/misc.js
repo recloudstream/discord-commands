c40fc1aa66af6af5df6769dc3693431e720e215b
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-10 17:05:02 +0600

    Update misc.js

--- embeds/misc.js
+++ embeds/misc.js
@@ -1,7 +1,7 @@
 module.exports = function main() {
   this.embeds = [
     {
-      "title": "mpv-android",
+     "title": "mpv-android",
       "description": "mpv-android is a video player for Android based on libmpv.\n\n**Features:**\n🔹Hardware and software video decoding\n🔹Gesture-based seeking, volume/brightness control and more\n🔹Advanced video settings (interpolation, debanding, scalers, ...)\n🔹Play network streams with the \"Open URL\" function\n🔹Background playback, Picture-in-Picture, keyboard input supported",
       "url": "https://play.google.com/store/apps/details?id=is.xyz.mpv&hl=en&gl=US",
       "color": 6361200,
@@ -21,6 +21,7 @@ module.exports = function main() {
     {
       "title": "Web Video Caster",
       "description": "Web Video Caster allows you to watch on your TV videos from your favorite websites.\n\n**Fetures:**\n🔹Shows, live streams, photos, audio files and IPTV.\n🔹Cast local videos stored on your phone.\n🔹Subtitles from the web page, local storage or OpenSubtitles\n🔹Supports Chromecast, Roku, DLNA receivers, Fire tv and sticks, Smart tv and playstations.",
+      "url": "https://play.google.com/store/apps/details?id=com.instantbits.cast.webvideo",
       "color": 14644241,
       "thumbnail": {
         "url": "https://is4-ssl.mzstatic.com/image/thumb/Purple112/v4/ad/1d/f1/ad1df1a7-870b-af66-d5c9-edf7423208bc/AppIcon-0-1x_U007emarketing-0-10-0-85-220.png/230x0w.webp"
