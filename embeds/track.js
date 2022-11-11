de20fbb0c3a801f22552a40979c728e3dffbdad4
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-11 10:03:27 +0600

    Update track.js

--- embeds/track.js
+++ embeds/track.js
@@ -1,3 +1,12 @@
 module.exports = function main() {
-  this.content = "https://rentry.org/anitrack"
+  this.embeds = [{
+ "title": "Anime tracking",
+      "description": "Anime tracking process works with **only anime extensions**. So, it'll not work with extensions like superstream, sflix, loklok etc. Some anime extensions might also not work with the tracking process as the sources don't contain all the required metadata.",
+      "url": "https://rentry.org/anitrack",
+      "color": 31487,
+      "thumbnail": {
+        "url": "https://cdn-icons-png.flaticon.com/512/3031/3031727.png"
+      }
+      
+  }]
 }
