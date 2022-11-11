54f5e9cb99a9709fcd7e8299c96e2ceaeb2b89a9
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-11 09:56:20 +0600

    Update trakt.js

--- embeds/trakt.js
+++ embeds/trakt.js
@@ -1,3 +1,13 @@
 module.exports = function main() {
-  this.content = "https://rentry.org/trakt \n https://streamable.com/wbrlnp"
+  this.files = [""]
+  this.embeds = [{
+"title": "How to use trakt list through stremio",
+      "description": "The Stremio extension is still experimental. Don't expect a stremio like experience. Most of the public stremio sources are torrent based but cloudstream doesn't support torrent stream. So, none of the popular sources will work here.\n\n**The trakt example is link only and it will not play any video. Also, this trakt doesn't sync anything.**",
+      "url": "https://rentry.org/trakt",
+      "color": 13828096,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1040473552371060766/trakt-icon-red-white.png"
+      }
+      
+  }]
 }
