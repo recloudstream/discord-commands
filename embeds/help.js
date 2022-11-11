d95c2c5c156613014287c72c7bcbe93e0c0990cf
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-12 01:45:19 +0200

    Update help.js

--- embeds/help.js
+++ embeds/help.js
@@ -1,3 +1,39 @@
 module.exports = function main() {
-  this.messageUrl = "https://discord.com/channels/737724143126052974/1031508285393748008/1036346889467076638"
+  this.content = "Help"
+  this.components = [
+    {
+        "components": [
+            {
+                "custom_id": "help_select",
+                "max_values": 1,
+                "min_values": 1,
+                "options": [
+                  {
+                    "label": "Features",
+                    "emoji": "💠",
+                    "value": "https://discord.com/channels/737724143126052974/1031508285393748008/1040671780488085504"
+                  },
+                  {
+                    "label": "How to",
+                    "emoji": "💠",
+                    "value": "https://discord.com/channels/737724143126052974/1031508285393748008/1040671883357585489"
+                  },
+                  {
+                    "label": "Others",
+                    "emoji": "💠",
+                    "value": "https://discord.com/channels/737724143126052974/1031508285393748008/1040671932816826430"
+                  },
+                  {
+                    "label": "Responses",
+                    "emoji": "💠",
+                    "value": "https://discord.com/channels/737724143126052974/1031508285393748008/1040671950479036507"
+                  }
+                ],
+                "placeholder": "Topics",
+                "type": 3
+            }
+        ],
+        "type": 1
+    }
+  ]
 }
