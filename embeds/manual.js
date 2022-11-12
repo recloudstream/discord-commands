70bce24e7cb1c10c3919e7ccbfef9e32e116456d
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-12 23:30:35 +0600

    Update manual.js

--- embeds/manual.js
+++ embeds/manual.js
@@ -1,20 +1,11 @@
 module.exports = function main() {
   //this.files = ["https://cdn.discordapp.com/attachments/1008296541926199337/1036935177508757544/manual.mp4"]
   this.embeds = [{
-    "title": "For manual install, copy/type the link in the repo box (`cs3 > Settings > Extensions > Add repository`)",
-    "description": `
-      
-:small_blue_diamond: english: **eng**
-:small_blue_diamond: multi: **multi**
-:small_blue_diamond: arabic: **arab**
-:small_blue_diamond: hexated: **hexa**
-:small_blue_diamond: darkdemon: **drepo**
-:small_blue_diamond: LikDev:  **likrepo**
-:small_blue_diamond: nsfw: **nsfw**
-      
-      `,
-    "image": {
-        "url": "https://media.discordapp.net/attachments/1035987415082467338/1041034186552774776/manual.gif?width=439&height=472"
-     },
+     "title": "For manual install of the repositories",
+      "description": ":flag_gb: English: **eng**\n:earth_asia: Multi: **multi**\n:flag_ae: Arabic: **arab**\n:earth_asia: Hexated: **hexa**\n:flag_in: Darkdemon: **drepo**\n:flag_in: LikDev:  **likrepo**\n:underage: NSFW: **nsfw**",
+      "color": 16711680,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1035987415082467338/1041034186552774776/manual.gif"
+      }
   }]
 }
