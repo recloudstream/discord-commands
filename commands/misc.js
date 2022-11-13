0fef1e241fb74926cf0e7ce6c0ea0adbee1a6053
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-13 17:54:23 +0300

    misc

--- commands/misc.js
+++ commands/misc.js
@@ -1,40 +1,39 @@
 module.exports = function main() {
-  this.embeds = [
+  this.content = "Useful miscs"
+  this.components = [
     {
-     "title": "mpv-android",
-      "description": "mpv-android is a video player for Android based on libmpv.\n\n**Features:**\n🔹Hardware and software video decoding\n🔹Gesture-based seeking, volume/brightness control and more\n🔹Advanced video settings (interpolation, debanding, scalers, ...)\n🔹Play network streams with the \"Open URL\" function\n🔹Background playback, Picture-in-Picture, keyboard input supported",
-      "url": "https://play.google.com/store/apps/details?id=is.xyz.mpv&hl=en&gl=US",
-      "color": 6361200,
-      "thumbnail": {
-        "url": "https://upload.wikimedia.org/wikipedia/commons/7/73/Mpv_logo_%28official%29.png"
-      }
-    },
-    {
-      "title": "Download Navi",
-      "description": "Download Navi is a free and open-source download manager with no ads.\n\n**Features:**\n🔹Pause and resume downloads\n🔹Browsers support\n🔹Simultaneous downloads (default 3)\n🔹Multi part downloading (max 16)",
-      "url": "https://play.google.com/store/apps/details?id=com.tachibana.downloader",
-      "color": 1412488,
-      "thumbnail": {
-        "url": "https://f-droid.org/repo/com.tachibana.downloader/en-US/icon_Hyw-WJaW3eM4qTBAiCpeIfImHRPTpvxEUaPSHvDUuIY=.png"
-      }
-    },
-    {
-      "title": "Web Video Caster",
-      "description": "Web Video Caster allows you to watch on your TV videos from your favorite websites.\n\n**Features:**\n🔹Shows, live streams, photos, audio files and IPTV.\n🔹Cast local videos stored on your phone.\n🔹Subtitles from the web page, local storage or OpenSubtitles\n🔹Supports Chromecast, Roku, DLNA receivers, Fire tv and sticks, Smart tv and playstations.",
-      "url": "https://play.google.com/store/apps/details?id=com.instantbits.cast.webvideo",
-      "color": 14644241,
-      "thumbnail": {
-        "url": "https://www.webvideocaster.com/assetsWVC/wvc_icon.png"
-      }
-    },
-    {
-      "title": "GetSub - Subtitles Downloader",
-      "description": "An ad-free [**subscene**](https://subscene.com/) client. Find the right subtitles for all of your movies and shows as soon as possible. A simple design, autocomplete search suggestions and easy download let you just start watching.",
-      "url": "https://play.google.com/store/apps/details?id=ir.getsub",
-      "color": 29680,
-      "thumbnail": {
-        "url": "https://play-lh.googleusercontent.com/Jk4zs3DdW3tGaQ-s8-xYIvqRFK-HH7D7Fm_ylMY2ScmnERI70PVQwt1GyRHstjetnw=w240-h480-rw"
-      }
+        "components": [
+            {
+                "custom_id": "select_commands",
+                "max_values": 1,
+                "min_values": 1,
+                "options": [
+                  {
+                    "label": "mpv-android",
+                    "emoji": "💠",
+                    "value": "misc.js|0"
+                  },
+                  {
+                    "label": "Download Navi",
+                    "emoji": "💠",
+                    "value": "misc.js|1"
+                  },
+                  {
+                    "label": "Web Video Caster",
+                    "emoji": "💠",
+                    "value": "misc.js|2"
+                  },
+                  {
+                    "label": "GetSub - Subtitles Downloader",
+                    "emoji": "💠",
+                    "value": "misc.js|3"
+                  }
+                ],
+                "placeholder": "Click to select",
+                "type": 3
+            }
+        ],
+        "type": 1
     }
   ]
 }
