23444c2250ff2f81d860317a6e688d1359ebf4db
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-13 22:23:38 +0200

    dummy me

--- commands/arg.js
+++ commands/arg.js
@@ -6,27 +6,28 @@ class AsyncConstructor {
 			var attachment = message.attachments.first()
 			if (!attachment?.url) return this;
 			var data = (await axios.get("https://api.trace.moe/search?url=" + attachment?.url)).data
-			this.content = "First Result:"
-			var firstResult = data?.result[0];
-			this.embed = [{
-				"title": "Anilist",
-				"description": firstResult.filename,
-				"url": `https://anilist.co/anime/${firstResult.anilist}`,
-				"color": null,
-				"fields": [
-					{
-						"name": "Episode",
-						"value": firstResult.episode,
-						"inline": true
-					},
-					{
-						"name": "From/To",
-						"value": `${firstResult.from}/${firstResult.to}`,
-						"inline": true
-					}
-				]
-			}]
-			message.channel.send({files: [firstResult.video]})
+			this.content = ""
+			this.embeds = [data.result?.map(json => {
+				return {
+					"title": "Anilist",
+					"description": json.filename,
+					"url": `https://anilist.co/anime/${json.anilist}`,
+					"color": null,
+					"fields": [
+						{
+							"name": "Episode",
+							"value": json.episode,
+							"inline": true
+						},
+						{
+							"name": "From/To",
+							"value": `${json.from}/${json.to}`,
+							"inline": true
+						}
+					]
+				}
+			})]
+			message.channel.send({files: [data?.result[0].video]})
 			return this;
 		})(args);
 	}
