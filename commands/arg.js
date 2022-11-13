0279900eb887533dd9b76abcacd7514411513848
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-13 22:13:44 +0200

    aaa

--- commands/arg.js
+++ commands/arg.js
@@ -4,10 +4,29 @@ class AsyncConstructor {
 		return (async () => {
 			this.content = "No image.";
 			var attachment = message.attachments.first()
-			if(!attachment?.url) return this;
+			if (!attachment?.url) return this;
 			var data = (await axios.get("https://api.trace.moe/search?url=" + attachment?.url)).data
-			this.content = "File: " + attachment?.url ?? "None"
-			console.log(data)
+			this.content = ""
+			this.embed = [data?.result?.map(json => {
+				return {
+					"title": "Anilist",
+					"description": json.filename,
+					"url": `https://anilist.co/anime/${json.anilist}`,
+					"color": null,
+					"fields": [
+						{
+							"name": "Episode",
+							"value": json.episode,
+							"inline": true
+						},
+						{
+							"name": "From/To",
+							"value": `${json.from}/${json.to}`
+						}
+					]
+				}
+			})]
+			message.channel.send({files: [data?.result[0].video]})
 			return this;
 		})(args);
 	}
