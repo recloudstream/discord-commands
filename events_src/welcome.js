60e31b7eb403592489770913d5ea6084965e7cdb
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-13 16:59:03 +0100

    Update welcome.js

--- events_src/welcome.js
+++ events_src/welcome.js
@@ -2,30 +2,27 @@ const sendDm = require("./src/utils/sendDm.js")
 
 module.exports = function main(member){
     try {
-    sendDm(member, {
-      content: "",
-      embeds: [{  
+    sendDm(member, "",
+      [{
         "title": "Welcome to the Lagrapps server!",
         "color": 16711680
       }]
-    })
-    sendDm(member, {
-      content: "",
-      files: ["https://cdn.discordapp.com/attachments/1008207706143871067/1030755127189065748/cs3start.mp4"],
-      embeds: [{
+    )
+    sendDm(member, "",
+      [{
         "title": "Cloudstream setup process",
         "description": "**Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease-release.apk).\n**Step 2:** Install the [repositories](https://cloudstream.cf/repos).\n\n**GIF guide of the process 🔽**",
         "color": 16711680
-      }]
+      }],
+      ["https://cdn.discordapp.com/attachments/1008207706143871067/1030755127189065748/cs3start.mp4"]
     })
-    sendDm(member, {
-      content: "",
-      files: ["https://cdn.discordapp.com/attachments/1008296541926199337/1036935177508757544/manual.mp4"],
-      embeds: [{
+    sendDm(member, "",
+      [{
         "title": "If direct repo install doesn't work",
         "description": "**Copy/type the link in the repo box (`cs3 > Settings > Extensions > Add repository`)**\n\n:small_blue_diamond: english: **eng**\n:small_blue_diamond: multi: **multi**\n:small_blue_diamond: arabic: **arab**\n:small_blue_diamond: hexated: **hexa**\n:small_blue_diamond: darkdemon: **drepo**\n:small_blue_diamond: LikDev:  **likrepo**\n:small_blue_diamond: nsfw: **nsfw**\n\n**GIF guide of the process 🔽**",
         "color": 16711680,
-      }]
+      }],
+      ["https://cdn.discordapp.com/attachments/1008296541926199337/1036935177508757544/manual.mp4"]
     })
     this.embeds = [{
       "description": "If you need more guides, go to <#1031508285393748008>.",
