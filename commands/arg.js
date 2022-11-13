6f0e41f4a79bf318cb486265ba61cd4ef58e3e66
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-13 22:07:14 +0200

    Update arg.js

--- commands/arg.js
+++ commands/arg.js
@@ -3,10 +3,10 @@ class AsyncConstructor {
 	constructor(args, message) {
 		return (async () => {
 			this.content = "No image.";
-			console.log(message.attachments)
-			if(!message.attachments[0]?.url) return this;
-			var data = (await axios.get("https://api.trace.moe/search?url=" + message.attachments[0].url)).data
-			this.content = "File: " + message.attachments[0].url ?? "None"
+			var attachment = message.attachments.first()
+			if(!attachment?.url) return this;
+			var data = (await axios.get("https://api.trace.moe/search?url=" + attachment?.url)).data
+			this.content = "File: " + attachment?.url ?? "None"
 			console.log(data)
 			return this;
 		})(args);
