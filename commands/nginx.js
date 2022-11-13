1b83262ae94b83bffe6f47d17fff956b5a84c3ca
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-13 10:10:18 +0600

    Create nginx.js

---
+++ commands/nginx.js
@@ -0,0 +1,11 @@
+module.exports = function main() {
+this.embeds = [{
+  "title": "How to use nginx with cloudstream",
+      "description": "NGINX is open source software for web serving, reverse proxying, caching, load balancing, media streaming, and more. It started out as a web server designed for maximum performance and stability.",
+      "url": "https://cloudstream.cf/docs/users/use-nginx.md",
+      "color": 38201,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1013281939026890793/1041201937900642374/nginx.png"
+      }
+  }]
+}
