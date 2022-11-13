6bc8261ff470cc117c8f4df5827a2cab9fac3a44
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-13 21:40:42 +0200

    test

--- commands/arg.js
+++ commands/arg.js
@@ -1,9 +1,9 @@
 class AsyncConstructor {
 	constructor(args) {
-        this.args = args
+		this.args = args
 		return (async (inputs) => {
-                  this.content = "Hello "+ inputs.join(", ")
-		  return this;
+			this.content = "Hello " + inputs.join(", ")
+			return this;
 		})(args);
 	}
 }
