c0203f9824e160f59bdf485cb976dffede7bf81c
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-13 17:02:56 +0100

    Update welcome.js

--- events_src/welcome.js
+++ events_src/welcome.js
@@ -11,23 +11,23 @@ module.exports = function main(member){
     sendDm(member, "",
       [{
         "title": "Cloudstream setup process",
-        "description": "**Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease-release.apk).\n**Step 2:** Install the [repositories](https://cloudstream.cf/repos).\n\n**GIF guide of the process 🔽**",
+        "description": "**Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease-release.apk).\n**Step 2:** Install the [repositories](https://cloudstream.cf/repos).\n\n**Video guide of the process 🔽**",
         "color": 16711680
-      }],
-      ["https://cdn.discordapp.com/attachments/1008207706143871067/1030755127189065748/cs3start.mp4"]
+      }]
     )
     sendDm(member, "",
       [{
         "title": "If direct repo install doesn't work",
-        "description": "**Copy/type the link in the repo box (`cs3 > Settings > Extensions > Add repository`)**\n\n:small_blue_diamond: english: **eng**\n:small_blue_diamond: multi: **multi**\n:small_blue_diamond: arabic: **arab**\n:small_blue_diamond: hexated: **hexa**\n:small_blue_diamond: darkdemon: **drepo**\n:small_blue_diamond: LikDev:  **likrepo**\n:small_blue_diamond: nsfw: **nsfw**\n\n**GIF guide of the process 🔽**",
+        "description": "**Copy/type the link in the repo box (`cs3 > Settings > Extensions > Add repository`)**\n\n:small_blue_diamond: english: **eng**\n:small_blue_diamond: multi: **multi**\n:small_blue_diamond: arabic: **arab**\n:small_blue_diamond: hexated: **hexa**\n:small_blue_diamond: darkdemon: **drepo**\n:small_blue_diamond: LikDev:  **likrepo**\n:small_blue_diamond: nsfw: **nsfw**\n\n**Video guide of the process 🔽**",
         "color": 16711680,
       }],
-      ["https://cdn.discordapp.com/attachments/1008296541926199337/1036935177508757544/manual.mp4"]
+      ["https://cdn.discordapp.com/attachments/1008207706143871067/1030755127189065748/cs3start.mp4"]
     )
     this.embeds = [{
       "description": "If you need more guides, go to <#1031508285393748008>.",
       "color": 16711680
     }]
+    this.files = ["https://cdn.discordapp.com/attachments/1008296541926199337/1036935177508757544/manual.mp4"]
     } catch (ex) {
         console.error(ex)   
     }
