43b9678a879e12d421737ebb3f476c6e9473c79b
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-14 19:05:40 +0600

    Create rec.js

---
+++ events_src/rec.js
@@ -0,0 +1,31 @@
+module.exports = [
+  function main() {
+  this.embeds = [{
+     "title": "Movies & Series",
+    "description": "1. Super stream [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n2. Sora stream [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n3. Loklok [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n4. Ask4movies [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n5. Olgply [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)",
+    "color": null
+      }
+    }]
+  },
+  function main() {
+  this.embeds = [{
+      "title": "Anime",
+    "description": "1. Animixplay [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n2. Crunchyroll **(disabled)** [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n3. Zoro [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n4. Tenshi [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n5. 9anime **(disabled)** [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n6. Allanime [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n7. Gogo anime [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)",
+    "color": null
+    }]
+  },
+  function main() {
+  this.embeds = [{
+      "title": "Asian",
+    "description": "1. Loklok [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n2. Kisskh [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n3. Kissasian [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)",
+    "color": null
+    }]
+  },
+  function main() {
+  this.embeds = [{
+       "title": "TV & Sports",
+    "description": "1. IPTV-org **(TV)**[Multi](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions-multilingual/builds/repo.json)\n2. Time4TV **(Sports)**[Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n3. TV247 [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n4. 123TV [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)",
+    "color": null
+    }]
+  }
+]
