22a0bbb424305dd853bdc2cb89745509bfcf916e
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-14 19:02:35 +0600

    Create rec.js

---
+++ commands/rec.js
@@ -0,0 +1,35 @@
+module.exports = function main() {
+  this.content = "**Recommended English Sources**"
+  this.components = [
+    {
+        "components": [
+            {
+                "custom_id": "select_commands",
+                "max_values": 1,
+                "min_values": 1,
+                "options": [
+                  {
+                    "label": "Movies & Series",
+                    "value": "rec.js|0"
+                  },
+                  {
+                    "label": "Anime",
+                    "value": "rec.js|1"
+                  },
+                  {
+                    "label": "Asian",
+                    "value": "misc.js|2"
+                  },
+                  {
+                    "label": "TV & Sports",
+                    "value": "misc.js|3"
+                  }
+                ],
+                "placeholder": "Click to select",
+                "type": 3
+            }
+        ],
+        "type": 1
+    }
+  ]
+}
