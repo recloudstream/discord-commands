b8bf98f21949f7b8e9099b10d0ff1d27db29274d
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-14 22:31:04 +0100

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,3 +1,6 @@
 module.exports = function main() {
-  this.files = ["https://cdn.discordapp.com/attachments/737729263221997619/1035548185063591986/nsfw.mp4"]
+  if ((new Date()).getMonth() === 10 && Math.random() <= 0.3) // somewhere around 30% prolly
+    this.content = "https://tenor.com/view/no-nut-november-nnn-discord-ryan-gosling-embed-fail-gif-23637560"
+  else
+    this.files = ["https://cdn.discordapp.com/attachments/737729263221997619/1035548185063591986/nsfw.mp4"]
 }
