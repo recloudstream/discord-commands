6fe9554568d04882c1097197d0d8687778112c80
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-14 19:45:25 +0600

    Update enrec.js

--- commands/enrec.js
+++ commands/enrec.js
@@ -1,26 +1,35 @@
 module.exports = function main() {
-  this.embeds = [{
-    "title": "Movies & Series",
-    "description": "1. Super stream [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n2. Sora stream [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n3. Loklok [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n4. Ask4movies [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n5. Olgply [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)",
-    "color": null
-  },
-  {
-    "title": "Anime",
-    "description": "1. Animixplay [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n2. Crunchyroll **(disabled)** [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n3. Zoro [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n4. Tenshi [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n5. 9anime **(disabled)** [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n6. Allanime [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n7. Gogo anime [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)",
-    "color": null
-  },
-  {
-    "title": "Asian",
-    "description": "1. Loklok [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n2. Kisskh [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n3. Kissasian [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)",
-    "color": null
-  },
-  {
-    "title": "TV & Sports",
-    "description": "1. IPTV-org **(TV)**[Multi](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions-multilingual/builds/repo.json)\n2. Time4TV **(Sports)**[Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n3. TV247 [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n4. 123TV [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)",
-    "color": null
-  },
-  {
-    "description": "💡For more details, click [here](https://rentry.org/enrec)",
-    "color": null
-  }]
+  this.content = "**Recommended English Sources**"
+  this.components = [
+    {
+        "components": [
+            {
+                "custom_id": "select_commands",
+                "max_values": 1,
+                "min_values": 1,
+                "options": [
+                  {
+                    "label": "Movies & Series",
+                    "value": "enrec.js|0"
+                  },
+                  {
+                    "label": "Anime",
+                    "value": "enrec.js|1"
+                  },
+                  {
+                    "label": "Asian",
+                    "value": "enrec.js|2"
+                  },
+                  {
+                    "label": "TV & Sports",
+                    "value": "enrec.js|3"
+                  }
+                ],
+                "placeholder": "Click to select",
+                "type": 3
+            }
+        ],
+        "type": 1
+    }
+  ]
 }
