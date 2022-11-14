53b01d3672b120f90606df77653490d53d039fbd
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-14 22:32:58 +0100

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,5 +1,5 @@
 module.exports = function main() {
-  if ((new Date()).getMonth() === 10 && Math.random() <= 0.3) // somewhere around 30% prolly
+  if ((new Date()).getMonth() === 10 && Math.random() <= 0.35) // somewhere around 35% prolly
     this.content = "https://tenor.com/view/no-nut-november-nnn-discord-ryan-gosling-embed-fail-gif-23637560"
   else
     this.files = ["https://cdn.discordapp.com/attachments/737729263221997619/1035548185063591986/nsfw.mp4"]
