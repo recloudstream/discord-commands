235c49995d8af8fa05145850cdbf8ade4411bac7
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-16 22:53:04 +0300

    xo

--- commands/monkey.js
+++ commands/monkey.js
@@ -1,35 +1,35 @@
 const { Snake } = require('discord-gamecord');
 class AsyncConstructor {
-	constructor(args, message) {
-		return (async () => {
+  constructor(args, message) {
+    return (async () => {
       const Game = new Snake({
-          message: message,
-          isSlashGame: false,
-          embed: {
-            title: 'Monkey Game',
-            overTitle: 'Game Over',
-            color: '#5865F2'
-          },
-          emojis: {
-            board: '⬜',
-            food: '🍌',
-            up: '⬆️', 
-            down: '⬇️',
-            left: '⬅️',
-            right: '➡️',
-          },
-          snake: { head: '🦍', body: '⬛', tail: '⚫', over: '💀' },
-          foods: ['🍌'],
-          stopButton: 'Stop',
-          timeoutTime: 60000,
-          playerOnlyMessage: 'Only {player} can use these buttons.'
-        });
-        Game.startGame();
-        this.content = "🍌"
-        this.allowedChannels = ["737729263221997619", "851217659395571712"]
-			return this;
-		})(args);
-	}
+        message: message,
+        isSlashGame: false,
+        embed: {
+          title: 'Monkey Game',
+          overTitle: 'Game Over',
+          color: '#5865F2'
+        },
+        emojis: {
+          board: '⬜',
+          food: '🍌',
+          up: '⬆️',
+          down: '⬇️',
+          left: '⬅️',
+          right: '➡️',
+        },
+        snake: { head: '🦍', body: '⬛', tail: '⚫', over: '💀' },
+        foods: ['🍌'],
+        stopButton: 'Stop',
+        timeoutTime: 60000,
+        playerOnlyMessage: 'Only {player} can use these buttons.'
+      });
+      Game.startGame();
+      this.content = "🍌"
+      this.allowedChannels = ["737729263221997619", "851217659395571712"]
+      return this;
+    })(args);
+  }
 }
 
 module.exports = AsyncConstructor
---
+++ commands/xo.js
@@ -0,0 +1,37 @@
+const { TicTacToe } = require('discord-gamecord'); class AsyncConstructor {
+  constructor(args, message) {
+    return (async () => {
+      this.content = "Mention someone."
+      if(!message.mentions.users.first()) return this;
+      const Game = new TicTacToe({
+        message: message,
+        isSlashGame: false,
+        opponent: message.mentions.users.first(),
+        embed: {
+          title: 'Tic Tac Toe',
+          color: '#5865F2',
+          statusTitle: 'Status',
+          overTitle: 'Game Over'
+        },
+        emojis: {
+          xButton: '❌',
+          oButton: '🔵',
+          blankButton: '➖'
+        },
+        timeoutTime: 60000,
+        xButtonStyle: 'DANGER',
+        oButtonStyle: 'PRIMARY',
+        turnMessage: '{emoji} | Its turn of player **{player}**.',
+        winMessage: '{emoji} | **{player}** won the TicTacToe Game.',
+        tieMessage: 'The Game tied! No one won the Game!',
+        timeoutMessage: 'The Game went unfinished! No one won the Game!',
+        playerOnlyMessage: 'Only {player} and {opponent} can use these buttons.'
+      });
+      Game.startGame();
+      this.allowedChannels = ["737729263221997619", "851217659395571712"]
+      return this;
+    })(args);
+  }
+}
+
+module.exports = AsyncConstructor
