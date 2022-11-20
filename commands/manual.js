3792172a5c0a045892cff633bb0743814fda2240
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-20 13:54:57 +0600

    Update manual.js

--- commands/manual.js
+++ commands/manual.js
@@ -2,7 +2,7 @@ module.exports = function main() {
   this.files = ["https://cdn.discordapp.com/attachments/1008296541926199337/1036935177508757544/manual.mp4"]
   this.embeds = [{
      "title": "For manual install of the repositories",
-      "description": ":flag_gb: English: **eng**\n:earth_asia: Multi: **multi**\n:flag_ae: Arabic: **arab**\n:earth_asia: Hexated: **hexa**\n:flag_in: Darkdemon: **drepo**\n:flag_in: LikDev:  **likrepo**\n:underage: NSFW: **nsfw**",
+      "description": ":flag_gb: English: **eng**\n:earth_asia: Multi: **multi**\n:flag_ae: Arabic: **arab**\n:earth_asia: Hexated: **hexa**\n:flag_in: Darkdemon: **drepo**\n:flag_in: LikDev:  **likrepo**\n:underage: NSFW: **nsfw**\n:flag_ea: Stormunblessed: **Storm**",
       "color": 16711680,
       //"image": {
         //"url": "https://media.discordapp.net/attachments/1035987415082467338/1041034186552774776/manual.gif"
