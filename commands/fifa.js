594d8682d5e572989a46b0e2ea8544e581f8790f
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-20 21:45:58 +0600

    Create fifa.js

---
+++ commands/fifa.js
@@ -0,0 +1,19 @@
+module.exports = function main() {
+  this.files = ["https://cdn.discordapp.com/attachments/1008654622740729897/1038783835233603604/time4tv.mp4"]
+  this.embeds = [{
+    "title": "To watch Fifa WC, use Time4tv",
+      "description": "**Step 1:** Install [Hexated](https://l.cloudstream.cf/dir_hexa) repo.\n**Step 2:** Click on the Hexated repo.\n**Step 3:** Scroll down the source list and install time4tv.\n\n**Step 4:** Go to Home> \"None\" > Select \"Live streaming\" > Time4tv",
+      "color": 10160136,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1043912251708559450/2022_FIFA_World_Cup.svg.png"
+      }
+    },
+    {
+      "title": "To get schedule and channel names of every event, click here.",
+      "url": "https://time4tv.stream/schedule.php",
+      "color": 16732930,
+      "thumbnail": {
+        "url": "https://time4tv.stream/images/logo.png"
+      }
+  }]
+}
