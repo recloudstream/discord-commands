5b600954ebf2effc8b0974d91f1dc455a155b114
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-20 22:42:35 +0600

    Create cr.js

---
+++ commands/cr.js
@@ -0,0 +1,10 @@
+module.exports = function main() {
+this.embeds = [{
+  "title": "Use crunchyroll",
+      "description": "[**Install**](https://cs.repo/?raw.githubusercontent.com/Stormunblessed/kamy-cs3/master/repo.json) the repo to get the official WEB versions of the anime with the best quality possible.",
+      "color": 15758080,
+      "thumbnail": {
+        "url": "https://m.media-amazon.com/images/I/417bVUqe0pL.png"
+      }
+  }]
+}
