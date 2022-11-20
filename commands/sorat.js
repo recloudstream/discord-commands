d164a58324025a891b3303c8055e2287a02b2aa1
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-11-20 21:41:58 +0300

    pfft

---
+++ commands/sorat.js
@@ -0,0 +1,36 @@
+module.exports = function main() {
+  this.embeds = [{
+  "title": "Sorastream",
+      "description": "Sorastream is a MultiAPI based source that scrapes **A LOT** of sites and uses tmdb for catalogue.",
+      "color": 11485,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1040642784362762260/SORA.png"
+      }
+    },
+    {
+      "title": "How to install Sorastream",
+      "description": "**Step 1:** Install [**%Hexated%**](%https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json%) repo.\n**Step 2:** Click on the %Hexated% repo.\n**Step 3:** Scroll down the source list and install sorastream.",
+      "color": 11485,
+      "image": {
+        "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1040646459437092985/image.png"
+      }
+    },
+    {
+      "title": "FAQ",
+      "description": "**1. The episode loading is taking too long**\nAs it scrapes a lot of sites, it takes time to load all of them. Skip loading after 5-10 seconds.\n**2. Some sources are not loading**\nSora has some geo restricted sources. Use vpn to access those sources.\n**3. Some titles are showing \"No links found\"**\nSora uses tmdb for catalogue, not the sources. So, it may show titles that no site has.",
+      "color": 11485
+  }]
+  this.components = [
+    {
+        "type": 1,
+        "components": [
+            {
+                "type": 2,
+                "label": "Translate!",
+                "style": 1,
+                "custom_id": "translate"
+            }
+        ]
+    }
+]
+}
