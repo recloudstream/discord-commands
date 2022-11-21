41a73866e78207a8b0e61ee6ffd701e2f37fbee6
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-21 13:06:52 +0600

    Update fifa.js

--- commands/fifa.js
+++ commands/fifa.js
@@ -2,7 +2,7 @@ module.exports = function main() {
   this.files = ["https://cdn.discordapp.com/attachments/1008654622740729897/1038783835233603604/time4tv.mp4"]
   this.embeds = [{
     "title": "To watch Fifa WC, use Time4tv",
-      "description": "**Step 1:** Install [Hexated](https://l.cloudstream.cf/dir_hexa) repo.\n**Step 2:** Click on the Hexated repo.\n**Step 3:** Scroll down the source list and install time4tv.\n\n**Step 4:** Go to Home> \"None\" > Select \"Live streaming\" > Time4tv",
+      "description": "**Step 1:** Install [Hexated](https://l.cloudstream.cf/dir_hexa) repo.\n**Step 2:** Click on the Hexated repo.\n**Step 3:** Scroll down the source list and install time4tv.",
       "color": 10160136,
       "thumbnail": {
         "url": "https://cdn.discordapp.com/attachments/1015131233824538624/1044145155365687356/fifa.png"
