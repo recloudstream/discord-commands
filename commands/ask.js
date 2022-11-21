b0ade6e34b184260a4696993f30f621ad321d812
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-21 12:57:35 +0600

    Update ask.js

--- commands/ask.js
+++ commands/ask.js
@@ -1,4 +1,4 @@
 module.exports = function main() {
-this.files = ["https://cdn.discordapp.com/attachments/1008654622740729897/1041782705374887936/justask.png"]
+this.files = ["https://cdn.discordapp.com/attachments/1015131233824538624/1044144449267171349/justask.png"]
  
 }
