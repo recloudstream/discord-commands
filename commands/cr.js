ac1ed51948fb8e8f3a00f9c2c2f497e5be6be71e
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-24 22:27:59 +0600

    Update cr.js

--- commands/cr.js
+++ commands/cr.js
@@ -4,7 +4,7 @@ this.embeds = [{
       "description": "[**Install**](https://l.cloudstream.cf/dir_kamy) the repo to get the official WEB versions of the anime with the best quality possible. \n\nFor **Manual** installation, type and add **Kamy**",
       "color": 15758080,
       "thumbnail": {
-        "url": "https://m.media-amazon.com/images/I/417bVUqe0pL.png"
+        "url": "https://cdn.discordapp.com/attachments/1035159969332478035/1045375030093156443/cr.png"
       }
   }]
 }
