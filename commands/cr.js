8c191c2857b5346a6a20129290be248094868a45
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-24 22:06:31 +0600

    Update cr.js

--- commands/cr.js
+++ commands/cr.js
@@ -1,7 +1,7 @@
 module.exports = function main() {
 this.embeds = [{
   "title": "Use crunchyroll",
-      "description": "[**Install**](https://cs.repo/?raw.githubusercontent.com/Stormunblessed/kamy-cs3/master/repo.json) the repo to get the official WEB versions of the anime with the best quality possible.",
+      "description": "[**Install**](https://l.cloudstream.cf/dir_kamy) the repo to get the official WEB versions of the anime with the best quality possible.",
       "color": 15758080,
       "thumbnail": {
         "url": "https://m.media-amazon.com/images/I/417bVUqe0pL.png"
