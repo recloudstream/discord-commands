ff156eb3bb403bf854661e9f28f5136b4bb37f35
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-25 21:05:33 +0600

    Update fifa.js

--- commands/fifa.js
+++ commands/fifa.js
@@ -10,7 +10,7 @@ module.exports = function main() {
       "color": 8328241
     },
     {
-      "title": "Stremio iptv.org",
+      "title": "Stremiotv",
       "description": "**Step 1:** Install [English](https://l.cloudstream.cf/dir_eng) repo.\n**Step 2:** Click on the English repo and install **Stremio**\n**Step 3:** Then **clone stremio** using [this link](https://2ecbbd610840-stremio-iptv.baby-beamup.club/cmVnaW9ucz1iZWluaXB0dixlZ2lwdHZ8Y29zdHVtZT12aXA6VklQIDphSFIwY0hNNkx5OWpaRzR1WkdselkyOXlaR0Z3Y0M1amIyMHZZWFIwWVdOb2JXVnVkSE12TVRBME5UQTVOVEUxTlRNek1UVXhNRE13TWk4eE1EUTFNRGsxTXpBek5EQXpNREkwTkRNMEwzZHZlV1ZrYjNWaWJHVndkV05sWHprdWJUTjE=/)\n\n⚠️ To get the video guide of the process, use **.stremiotv** in <#737729263221997619>",
       "color": 8328241
   }]
