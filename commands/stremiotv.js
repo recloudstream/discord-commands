c886276f019cc20c138933930674ec9eac999819
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-25 20:28:44 +0600

    Update stremiotv.js

--- commands/stremiotv.js
+++ commands/stremiotv.js
@@ -1,4 +1,4 @@
 module.exports = function main() {
-this.files = ["https://cdn.discordapp.com/attachments/1015131233824538624/1045702114342621184/stremiotv.mp4"]
+this.files = ["https://cdn.discordapp.com/attachments/1035159969332478035/1045707470284271616/stremiotv.mp4"]
  
 }
