ec4433a06eb27322d1c4e370f8a67e846ddd9b85
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-27 14:38:01 +0100

    add .voteinfo

---
+++ commands/voteinfo.js
@@ -0,0 +1,54 @@
+const axios = require("axios");
+const { createHash } = require("crypto");
+function hash(url) {
+    return createHash("sha256")
+        .update(url + "#funny-salt")
+        .digest("hex");
+}
+
+class AsyncConstructor {
+	constructor(args) {
+		return (async (inputs) => {
+            let repoId = inputs.at(0)
+            let pluginId = inputs.at(1)
+			let file = globalThis.events_src.filter(json => json.name == "repos.js")[0]
+            let repo = requireFromString(file.code).find(it => it?.name === repoId || it?.sort === shortcut).url
+            if (!repo && repoId.indexOf("http") !== -1) repo = repoId 
+            let repoResponse = (await axios.get(repo, {
+                headers: {
+                    'accept-encoding': 'null'
+                }
+            })).data;
+            let repoPlugins = []
+            for (const pluginUrl of RepoResponse.pluginLists) {
+                if (!pluginUrl || !pluginUrl.startsWith("http")) continue;
+                (await axios.get(pluginUrl, {
+                    headers: {
+                        'accept-encoding': 'null'
+                    }
+                })).data.forEach((data) => {
+                    repoPlugins.push(data);
+                });
+            }
+            let plugin = repoPlugins.find(it => it?.name === pluginId || it?.internalName == pluginId)
+            let url = "https://api.countapi.xyz/info/" + hash(plugin?.url || repoId);
+            let countReponse = (await axios.get(url)).data;
+            this.embeds = [
+                {
+                    "title": "VoteAPI entry info",
+                    "fields": (["Created", "TTL"].map(it=>({
+                        "name": it,
+                        "value": `<t:${countReponse[it.toLowerCase()]}:R>`,
+                        "inline": false
+                    })) + ["Value", "Update Lowerbound", "Update Upperbound"].map(it=>({
+                        "name": it,
+                        "value": `<t:${countReponse[it.toLowerCase().replace(/\s/g,"_")]}:R>`,
+                        "inline": false
+                    })))
+                }
+            ]
+			return this;
+		})();
+	}
+}
+module.exports = AsyncConstructor
