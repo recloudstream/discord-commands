db148f1d6fc11c1ff6d152b27830435f1894f880
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-11-27 15:12:24 +0100

    Update voteinfo.js

--- commands/voteinfo.js
+++ commands/voteinfo.js
@@ -52,7 +52,7 @@ class AsyncConstructor {
                         "value": `${countReponse[it.toLowerCase().replace(/\s/g,"_")]}`,
                         "inline": false
                     }))
-	    console.error({fields})
+		fields = fields || []
             this.embeds = [
                 {
                     "title": "VoteAPI entry info",
