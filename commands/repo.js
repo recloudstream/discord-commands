ca34717f968cc2d7878da8e4c725dca3f4906897
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-28 17:17:29 +0600

    Update repo.js

--- commands/repo.js
+++ commands/repo.js
@@ -4,12 +4,9 @@ module.exports = function main() {
   var repos = requireFromString(file.code)
   this.embeds = [{
     "title": "Cloudstream Repositories",
-    "description": repos.map(value => `${value.emoji} **${value.name}**: [Install](${value.url})`).join("\n\n") + "\n\nDon't install all the plugins from a repo without knowing.",
+    "description": repos.map(value => `${value.emoji} **${value.name}**: [Install](${value.url})`).join("\n\n")",
     "url": "https://cloudstream.cf/repos",
     "color": 16711680,
-    "image": {
-      "url": "https://cdn.discordapp.com/attachments/1036209801803333632/1036609641636962334/unknown.png"
-    }
   }]
 
 }
