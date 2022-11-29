eba5179e4df7351312f6e54b4f1f80a68e099c19
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-29 20:53:01 +0600

    Update super.js

--- commands/super.js
+++ commands/super.js
@@ -1,8 +1,8 @@
 module.exports = function main() {
 this.embeds = [{
   
-  "title": "PLEASE Read updates",
-      "description": "Superstream breaks when the main source of the API fucks up something. Cloudstream can't solve this issue which is already posted in <#737729080492818543>. FFS read those before reporting about the issue.\n\n**When the owner of the source fixes the issue**, it'll be fixed in Superstream too. So don't report about it.\n\nUse **Sorastream** instead which is a very stable source with a lot of video and subtitle options.",
+  "title": "PLEASE Read updates FFS",
+      "description": "Superstream breaks when the main source of the API fucks up something. Cloud stream can't solve this issue, which is already posted in <#737729080492818543>. FFS read those before reporting the issue.\n\n**When the owner of the source fixes the issue**, it'll be fixed in Superstream too. So don't report it.\n\nUse **Sorastream** instead, which is a very stable source with a lot of video and subtitle options.",
       "color": 16711680
   
   }]
