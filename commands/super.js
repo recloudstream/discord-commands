4add7ebf6368e1977d85b7733784ebc3178577d6
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-29 19:55:40 +0600

    Create super.js

---
+++ commands/super.js
@@ -0,0 +1,9 @@
+module.exports = function main() {
+this.embeds = [{
+  
+  "title": "PLEASE Read updates FFS",
+      "description": "Superstream breaks when the main source of the API fucks up something. Cloudstream can't solve this issue which is already posted in <#737729080492818543>. FFS read those before reporting about the issue.\n\n**When the owner of the source fixes the issue**, it'll be fixed in Superstream too. So don't report about it.\n\nUse **Sorastream** instead which is a very stable source with a lot of video and subtitle options.",
+      "color": 16711680
+  
+  }]
+}
