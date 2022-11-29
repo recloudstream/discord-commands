600a377357cdb71c63c08ff65cdff9c752b8ae32
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-11-29 12:29:03 +0600

    Create cast.js

---
+++ commands/cast.js
@@ -0,0 +1,17 @@
+module.exports = function main() {
+this.embeds = [{
+  
+  "title": "Why is chromecasting not working?",
+      "description": "**1. **The provider/video host simply not supporting casting (referrer blocked). **We can't fix this.**\n**2. **You are using a VPN, most video links are IP blocked meaning that casting will fail if you are connected to a VPN.",
+      "color": 14855947
+    },
+    {
+      "title": "You can try external web caster.",
+      "description": "For example, Web Video Caster allows you to watch on your TV videos from your favorite websites.\n**Features:**\n🔹Shows, live streams, photos, audio files and IPTV.\n🔹Cast local videos stored on your phone.\n🔹Subtitles from the web page, local storage or OpenSubtitles\n🔹Supports Chromecast, Roku, DLNA receivers, Fire tv and sticks, Smart tv and playstations.\n\n[**Click here**](https://play.google.com/store/apps/details?id=com.instantbits.cast.webvideo) to install the app. But keep in mind that this software may not solve your issue.",
+      "color": 14855947,
+      "thumbnail": {
+        "url": "https://www.webvideocaster.com/assetsWVC/wvc_icon.png"
+      }
+  
+  }]
+}
