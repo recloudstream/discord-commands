0a180f9527ce480b0b3e5ca1fc8f03a2fae8a98f
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-01 21:56:26 +0600

    Create ola.js

---
+++ commands/ola.js
@@ -0,0 +1,10 @@
+module.exports = function main() {
+this.embeds = [{
+  "title": "Olamovies is still Beta",
+      "description": "This source is an **unfinished source and catalogue only** at this moment. Likdev is busy with his study. So don't use and report about the source.",
+      "color": 16765696,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1035159969332478035/1047902926984720474/9DlbfMc.png"
+      }
+  }]
+}
