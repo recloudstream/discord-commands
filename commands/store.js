c340d4c23e469ab0499b7d2b0e7e4735842b3a19
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-02 22:11:32 +0600

    s

--- commands/store.js
+++ commands/store.js
@@ -1,6 +1,6 @@
 module.exports = function main() {
   this.embeds = [{
-    "title": "Cloudstream Beta - Miscrosoft Store Apps",
+    "title": "Cloudstream Beta - Microsoft Store Apps",
     "description": "Cloudstream is a windows app for streaming and downloading Movies, TV-Series, Asian dramas, Anime and IPTV. The app provides all the contents without any sign-up and subscription.\n\nFeatures include:\n- 4K movies\n- Anime tracking\n- Subtitle downloads\n- Chromecast support",
     "url": "https://rentry.org/wsa",
     "image": {
