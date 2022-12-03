11a72e70f8dedf3d33b0556a4b610879f180b071
0x00568 <68516357+MustardChef@users.noreply.github.com>
2022-12-03 13:11:26 +0000

    Update pr.js

--- commands/pr.js
+++ commands/pr.js
@@ -1,6 +1,6 @@
 module.exports = function main() {
   this.embeds = [{
-    "title": "Pull request feature is free for all <:verycoo:888951267873923093>",
+    "title": "The Pull request feature is free for all <:verycoo:888951267873923093>",
     "color": 16711680,
     "image": {
       "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1037344275681247372/pr.png"
