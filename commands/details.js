af1d3f60c1008fde3fb4e8db56672a875203f441
0x00568 <68516357+MustardChef@users.noreply.github.com>
2022-12-03 13:00:51 +0000

    Update details.js

--- commands/details.js
+++ commands/details.js
@@ -1,6 +1,6 @@
 module.exports = function main() {
   this.embeds = [{
-    "description": "1. Don't put your issue in <#737724143570780260>\n2. Don't spam your issue. If anybody knows the solution, he will help you. Otherwise, wait for the devs' response.\n3. Post your issue at <#737725084881387652>",
+    "description": "1. Don't put your issue in <#737724143570780260>\n2. Don't spam your issue. If anybody knows the solution, they will help you. Otherwise, wait for the devs' response.\n3. Post your issue at <#737725084881387652>",
     "color": 15000804
   },
   {
@@ -10,7 +10,7 @@ module.exports = function main() {
   },
   {
     "title": "Post your issue with these details",
-    "description": "1. The app version (example: 3.1.56 · 65b5efb)\n2. Source(s) names\n3. Full description of your issue.\n4. Screen shots or screen recordings or photos of the issue.\n5. Post the logcat (cs3 > settings > backup & updates > show logcat) (Optional)",
+    "description": "1. The app version (example: 3.1.56 · 65b5efb)\n2. Source(s) names\n3. Full description of your issue.\n4. Screenshots or screen recordings or photos of the issue.\n5. Post the logcat (cs3 > Settings > Backup & updates > Show logcat🐈) (Optional)",
     "color": 15000804
   },
   {
