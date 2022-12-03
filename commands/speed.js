8b672365a182089ac982077cc07b8602e35f5107
0x00568 <68516357+MustardChef@users.noreply.github.com>
2022-12-03 13:14:36 +0000

    Update speed.js

--- commands/speed.js
+++ commands/speed.js
@@ -1,7 +1,7 @@
 module.exports = function main() {
 this.embeds = [{
   "title": "Why \"XYZ\" server is so slow?",
-      "description": "Speed of the servers are **NOT** constant for every user. It has various factors. For example:\n**1.** Your region\n**2.** Your bandwidth\n**3.** Your isp's DNS\n**4.** Server traffic",
+      "description": "Speed of the servers are **NOT** constant for every user. It has various factors. For example:\n**1.** Your region\n**2.** Your bandwidth\n**3.** Your ISP's DNS\n**4.** Server traffic",
       "color": 16711680
   }]
 }
