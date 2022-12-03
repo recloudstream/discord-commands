7ae66a2f596b930825cba43d4f0c076e56989935
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-12-03 15:33:12 +0300

    aaa

--- commands/pinfo.js
+++ commands/pinfo.js
@@ -99,7 +99,7 @@ class AsyncConstructor {
                         },
                         {
                             "name": "Repository",
-                            "value": `<cloudstreamrepo://${repo}>`,
+                            "value": `<cloudstreamrepo://${repo.replace("https//", "")}>`,
                             "inline": true
                         },
                         {
