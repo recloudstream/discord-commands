fceac0ee04480ecf2fe2e48e3f47470a3e2affc0
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-03 22:35:40 +0600

    Create button.js

---
+++ commands/button.js
@@ -0,0 +1,17 @@
+module.exports = function main() {
+  this.content = "Cloudstream Related Links"
+  this.components = [
+    {
+        "type": 1,
+        "components": [
+            {
+                "type": 2,
+                "label": "Click me!",
+                "url": "https://discord.com/developers/docs/interactions/message-components#action-rows",
+                "style": 5,
+                "emoji": "😀"
+            }
+        ]
+    }
+  ]
+}
