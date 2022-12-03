2c1a185f80f8b3825f3cd9e0169f4a117f56e6c6
0x00568 <68516357+MustardChef@users.noreply.github.com>
2022-12-03 13:16:47 +0000

    Update subs.js

--- commands/subs.js
+++ commands/subs.js
@@ -1,7 +1,7 @@
 module.exports = function main() {
   this.embeds = [{
     "title": "Subtitles guide",
-    "description": "You can read this page to get the idea how to use all the common subtitle functions in the app.",
+    "description": "You can read this page to get an idea on how to use all the common subtitle functions in the app.",
     "url": "https://rentry.org/cs3sub",
     "color": 16711680,
     "thumbnail": {
