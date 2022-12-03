3f095282fd3dd64ac92b5a82fb78d5d51f7910a8
0x00568 <68516357+MustardChef@users.noreply.github.com>
2022-12-03 13:09:44 +0000

    Update nginx.js

--- commands/nginx.js
+++ commands/nginx.js
@@ -1,7 +1,7 @@
 module.exports = function main() {
 this.embeds = [{
   "title": "How to use nginx with cloudstream",
-      "description": "NGINX is open source software for web serving, reverse proxying, caching, load balancing, media streaming, and more. It started out as a web server designed for maximum performance and stability.",
+      "description": "NGINX is an open source software for web serving, reverse proxying, caching, load balancing, media streaming, and more. It started out as a web server designed for maximum performance and stability.",
       "url": "https://cloudstream.cf/docs/users/use-nginx.md",
       "color": 38201,
       "thumbnail": {
