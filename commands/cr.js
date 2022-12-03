9af5eb22cb391fcb9989818dc34ddf2bdef013a1
0x00568 <68516357+MustardChef@users.noreply.github.com>
2022-12-03 12:59:00 +0000

    Update cr.js

--- commands/cr.js
+++ commands/cr.js
@@ -1,6 +1,6 @@
 module.exports = function main() {
 this.embeds = [{
-  "title": "Use crunchyroll",
+  "title": "Use Crunchyroll",
       "description": "[**Install**](https://l.cloudstream.cf/dir_kamy) the repo to get the official WEB versions of the anime with the best quality possible. \n\nFor **Manual** installation, type and add **Kamy**",
       "color": 15758080,
       "thumbnail": {
