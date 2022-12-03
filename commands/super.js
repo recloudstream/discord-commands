3e4583d7ef05904e857eb6e9cfc7317a5668385e
0x00568 <68516357+MustardChef@users.noreply.github.com>
2022-12-03 13:19:07 +0000

    Update super.js

--- commands/super.js
+++ commands/super.js
@@ -2,7 +2,7 @@ module.exports = function main() {
 this.embeds = [{
   
   "title": "PLEASE Read updates FFS",
-      "description": "Superstream breaks when the main source of the API fucks up something. Cloud stream can't solve this issue, which is already posted in <#737729080492818543>. FFS read those before reporting the issue.\n\n**When the owner of the source fixes the issue**, it'll be fixed in Superstream too. So don't report it.\n\nUse **Sorastream** instead, which is a very stable source with a lot of video and subtitle options.",
+      "description": "SuperStream breaks when the main source of the API fucks up something. Cloudstream can't solve this issue, which is already posted in <#737729080492818543>. FFS read those before reporting the issue.\n\n**When the owner of the source fixes the issue**, it'll be fixed in SuperStream too. So don't report it.\n\nUse **SoraStream** instead, which is a very stable source with a lot of video and subtitle options.",
       "color": 16711680
   
   }]
