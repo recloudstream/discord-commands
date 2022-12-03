94a23f0f0a904b385f2b8e8e152b3e632741e665
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-03 13:22:48 +0600

    Update vpn.js

--- commands/vpn.js
+++ commands/vpn.js
@@ -3,9 +3,9 @@ module.exports = function main() {
     "title": "VPN related article",
     "description": "In this page you will get the basic ideas and other FAQs.",
     "url": "https://rentry.org/askvpn",
-    "color": 16711680,
+    "color": 8981425,
     "thumbnail": {
-      "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
+      "url": "https://cdn-icons-png.flaticon.com/512/188/188120.png"
     }
   }]
 }
