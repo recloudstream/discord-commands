948302bcb822b858fe7fe45e155a3f3aaf049a9a
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-06 19:07:02 +0600

    sora sflix

--- commands/sflix.js
+++ commands/sflix.js
@@ -4,10 +4,12 @@ module.exports = {
   async execute(message) {
     message.channel.send({
       embeds: [{
-        "color": 16711680,
-        "image": {
-          "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1037710730260131881/SFLIX.png"
-        }
+      "title": "Sflix.to is now available in Sorastream.",
+      "description": "The standalone sflix extension in English repo is broken amd there is no eta. But there is an API for sflix.to in Sorastream. You can find the Sflix servers as **Upcloud** and **Vidcloud**. But use other sources like UHD, Gdrive movies, HDmoviebox, Uniquestream to get better qualities.",
+      "color": 370579,
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1015131233824538624/1049671048800518154/UzT2h5G.png"
+      }
       },
       {
         "title": "Try these sources",
