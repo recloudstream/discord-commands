494ce02850b358b4ec1c5b2b66de4cc2d372f187
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-12-06 14:34:30 +0100

    add .read

---
+++ commands/read.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "read",
+  async execute(message) {
+    message.channel.send({
+      files: ["https://cdn.discordapp.com/attachments/859049451195727922/943918348092194846/videoplayback.mp4"]
+    })
+  },
+};
