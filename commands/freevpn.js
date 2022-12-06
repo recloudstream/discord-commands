2ee684eb0ebd0fa88ec24e7bf30a6d8495883a03
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-07 00:32:05 +0600

    edit

--- commands/freevpn.js
+++ commands/freevpn.js
@@ -6,14 +6,14 @@ module.exports = {
 "title": "Winscribe",
       "description": "**Features:**\n🔹10 server locations\n🔹10 GB/month bandwidth\n🔹Unlimited simultaneous connection\n\n**Server locations:** U.S., Canada, U.K, Hong Kong, France, Germany, Netherlands, Switzerland, Norway, and Romania",
       "url": "https://play.google.com/store/apps/details?id=com.windscribe.vpn&hl=en&gl=US",
-      "color": 0,
+      "color": 16777215,
       "thumbnail": {
         "url": "https://avatars.githubusercontent.com/u/30499389?s=280&v=4"
       }
     },
     {
       "title": "Proton",
-      "description": "**Features:**\n🔹3 server locations\n🔹Unlimited bandwidth\n🔹Single VPN connection\n\n**Server locations:** U.S., Netherlands, Japan.",
+      "description": "**Features:**\n🔹3 server locations\n🔹Unlimited bandwidth\n🔹Single VPN connection\n\n**Server locations:** U.S., Netherlands, Japan.\n\nThe number of free servers is pretty low. So, you may face high ping and connection issues.",
       "url": "https://play.google.com/store/apps/details?id=ch.protonvpn.android&hl=en&gl=US",
       "color": 7764701,
       "thumbnail": {
@@ -21,11 +21,11 @@ module.exports = {
       }
     },
     {
-      "title": "Self-hosted VPN",
-      "description": "**Features: **\n🔹1 server location\n🔹10TB/month\n🔹up to 500Mbps speed\n\n[Sample video tutorial for Oracle](https://www.youtube.com/watch?v=E-CLtExRzX8)\n⚠️We won't provide any support for self-hosted vpn method. Go to other servers or sites to get the the support.",
-      "color": 16750848,
+      "title": "Oracle Self-hosted VPN",
+      "description": "**Features: **\n🔹1 server location\n🔹10TB/month\n🔹up to 500Mbps speed\n\n**Server locations:** U.S., Netherlands, Switzerland. \n\n[Sample video tutorial for Oracle](https://www.youtube.com/watch?v=E-CLtExRzX8) | [Documentation](https://blogs.oracle.com/developers/post/launching-your-own-free-private-vpn-in-the-oracle-cloud)\n\n⚠️We won't provide any support for self-hosted VPN method. Go to other servers or sites to get the support. Also, you can do the same process with other services too (AWS, Google cloud etc.)",
+      "color": 16711680,
       "thumbnail": {
-        "url": "https://is2-ssl.mzstatic.com/image/thumb/Purple112/v4/dd/af/ea/ddafea05-cb7c-3d74-1f2a-03279fa6d349/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp"
+        "url": "https://leadsbridge.com/wp-content/themes/leadsbridge/img/integration-lg-logos/logo106.png"
       }
       }]
     })
