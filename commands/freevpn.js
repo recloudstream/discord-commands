de0d9c5edf48863762e0f1938bf1db82ab56391e
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-07 00:06:22 +0600

    Create freevpn.js

---
+++ commands/freevpn.js
@@ -0,0 +1,28 @@
+module.exports = function main() {
+this.embeds = [{
+      "title": "Winscribe",
+      "description": "**Features:**\n🔹10 server locations\n🔹10 GB/month bandwidth\n🔹Unlimited simultaneous connection\n\n**Server locations:** U.S., Canada, U.K, Hong Kong, France, Germany, Netherlands, Switzerland, Norway, and Romania",
+      "url": "https://play.google.com/store/apps/details?id=com.windscribe.vpn&hl=en&gl=US",
+      "color": 0,
+      "thumbnail": {
+        "url": "https://avatars.githubusercontent.com/u/30499389?s=280&v=4"
+      }
+    },
+    {
+      "title": "Proton",
+      "description": "**Features:**\n🔹3 server locations\n🔹Unlimited bandwidth\n🔹Single VPN connection\n\n**Server locations:** U.S., Netherlands, Japan.",
+      "url": "https://play.google.com/store/apps/details?id=ch.protonvpn.android&hl=en&gl=US",
+      "color": 7764701,
+      "thumbnail": {
+        "url": "https://seeklogo.com/images/P/proton-vpn-logo-A50452564D-seeklogo.com.png"
+      }
+    },
+    {
+      "title": "Self-hosted VPN",
+      "description": "**Features: **\n🔹1 server location\n🔹10TB/month\n🔹up to 500Mbps speed\n\n[Sample video tutorial for Oracle](https://www.youtube.com/watch?v=E-CLtExRzX8)\n⚠️We won't provide any support for self-hosted vpn method. Go to other servers or sites to get the the support.",
+      "color": 16750848,
+      "thumbnail": {
+        "url": "https://is2-ssl.mzstatic.com/image/thumb/Purple112/v4/dd/af/ea/ddafea05-cb7c-3d74-1f2a-03279fa6d349/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp"
+      }
+  }]
+}
