960038ee818ae431430c39f0c74452de0328ed90
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-12-06 18:57:17 +0300

    Update enrec.js

--- events_src/enrec.js
+++ events_src/enrec.js
@@ -10,21 +10,21 @@ module.exports = [
   function main() {
   this.embeds = [{
       "title": "Anime",
-    "description": "1. Animixplay [Hexated]https://l.cloudstream.cf/dir_hexa)\n2. Crunchyroll [Kamyroll]( https://l.cloudstream.cf/dir_kamy)\n3. Zoro [English](https://l.cloudstream.cf/dir_eng)\n4. Tenshi [English](https://l.cloudstream.cf/dir_eng)\n5. 9anime **(disabled)** [English](https://l.cloudstream.cf/dir_eng)\n6. Allanime [English](https://l.cloudstream.cf/dir_eng)\n7. Gogo anime [English](https://l.cloudstream.cf/dir_eng)",
+    "description": "1. Animixplay [Hexated](https://l.cloudstream.cf/dir_hexa)\n2. Crunchyroll [Kamyroll](https://l.cloudstream.cf/dir_kamy)\n3. Zoro [English](https://l.cloudstream.cf/dir_eng)\n4. Tenshi [English](https://l.cloudstream.cf/dir_eng)\n5. 9anime **(disabled)** [English](https://l.cloudstream.cf/dir_eng)\n6. Allanime [English](https://l.cloudstream.cf/dir_eng)\n7. Gogo anime [English](https://l.cloudstream.cf/dir_eng)",
     "color": null
     }]
   },
   function main() {
   this.embeds = [{
       "title": "Asian",
-    "description": "1. Loklok [Hexated]https://l.cloudstream.cf/dir_hexa)\n2. Kisskh [Hexated]https://l.cloudstream.cf/dir_hexa)\n3. Kissasian [Hexated]https://l.cloudstream.cf/dir_hexa)",
+    "description": "1. Loklok [Hexated](https://l.cloudstream.cf/dir_hexa)\n2. Kisskh [Hexated](https://l.cloudstream.cf/dir_hexa)\n3. Kissasian [Hexated](https://l.cloudstream.cf/dir_hexa)",
     "color": null
     }]
   },
   function main() {
   this.embeds = [{
        "title": "TV & Sports",
-    "description": "1. IPTV-org **(TV)** [Multilingual]( https://l.cloudstream.cf/dir_multi)\n2. Time4TV **(Sports)** [Hexated]https://l.cloudstream.cf/dir_hexa)\n3. 9goal **(Football) [Multilingual]( https://l.cloudstream.cf/dir_multi)**\n4. TV247 [Hexated]https://l.cloudstream.cf/dir_hexa)\n5. 123TV [Hexated]https://l.cloudstream.cf/dir_hexa)",
+    "description": "1. IPTV-org **(TV)** [Multilingual](https://l.cloudstream.cf/dir_multi)\n2. Time4TV **(Sports)** [Hexated](https://l.cloudstream.cf/dir_hexa)\n3. 9goal **(Football) [Multilingual](https://l.cloudstream.cf/dir_multi)**\n4. TV247 [Hexated]https://l.cloudstream.cf/dir_hexa)\n5. 123TV [Hexated](https://l.cloudstream.cf/dir_hexa)",
     "color": null
     }]
   }
