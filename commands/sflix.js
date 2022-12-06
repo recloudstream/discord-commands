b7b2dc711e11ef1e33d47f6e69f7e832a9d81ee8
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-06 19:54:08 +0600

    removed the recs from sflix

--- commands/sflix.js
+++ commands/sflix.js
@@ -10,14 +10,6 @@ module.exports = {
       "thumbnail": {
         "url": "https://cdn.discordapp.com/attachments/1015131233824538624/1049671048800518154/UzT2h5G.png"
       }
-      },
-      {
-        "title": "Try these sources",
-        "description": "1. Super stream [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n2. Sora stream [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n3. Loklok [Hexated](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json)\n4. Ask4movies [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)\n5. Olgply [English](https://cs.repo/?raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json)",
-        "color": 16711680,
-        "thumbnail": {
-          "url": "https://cdn.discordapp.com/emojis/888951267873923093.webp?size=128&quality=lossless"
-        }
       }]
     })
   },
