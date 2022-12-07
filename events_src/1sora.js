f4192be3bacfc987a7f62ba9c324097998b863a7
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-07 12:29:52 +0600

    Create 1sora.js

---
+++ events_src/1sora.js
@@ -0,0 +1,19 @@
+module.exports = [
+  function main() {
+  this.embeds = [{
+     "title": "How to install Sorastream",
+        "description": "**Step 1:** Install [**Hexated**](https://cs.repo/?raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/builds/repo.json) repo.\n**Step 2:** Click on the Hexated repo.\n**Step 3:** Scroll down the source list and install Sorastream.",
+        "color": 11485,
+        "image": {
+          "url": "https://cdn.discordapp.com/attachments/1008207706143871067/1047801841729159198/4HrnIIn.png"
+        }
+    }]
+  },
+  function main() {
+  this.embeds = [{
+   "title": "FAQ",
+        "description": "**1. The episode loading is taking too long**\nAs it scrapes a lot of sites, it takes time to load all of them. Skip loading after 3-5 seconds.\n**2. Some sources are not loading**\nSora has some geo restricted sources. Use a VPN to access those sources.\n**3. Some titles are showing \"No links found\"**\nSora uses TMDb for catalogue, not the sources. So, it may show titles that no site has.",
+        "color": 11485
+    }]
+  }
+]
