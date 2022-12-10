a0c188fee113c35b2869efbb3f7d3ef577203dad
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-10 22:15:08 +0600

    Update ola.js

--- commands/ola.js
+++ commands/ola.js
@@ -3,8 +3,8 @@ module.exports = {
   async execute(message) {
     message.channel.send({
       embeds: [{
-        "title": "Olamovies is still Beta",
-        "description": "This source is an **unfinished source and catalogue only** at this moment. Likdev is busy with his study. So don't use and report about the source.",
+        "title": "Olamovies is unfixable right now",
+        "description": "This source is an **unfinished source and catalogue only**. The source is unfixable right now. So don't use and report about the source.",
         "color": 16765696,
         "thumbnail": {
           "url": "https://cdn.discordapp.com/attachments/1035159969332478035/1047902926984720474/9DlbfMc.png"
