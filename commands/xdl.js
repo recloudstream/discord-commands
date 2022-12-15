9a7efbe69a3beb2354ccd67c5c90b55ff44ddd53
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-15 17:56:33 +0600

    making it 1dm

--- commands/xdl.js
+++ commands/xdl.js
@@ -1,8 +1,17 @@
 module.exports = {
-  name: "whystable",
+  name: "xdl",
   async execute(message) {
     message.channel.send({
-      files: ["https://cdn.discordapp.com/attachments/1008207706143871067/1030877443868852374/extdl.mp4"]
+      files: ["https://cdn.discordapp.com/attachments/1015131233824538624/1052276644569677864/use1DM.mp4"],
+      embeds: [{
+       "title": "Use 1DM",
+      "description": "You can download from any source without any issue using 1DM. **Max the Number of connections** to get the best download speed",
+      "url": "https://play.google.com/store/apps/details?id=idm.internet.download.manager&hl=en&gl=US",
+      "color": null,
+      "thumbnail": {
+        "url": "https://play-lh.googleusercontent.com/cvdER0qgn-J36TrPVdvoQ3oWa3QzUVnIaPvXhddOgVZhz50tfxFAAbw76PqWEtlHmTo=w240-h480-rw"
+      }
+      }]
     })
   },
 };
