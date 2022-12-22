5092a79dee1270853e5d782bd0e7e228460bfe3e
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-22 12:22:30 +0600

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  name: "cast",
+  async execute(message) {
+    message.channel.send({
+      embeds: [{
+    "title": "Cloudstream Help",
+      "description": "1. [**CS3 Links**](https://discord.com/channels/737724143126052974/1055344541558919229/1055344545329598514)\n2. [**CS3 Setup**](https://discord.com/channels/737724143126052974/1055345748822204487/1055345754555818024)\n3. [**TV Setup**](https://discord.com/channels/737724143126052974/1055347828794339378/1055347836201488404)\n4. [**WSA Setup**](https://discord.com/channels/737724143126052974/1055348198320902175/1055348205430259732)\n5. [**Subtitles FAQ**](https://discord.com/channels/737724143126052974/1055349163396706445/1055349167939141672)\n6. [**Opensubs FAQ**](https://discord.com/channels/737724143126052974/1055350005088653352/1055350008788033626)\n7. [**Anime Tracking**](https://discord.com/channels/737724143126052974/1055350390935269448/1055350398241746995)\n8. [**Downloader FAQ**](https://discord.com/channels/737724143126052974/1055351934430740530/1055351936968302692)\n9. [**Other Settings**](https://discord.com/channels/737724143126052974/1055354229126074390/1055354317181308958)\n10. [**Related Apps**](https://discord.com/channels/737724143126052974/1055355930318995476/1055355933225652235)\n11. [**CS3 Fixes**](https://discord.com/channels/737724143126052974/1055362005193740389/1055362007840325643)\n12. [**Command List**](https://discord.com/channels/737724143126052974/1055359189465178143/1055359192363450399)",
+      "color": 623359,
+      "thumbnail": {
+        "url": "https://media.discordapp.net/attachments/1015131233824538624/1055368886406619157/game-bloons-arcane-mastery-and-staff-pack.png"
+      }
+
+      }]
+    })
+  },
+};
