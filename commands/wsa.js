0b15cdd3c1c4d0001b62f40abfef5c6c427aab17
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-22 12:28:53 +0600

    Update wsa.js

--- commands/wsa.js
+++ commands/wsa.js
@@ -6,7 +6,7 @@ module.exports = {
       embeds: [{
         "title": "CS3 on Windows 11 installation guide",
         "description": "You can read this page to get the idea how to install cs3 app on Windows 11 using WSA.",
-        "url": "https://discord.com/channels/1003664099483336736/1036209801803333632/1055369806645301288",
+        "url": "https://discord.com/channels/737724143126052974/1055348198320902175/1055348205430259732",
         "color": 1083392,
         "thumbnail": {
           "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1039955155099078726/wsa.png"
