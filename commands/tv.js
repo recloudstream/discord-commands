1ed6ac6bfe360357c672954435c4e9e2910346a3
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-22 12:27:02 +0600

    Update tv.js

--- commands/tv.js
+++ commands/tv.js
@@ -2,11 +2,11 @@ module.exports = {
   name: "tv",
   async execute(message) {
     message.channel.send({
-      files: ["https://cdn.discordapp.com/attachments/1036144578690031726/1037765163127935016/tv.mp4"],
+      //files: ["https://cdn.discordapp.com/attachments/1036144578690031726/1037765163127935016/tv.mp4"],
       embeds: [{
         "title": "TV installation guide",
         "description": "You can read this page to get an idea on how to install cs3 app and repos on TV.",
-        "url": "https://rentry.org/cs3tv",
+        "url": "https://discord.com/channels/737724143126052974/1055347828794339378/1055347836201488404",
         "color": 16711680,
         "thumbnail": {
           "url": "https://cdn-icons-png.flaticon.com/512/716/716429.png"
