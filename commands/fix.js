54d1872378b0d8034d2d53b228f4c61e2dbf89b7
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-22 12:33:13 +0600

    Update fix.js

--- commands/fix.js
+++ commands/fix.js
@@ -5,7 +5,7 @@ module.exports = {
       embeds: [{
         "title": "Troubleshooting",
         "description": "You can read this page to get the idea how to fix your posted issue.",
-        "url": "https://rentry.org/cs3fix",
+        "url": "https://discord.com/channels/737724143126052974/1055362005193740389/1055362007840325643",
         "color": 16711680,
         "thumbnail": {
           "url": "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png"
