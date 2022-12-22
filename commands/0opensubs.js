d5af36756035f326738b89049f6d0d376358c08f
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-22 11:04:58 +0600

    Update 0opensubs.js

--- commands/0opensubs.js
+++ commands/0opensubs.js
@@ -1,40 +1,16 @@
-
 module.exports = {
-    name: "duckling",
-    onlyUsers: ["777478477569196044", "326466810150912000"],
-    async execute(message) {
-        message.channel.send({
+  name: "opensubs",
+  async execute(message) {
+    message.channel.send({
       embeds: [{
- "title": "Creating an account",
-      "description": "[Go here](https://www.opensubtitles.com/en) and create an account.",
-      "color": null,
-      "image": {
-        "url": "https://user-images.githubusercontent.com/57977673/194716796-c5556b8b-a0b7-4580-9054-f36d1304e545.png"
-      }
-    },
-    {
-      "title": "Adding Open Subtitles account in Cloudstream",
-      "description": "**Settings > Account > OpenSubtitles >** Login with **username and password**",
-      "color": null,
-      "image": {
-        "url": "https://user-images.githubusercontent.com/57977673/194716884-04860e09-7910-4216-bf26-52fbca4e0a86.png"
-      }
-    },
-    {
-      "title": "How to use Open subtitles",
-      "description": "**Video player > sources> subtitles> Load from internet** and then search and apply",
-      "color": null,
-      "image": {
-        "url": "https://user-images.githubusercontent.com/57977673/194716636-f16fe137-c2e6-4529-bfd8-b86214ab8843.png"
-      }
-    },
-    {
-      "color": null,
-      "image": {
-        "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1029316063659315210/unknown.png"
-      }
+        "title": "Opensubtitles guide",
+        "description": "You can read this page to get an idea on how to login, use and add your own subtitles to that site.",
+        "url": "https://rentry.org/opensub",
+        "color": 16711680,
+        "thumbnail": {
+          "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1040172066348990526/os.png"
         }
       }]
-        })
-    },
+    })
+  },
 };
