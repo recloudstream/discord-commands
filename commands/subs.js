5c11cde9947a706e5e58d340ad43eca2b7412be5
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-22 12:29:43 +0600

    Update subs.js

--- commands/subs.js
+++ commands/subs.js
@@ -5,7 +5,7 @@ module.exports = {
       embeds: [{
         "title": "Subtitles guide",
         "description": "You can read this page to get an idea on how to use all the common subtitle functions in the app.",
-        "url": "https://rentry.org/cs3sub",
+        "url": "https://discord.com/channels/737724143126052974/1055349163396706445/1055349167939141672",
         "color": 16711680,
         "thumbnail": {
           "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1040168844318216202/subtitles.png"
