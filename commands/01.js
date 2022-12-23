db4967ae49ae29865c5fb734a46abe49eafba682
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-23 20:59:17 +0600

    Create 01.js

---
+++ commands/01.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: "duckling",
+    onlyUsers: ["777478477569196044", "326466810150912000"],
+    async execute(message) {
+        message.channel.send({
+      embeds: [{
+        "description": "<#1055344541558919229>\n<#1055345442495397938>",
+      "color": 688895,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1031508285393748008/1055344427062788116/1_links.png?width=1440&height=360"
+      }
+  
+      }]
+        })
+    },
+};
