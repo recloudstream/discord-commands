08af38fa1de8ff4b893862f5146890cfd20dd0b6
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-23 19:48:24 +0600

    Create 0index.js

---
+++ commands/0index.js
@@ -0,0 +1,37 @@
+module.exports = {
+    name: "duckling",
+    onlyUsers: ["777478477569196044", "326466810150912000"],
+    async execute(message) {
+        message.channel.send({
+        files: ["https://media.discordapp.net/attachments/737724143570780260/1049346871946117173/cs3start.mp4"],
+      embeds: [{
+        "description": "<#1055344541558919229>\n<#1055345442495397938>",
+      "color": 688895,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1031508285393748008/1055344427062788116/1_links.png?width=1440&height=360"
+      }
+    },
+    {
+      "description": "<#1055345748822204487>\n<#1055346838644994128>\n<#1055347828794339378>\n<#1055348198320902175>\n<#1055348413216083989>",
+      "color": 688895,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1031508285393748008/1055345600771674172/2_setup.png?width=1440&height=360"
+      }
+    },
+    {
+      "description": "<#1055348949675950110>\n<#1055349163396706445>\n<#1055350005088653352>\n<#1055350390935269448>\n<#1055350767772508241>",
+      "color": 688895,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1031508285393748008/1055348578027065344/3_features.png?width=1440&height=360"
+      }
+    },
+    {
+      "description": "<#1055351934430740530>\n<#1055352375004635177>\n<#1055355930318995476>\n<#1055359189465178143>\n<#1055354229126074390>\n<#1055438609643221012>\n<#1055362005193740389>",
+      "color": 688895,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1031508285393748008/1055351529021919302/4_others.png?width=1440&height=360"
+      }
+      }]
+        })
+    },
+};
