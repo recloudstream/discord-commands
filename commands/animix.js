0c1ae6bbfb99f7edb543497a57143e8d51eb1629
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-23 22:17:13 +0600

    Create animix.js

---
+++ commands/animix.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "k",
+  async execute(message) {
+    message.channel.send({
+      embeds: [{
+
+        "title": "Use other Anime extensions. We recommend Kamyroll, Sora, Allanime, Gogo, tenshi.",
+        "color": 16711680,
+        "image": {
+          "url": "https://cdn.discordapp.com/attachments/1013281939026890793/1055880766883639418/image.png"
+        }
+      }]
+    })
+  },
+};
