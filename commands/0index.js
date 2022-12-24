f8b8fd13d834dacc21b6e4953b4b6e161bdf94dc
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-24 09:39:34 +0600

    Update 0index.js

--- commands/0index.js
+++ commands/0index.js
@@ -4,32 +4,36 @@ module.exports = {
     async execute(message) {
         message.channel.send({
             embeds: [{
-                "description": "[**Cloudstream Related Links**](https://discord.com/channels/737724143126052974/1055344541558919229/)\n[**Cloudstream Forks**](https://discord.com/channels/737724143126052974/1055345442495397938/)",
-                "color": 688895,
-                "image": {
-                    "url": "https://media.discordapp.net/attachments/1031508285393748008/1055344427062788116/1_links.png?width=1440&height=360"
-                }
-            },
-            {
-                "description": "[**Standard Setup Process**](https://discord.com/channels/737724143126052974/1055345748822204487/)\n[**Manual Setup Process**](https://discord.com/channels/737724143126052974/1055346838644994128/)\n[**TV Setup Process**](https://discord.com/channels/737724143126052974/1055347828794339378/)\n[**Installation on Windows 11**](https://discord.com/channels/737724143126052974/1055348198320902175/)\n[**Installation on Linux**](https://discord.com/channels/737724143126052974/1055348413216083989/)",
-                "color": 688895,
-                "image": {
-                    "url": "https://media.discordapp.net/attachments/1031508285393748008/1055345600771674172/2_setup.png?width=1440&height=360"
-                }
-            },
-            {
-                "description": "[**Cross Device Sync**](https://discord.com/channels/737724143126052974/1055348949675950110/)\n[**Subtitle Related FAQ**](https://discord.com/channels/737724143126052974/1055349163396706445/)\n[**Opensubtitles Related FAQ**](https://discord.com/channels/737724143126052974/1055350005088653352/)\n[**Anime tracking**](https://discord.com/channels/737724143126052974/1055350390935269448/)\n[**Cloning a Site**](https://discord.com/channels/737724143126052974/1055350767772508241/)",
-                "color": 688895,
-                "image": {
-                    "url": "https://media.discordapp.net/attachments/1031508285393748008/1055348578027065344/3_features.png?width=1440&height=360"
-                }
-            },
-            {
-                "description": "[**Downloading from CS3**](https://discord.com/channels/737724143126052974/1055351934430740530/)\n[**Stremio Related Stuff**](https://discord.com/channels/737724143126052974/1055352375004635177/)\n[**Cloudstream Related Apps**](https://discord.com/channels/737724143126052974/1055355930318995476/)\n[**Command List**](https://discord.com/channels/737724143126052974/1055359189465178143/)\n[**Others settings**](https://discord.com/channels/737724143126052974/1055354229126074390/)\n[**Other Guides**](https://discord.com/channels/737724143126052974/1055438609643221012/)\n[**Cloudstream Fixes**](https://discord.com/channels/737724143126052974/1055362005193740389/)",
-                "color": 688895,
-                "image": {
-                    "url": "https://media.discordapp.net/attachments/1031508285393748008/1055351529021919302/4_others.png?width=1440&height=360"
-                }
+"title": "Cloudstream Links",
+      "description": "[**Cloudstream Related Links**](https://discord.com/channels/737724143126052974/1055344541558919229/)\n[**Cloudstream Forks**](https://discord.com/channels/737724143126052974/1055345442495397938/)",
+      "color": 688895,
+      "thumbnail": {
+        "url": "https://media.discordapp.net/attachments/1015131233824538624/1055368886406619157/game-bloons-arcane-mastery-and-staff-pack.png"
+      }
+    },
+    {
+      "title": "Cloudstream Setup",
+      "description": "[**Standard Setup Process**](https://discord.com/channels/737724143126052974/1055345748822204487/)\n[**Manual Setup Process**](https://discord.com/channels/737724143126052974/1055346838644994128/)\n[**TV Setup Process**](https://discord.com/channels/737724143126052974/1055347828794339378/)\n[**Installation on Windows 11**](https://discord.com/channels/737724143126052974/1055348198320902175/)\n[**Installation on Linux**](https://discord.com/channels/737724143126052974/1055348413216083989/)",
+      "color": 688895,
+      "thumbnail": {
+        "url": "https://media.discordapp.net/attachments/1015131233824538624/1055368886406619157/game-bloons-arcane-mastery-and-staff-pack.png"
+      }
+    },
+    {
+      "title": "Cloudstream Features",
+      "description": "[**Cross Device Sync**](https://discord.com/channels/737724143126052974/1055348949675950110/)\n[**Subtitle Related FAQ**](https://discord.com/channels/737724143126052974/1055349163396706445/)\n[**Opensubtitles Related FAQ**](https://discord.com/channels/737724143126052974/1055350005088653352/)\n[**Anime tracking**](https://discord.com/channels/737724143126052974/1055350390935269448/)\n[**Cloning a Site**](https://discord.com/channels/737724143126052974/1055350767772508241/)",
+      "color": 688895,
+      "thumbnail": {
+        "url": "https://media.discordapp.net/attachments/1015131233824538624/1055368886406619157/game-bloons-arcane-mastery-and-staff-pack.png"
+      }
+    },
+    {
+      "title": "Others",
+      "description": "[**Downloading from CS3**](https://discord.com/channels/737724143126052974/1055351934430740530/)\n[**Stremio Related Stuff**](https://discord.com/channels/737724143126052974/1055352375004635177/)\n[**Cloudstream Related Apps**](https://discord.com/channels/737724143126052974/1055355930318995476/)\n[**Command List**](https://discord.com/channels/737724143126052974/1055359189465178143/)\n[**Others settings**](https://discord.com/channels/737724143126052974/1055354229126074390/)\n[**Other Guides**](https://discord.com/channels/737724143126052974/1055438609643221012/)\n[**Cloudstream Fixes**](https://discord.com/channels/737724143126052974/1055362005193740389/)",
+      "color": 688895,
+      "thumbnail": {
+        "url": "https://media.discordapp.net/attachments/1015131233824538624/1055368886406619157/game-bloons-arcane-mastery-and-staff-pack.png"
+      }
             }]
         })
     },
