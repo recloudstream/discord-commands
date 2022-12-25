b6222be07c9a078c5f98b6742023ffa7b2513607
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-25 19:29:51 +0600

    retry

--- commands/0index.js
+++ commands/0index.js
@@ -4,36 +4,36 @@ module.exports = {
     async execute(message) {
         message.channel.send({
             embeds: [{
-"title": "Cloudstream Links",
-      "description": "[**Cloudstream Related Links**](https://discord.com/channels/737724143126052974/1055344541558919229/)\n[**Cloudstream Forks**](https://discord.com/channels/737724143126052974/1055345442495397938/)",
-      "color": 688895,
-      "thumbnail": {
-        "https://media.discordapp.net/attachments/1038362748049965056/1056563038750257152/7R.png"
+                "title": "Cloudstream Links",
+                "description": "[**Cloudstream Related Links**](https://discord.com/channels/737724143126052974/1055344541558919229/)\n[**Cloudstream Forks**](https://discord.com/channels/737724143126052974/1055345442495397938/)",
+                "color": 688895,
+                "thumbnail": {
+                    "https://media.discordapp.net/attachments/1038362748049965056/1056563038750257152/7R.png"
       }
-    },
-    {
-      "title": "Cloudstream Setup",
-      "description": "[**Standard Setup Process**](https://discord.com/channels/737724143126052974/1055345748822204487/)\n[**TV Setup Process**](https://discord.com/channels/737724143126052974/1055347828794339378/)\n[**Installation on Windows 11**](https://discord.com/channels/737724143126052974/1055348198320902175/)\n[**Installation on Linux**](https://discord.com/channels/737724143126052974/1055348413216083989/)",
-      "color": 688895,
-      "thumbnail": {
-        "url": "https://media.discordapp.net/attachments/1038362748049965056/1056563039110959124/l9.png"
-      }
-    },
-    {
-      "title": "Cloudstream Features",
-      "description": "[**Cross Device Sync**](https://discord.com/channels/737724143126052974/1055348949675950110/)\n[**Subtitle Related FAQ**](https://discord.com/channels/737724143126052974/1055349163396706445/)\n[**Opensubtitles Related FAQ**](https://discord.com/channels/737724143126052974/1055350005088653352/)\n[**Anime tracking**](https://discord.com/channels/737724143126052974/1055350390935269448/)\n[**Cloning a Site**](https://discord.com/channels/737724143126052974/1055350767772508241/)",
-      "color": 688895,
-      "thumbnail": {
-        "url": "https://media.discordapp.net/attachments/1038362748049965056/1056563039962402866/JU1.png"
-      }
-    },
-    {
-      "title": "Others",
-      "description": "[**Downloading from CS3**](https://discord.com/channels/737724143126052974/1055351934430740530/)\n[**Stremio Related Stuff**](https://discord.com/channels/737724143126052974/1055352375004635177/)\n[**Cloudstream Related Apps**](https://discord.com/channels/737724143126052974/1055355930318995476/)\n[**Command List**](https://discord.com/channels/737724143126052974/1055359189465178143/)\n[**Others settings**](https://discord.com/channels/737724143126052974/1055354229126074390/)\n[**Other Guides**](https://discord.com/channels/737724143126052974/1055438609643221012/)\n[**Cloudstream Fixes**](https://discord.com/channels/737724143126052974/1055362005193740389/)",
-      "color": 688895,
-      "thumbnail": {
-        "url": "https://media.discordapp.net/attachments/1038362748049965056/1056563039496843316/Lw.png"
-      }
+            },
+            {
+                "title": "Cloudstream Setup",
+                "description": "[**Standard Setup Process**](https://discord.com/channels/737724143126052974/1055345748822204487/)\n[**TV Setup Process**](https://discord.com/channels/737724143126052974/1055347828794339378/)\n[**Installation on Windows 11**](https://discord.com/channels/737724143126052974/1055348198320902175/)\n[**Installation on Linux**](https://discord.com/channels/737724143126052974/1055348413216083989/)",
+                "color": 688895,
+                "thumbnail": {
+                    "url": "https://media.discordapp.net/attachments/1038362748049965056/1056563039110959124/l9.png"
+                }
+            },
+            {
+                "title": "Cloudstream Features",
+                "description": "[**Cross Device Sync**](https://discord.com/channels/737724143126052974/1055348949675950110/)\n[**Subtitle Related FAQ**](https://discord.com/channels/737724143126052974/1055349163396706445/)\n[**Opensubtitles Related FAQ**](https://discord.com/channels/737724143126052974/1055350005088653352/)\n[**Anime tracking**](https://discord.com/channels/737724143126052974/1055350390935269448/)\n[**Cloning a Site**](https://discord.com/channels/737724143126052974/1055350767772508241/)",
+                "color": 688895,
+                "thumbnail": {
+                    "url": "https://media.discordapp.net/attachments/1038362748049965056/1056563039962402866/JU1.png"
+                }
+            },
+            {
+                "title": "Others",
+                "description": "[**Downloading from CS3**](https://discord.com/channels/737724143126052974/1055351934430740530/)\n[**Stremio Related Stuff**](https://discord.com/channels/737724143126052974/1055352375004635177/)\n[**Cloudstream Related Apps**](https://discord.com/channels/737724143126052974/1055355930318995476/)\n[**Command List**](https://discord.com/channels/737724143126052974/1055359189465178143/)\n[**Others settings**](https://discord.com/channels/737724143126052974/1055354229126074390/)\n[**Other Guides**](https://discord.com/channels/737724143126052974/1055438609643221012/)\n[**Cloudstream Fixes**](https://discord.com/channels/737724143126052974/1055362005193740389/)",
+                "color": 688895,
+                "thumbnail": {
+                    "url": "https://media.discordapp.net/attachments/1038362748049965056/1056563039496843316/Lw.png"
+                }
             }]
         })
     },
