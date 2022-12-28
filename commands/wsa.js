089ba4c976416469948220194634d6621fc70e6f
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-28 22:27:22 +0600

    Update wsa.js

--- commands/wsa.js
+++ commands/wsa.js
@@ -4,9 +4,9 @@ module.exports = {
     async execute(message) {
         message.channel.send({
             embeds: [{
-                "title": "CS3 on Windows Subsystem for Android installation guide",
+                "title": "Cloudstream on WSA Installation Guide",
                 "description": "Windows Subsystem for Android™️ enables your Windows device to run Android applications. Choose your Windows OS version to get the installation guide.",
-                "url": "https://discord.com/channels/737724143126052974/1055348198320902175/1055348205430259732",
+             
                 "color": 1083392,
                 "thumbnail": {
                   "url": "https://cdn.discordapp.com/attachments/1008654622740729897/1039955155099078726/wsa.png"
