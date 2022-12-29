e0f8f5120ea96180f9b892303ba5b1f10a6adea0
sfaresee <57977673+sfaresee@users.noreply.github.com>
2022-12-29 19:44:27 +0600

    Update and rename super.md to super.js

---
+++ commands/super.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            //files: [""],
+            embeds: [{
+                  "title": "Superstream is overloaded! Again!! Very unexpected!!!",
+      "description": "Superstream's servers are over loaded because of too much user traffic. So, the superstream extension is down. Cloudstream developers can't fix the issue.\n**The Fix release date has no ETA. So don't report about the issue.**",
+      "color": null,
+      "thumbnail": {
+        "url": "https://images-ext-2.discordapp.net/external/u2Ki-fdmmSmwKm0ByB92Zk0LgWwj1hHJbsUYYIoZT80/https/raw.githubusercontent.com/recloudstream/cloudstream-extensions/master/SuperStream/icon.png"
+      } 
+            }]
+        })
+    },
+};
--- commands/super.md
+++
@@ -1,9 +0,0 @@
-module.exports = function main() {
-this.embeds = [{
-  
-  "title": "PLEASE Read updates FFS",
-      "description": "SuperStream breaks when the main source of the API fucks up something. Cloudstream can't solve this issue, which is already posted in <#737729080492818543>. FFS read those before reporting the issue.\n\n**When the owner of the source fixes the issue**, it'll be fixed in SuperStream too. So don't report it.\n\nUse **SoraStream** instead, which is a very stable source with a lot of video and subtitle options.",
-      "color": 16711680
-  
-  }]
-}
