509f29d1db5b1b262a6947beb95a61c61d480630
sfaresee <57977673+sfaresee@users.noreply.github.com>
2023-01-01 00:01:32 +0600

    Update wsa.js

--- commands/wsa.js
+++ commands/wsa.js
@@ -6,7 +6,7 @@ module.exports = {
             //files: [""],
             embeds: [{
 
-                "title": "CS3 on Windows Subsystem for Android installation guide",
+                "title": "CS3 on WSA guide",
                 "description": "Windows Subsystem for Android™️ enables your Windows device to run Android applications. Choose your Windows OS version to get the installation guide.",
                 "url": "https://discord.com/channels/737724143126052974/1055348198320902175/1055348205430259732",
                 "color": 1083392,
