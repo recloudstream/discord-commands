ee115f21d3c9695e12a35965bc65442d30e393ec
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-01-01 17:28:35 +0100

    Create nofap.js

---
+++ commands/nofap.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "nofap",
+  async execute(message) {
+    message.channel.send({
+      files: ["https://cdn.discordapp.com/attachments/1059099649409024030/1059145196497014784/XgmvI0N.mp4"]
+    })
+  },
+};
