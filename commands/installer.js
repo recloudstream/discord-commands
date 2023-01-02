03c76946546559c45c1f19030661a652fcfec6b7
sfaresee <57977673+sfaresee@users.noreply.github.com>
2023-01-02 23:00:00 +0600

    Update installer.js

--- commands/installer.js
+++ commands/installer.js
@@ -6,7 +6,7 @@ module.exports = {
             //files: [""],
             embeds: [{
                 "title": "Change APK Installer",
-                "description": "Go to **CS3 → Updates and backup → APK Installer → Legacy**",
+                "description": "Go to **CS3 → Updates and backup → APK Installer → Legacy → Apply**",
                 "color": null,
                 "image": {
                     "url": "https://media.discordapp.net/attachments/1015131233824538624/1059516097373683833/image.png"
