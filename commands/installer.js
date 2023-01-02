35a419db4bcc4a97d8fd3e5c9e3b346fa89a5b16
sfaresee <57977673+sfaresee@users.noreply.github.com>
2023-01-02 22:59:00 +0600

    Create installer.js

---
+++ commands/installer.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            //files: [""],
+            embeds: [{
+                "title": "Change APK Installer",
+                "description": "Go to **CS3 → Updates and backup → APK Installer → Legacy**",
+                "color": null,
+                "image": {
+                    "url": "https://media.discordapp.net/attachments/1015131233824538624/1059516097373683833/image.png"
+                }
+            }]
+        })
+    },
+};
