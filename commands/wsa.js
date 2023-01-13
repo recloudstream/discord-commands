8f938b44a8a5f9cc1c526410ec0e0af59a4cdf59
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-13 22:56:47 +0600

    Update wsa.js

--- commands/wsa.js
+++ commands/wsa.js
@@ -9,7 +9,7 @@ module.exports = {
           title: "WSA installation Guide For Win 10 & 11",
           description:
             "Windows Subsystem for Android™️ (WSA) enables your Windows 10 and 11 devices to run Android applications. The latest WSA version includes Android 13, Google Play Services and Magisk (to allow root access).",
-          url: "https://rentry.org/wsa",
+          url: "https://cs3docs.pages.dev/other-devices/windows/",
           color: 1083392,
           thumbnail: {
             url: "https://cdn.discordapp.com/attachments/1008654622740729897/1039955155099078726/wsa.png",
