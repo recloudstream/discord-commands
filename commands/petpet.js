cfae066155f253161c0b62c0d5b0bb07b016381f
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-01-13 17:21:42 +0100

    fix

--- commands/petpet.js
+++ commands/petpet.js
@@ -5,7 +5,7 @@ module.exports = {
 		let user = message.mentions.users.first()
 		if (user == undefined) user = message.author
 		let data = await axios.get(`https://petpet-api.clit.repl.co/petpet?url=https:%2f%2fcdn.discordapp.com%2favatars%2f${user.id}%2f${user.avatar}.png`)
-		let img = data?.result ?: `https://api.obamabot.me/v1/image/petpet?avatar=https:%2f%2fcdn.discordapp.com%2favatars%2f${user.id}%2f${user.avatar}.png`
+		let img = data?.result || `https://api.obamabot.me/v1/image/petpet?avatar=https:%2f%2fcdn.discordapp.com%2favatars%2f${user.id}%2f${user.avatar}.png`
 		message.channel.send({
 			files: [img]
 		})
