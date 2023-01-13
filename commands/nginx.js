659d321788afa64a0987066b7dabd64965841f77
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-13 22:52:03 +0600

    Update nginx.js

--- commands/nginx.js
+++ commands/nginx.js
@@ -7,7 +7,7 @@ module.exports = {
           title: "How to use nginx with cloudstream",
           description:
             "NGINX is an open source software for web serving, reverse proxying, caching, load balancing, media streaming, and more. It started out as a web server designed for maximum performance and stability.",
-          url: "https://cloudstream.cf/docs/users/use-nginx.md",
+          url: "https://cs3docs.pages.dev/integrations/nginx/",
           color: 38201,
           thumbnail: {
             url: "https://cdn.discordapp.com/attachments/1013281939026890793/1041201937900642374/nginx.png",
