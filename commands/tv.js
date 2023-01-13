fe442fc6572d3dadcb3829a86c3a4b22b9706a22
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-13 22:56:14 +0600

    Update tv.js

--- commands/tv.js
+++ commands/tv.js
@@ -8,7 +8,7 @@ module.exports = {
           title: "TV installation guide",
           description:
             "You can read this page to get an idea on how to install cs3 app and repos on TV.",
-          url: "https://discord.com/channels/737724143126052974/1055347828794339378/1055347836201488404",
+          url: "https://cs3docs.pages.dev/other-devices/tv/",
           color: 16711680,
           thumbnail: {
             url: "https://cdn-icons-png.flaticon.com/512/716/716429.png",
