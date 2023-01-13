5445cdd5763e4f89133a6c8f8d4876bd40b65ed7
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-13 22:54:58 +0600

    Update subs.js

--- commands/subs.js
+++ commands/subs.js
@@ -7,7 +7,7 @@ module.exports = {
           title: "Subtitles guide",
           description:
             "You can read this page to get an idea on how to use all the common subtitle functions in the app.",
-          url: "https://discord.com/channels/737724143126052974/1055349163396706445/1055349167939141672",
+          url: "https://cs3docs.pages.dev/settings/subtitle/",
           color: 16711680,
           thumbnail: {
             url: "https://cdn.discordapp.com/attachments/1008654622740729897/1040168844318216202/subtitles.png",
