8fabf507111ea01cf4fa812c9027752ca39543c1
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-13 22:48:59 +0600

    Update fix.js

--- commands/fix.js
+++ commands/fix.js
@@ -7,7 +7,7 @@ module.exports = {
           title: "Troubleshooting",
           description:
             "You can read this page to get the idea how to fix your posted issue.",
-          url: "https://discord.com/channels/737724143126052974/1055362005193740389/1055362007840325643",
+          url: "https://cs3docs.pages.dev/troubleshooting/",
           color: 16711680,
           thumbnail: {
             url: "https://cdn.discordapp.com/attachments/1016716040648015972/1036940396317638757/l9_1.png",
