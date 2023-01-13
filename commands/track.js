aea8914ff6e44af7e075798343878fce316879fd
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-13 22:55:23 +0600

    Update track.js

--- commands/track.js
+++ commands/track.js
@@ -7,7 +7,7 @@ module.exports = {
           title: "Anime tracking",
           description:
             "Anime tracking process works with **only anime extensions**. So, it'll not work with extensions like SuperStream, Sflix, loklok etc. Some anime extensions might also not work with the tracking process as the sources don't contain all the required metadata.",
-          url: "https://discord.com/channels/737724143126052974/1055350390935269448/1055350398241746995",
+          url: "https://cs3docs.pages.dev/integrations/animetracking/",
           color: 31487,
           thumbnail: {
             url: "https://cdn-icons-png.flaticon.com/512/3031/3031727.png",
