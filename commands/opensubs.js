0a1eb1c7b1230be6ce4b4b4b8bf8ce6183ce5a6b
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-13 22:52:43 +0600

    Update opensubs.js

--- commands/opensubs.js
+++ commands/opensubs.js
@@ -7,7 +7,7 @@ module.exports = {
           title: "Opensubtitles guide",
           description:
             "You can read this page to get an idea on how to login, use and add your own subtitles to that site.",
-          url: "https://discord.com/channels/737724143126052974/1055350005088653352/1055350008788033626",
+          url: "https://cs3docs.pages.dev/integrations/opensubtitles/",
           color: 16711680,
           thumbnail: {
             url: "https://cdn.discordapp.com/attachments/1008654622740729897/1040172066348990526/os.png",
