6b69507afd089bc4f8e2cd289d86a6816b5bbdaf
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-13 22:53:35 +0600

    Update stremio.js

--- commands/stremio.js
+++ commands/stremio.js
@@ -8,7 +8,7 @@ module.exports = {
           title: "Adding stremio addon for catalogue",
           description:
             "The Stremio extension is still experimental. Don't expect a stremio like experience. You can add sources in cs3 using this method, but it'll be only for catalogue (most of the time). So, when you will try to play the eps, it will show No link found.",
-          url: "https://discord.com/channels/737724143126052974/1055352375004635177/1055352384190169139",
+          url: "https://cs3docs.pages.dev/integrations/stemiorelated/",
           color: 7084972,
           thumbnail: {
             url: "https://cdn.discordapp.com/attachments/1008654622740729897/1040154389358329856/stremio.png",
