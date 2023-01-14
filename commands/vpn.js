a25155ff0763a0fc477a03f0bc5eb8cfc8fb20b6
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-14 23:48:01 +0600

    Update vpn.js

--- commands/vpn.js
+++ commands/vpn.js
@@ -7,7 +7,7 @@ module.exports = {
           title: "VPN related article",
           description:
             "In this page you will get the basic ideas and other FAQs.",
-          url: "https://rentry.org/askvpn",
+          url: "https://cs3docs.pages.dev/miscellaneous/#vpn",
           color: 8981425,
           thumbnail: {
             url: "https://cdn-icons-png.flaticon.com/512/188/188120.png",
