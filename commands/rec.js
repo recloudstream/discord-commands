0f044f5a15cab4f0e43f6943bb8d0dc7e4f003e1
anotherduckling <anotherduckling@users.noreply.github.com>
2023-01-18 17:37:47 +0000

    Prettified Code!

--- commands/rec.js
+++ commands/rec.js
@@ -5,23 +5,25 @@ module.exports = {
     message.channel.send({
       //files: [""],
       embeds: [
-          {
-      "title": "Recommended Sources",
-      "description": "This page covers the various sources recommended by the developers, ranked, based on their reliability, quality, speed and content availability.",
-      "url": "https://cs3docs.pages.dev/recsrc/",
-      "color": 41983,
-      "thumbnail": {
-        "url": "https://cdn-icons-png.flaticon.com/512/5562/5562017.png"
-      }
-    },
-    {
-      "title": "Recommended Apps",
-      "description": "This page covers the various apps recommended by the community, to enhance your media experience when using CS3.",
-      "url": "https://cs3docs.pages.dev/recapps/",
-      "color": 41983,
-      "thumbnail": {
-        "url": "https://cdn-icons-png.flaticon.com/512/5996/5996260.png"
-      }
+        {
+          title: "Recommended Sources",
+          description:
+            "This page covers the various sources recommended by the developers, ranked, based on their reliability, quality, speed and content availability.",
+          url: "https://cs3docs.pages.dev/recsrc/",
+          color: 41983,
+          thumbnail: {
+            url: "https://cdn-icons-png.flaticon.com/512/5562/5562017.png",
+          },
+        },
+        {
+          title: "Recommended Apps",
+          description:
+            "This page covers the various apps recommended by the community, to enhance your media experience when using CS3.",
+          url: "https://cs3docs.pages.dev/recapps/",
+          color: 41983,
+          thumbnail: {
+            url: "https://cdn-icons-png.flaticon.com/512/5996/5996260.png",
+          },
         },
       ],
     });
