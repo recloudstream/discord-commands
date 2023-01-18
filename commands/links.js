d47c43bd231f07c38c6e4f51724dc5e96d4dac9f
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-18 19:56:23 +0600

    Update links.js

--- commands/links.js
+++ commands/links.js
@@ -6,7 +6,7 @@ module.exports = {
         {
           title: "Cloudstream Related Links",
           description:
-            "<:7R:1037622920811384903>  **Site**: https://cloudstream.cf/\n\n<:puzzleplus:1041119684478644234>  **Repos**: https://cloudstream.cf/repos\n\n<:rentry:1037775510324916326> **Mirror**: https://rentry.org/cs3/\n\n<:github:1037775478234292285> **Github **: https://github.com/recloudstream/cloudstream/releases\n\n:package: **Archive **: https://github.com/recloudstream/cloudstream-archive\n\n<:gitdab:1037775447708160091> **Gitdab **: https://gitdab.com/recloudstream\n\n<:matrix:1037776230759551017> **Matrix**: https://matrix.to/#/#lagrapps:matrix.org",
+            "<:7R:1037622920811384903>  **Site**: https://cloudstream.cf/\n\n<:puzzleplus:1041119684478644234>  **Repos**: https://cloudstream.cf/repos\n\n<:base:1065267686713610270> **Docs**: https://cs3docs.pages.dev/\n\n<:github:1037775478234292285> **Github **: https://github.com/recloudstream/cloudstream/releases\n\n:package: **Archive **: https://github.com/recloudstream/cloudstream-archive\n\n<:gitdab:1037775447708160091> **Gitdab **: https://gitdab.com/recloudstream\n\n<:matrix:1037776230759551017> **Matrix**: https://matrix.to/#/#lagrapps:matrix.org",
           color: 16711680,
         },
       ],
