d65f3f89b2d08f00de25b6f06375fb0be9dc638c
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-19 17:49:12 +0600

    Update warp.js

--- commands/warp.js
+++ commands/warp.js
@@ -9,7 +9,7 @@ module.exports = {
           title: "Use warp to bypass geo restriction",
           description:
             "You can use **warp** if DOH is not bypassing the geo restriction. WARP is a hassle-free alternative which is free. This vpn is for bypassing geo restriction. not for privacy (1.1.1.1 will show your real ip to the site that has Cloudflare).\n\nIf you want a vpn that respects your privacy, you can use [**Winscribe**](https://play.google.com/store/apps/details?id=com.windscribe.vpn) (free) or [**Mullvad**](https://play.google.com/store/apps/details?id=net.mullvad.mullvadvpn) (paid).",
-          url: "https://cs3docs.pages.dev/miscellaneous/#cloudflare-warp",
+          url: "https://cs3docs.pages.dev/more-stuff/vpn/#cloudflare-warp",
           color: 16740352,
           thumbnail: {
             url: "https://play-lh.googleusercontent.com/39xbI_gFcmGEIl6JEAGhJQKv8GBjR_RFyJ6qhrFfA9CUOKZU2clSs7uo81e30zPGtg",
