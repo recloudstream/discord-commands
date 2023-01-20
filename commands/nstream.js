6ae45354a152d2b897f80ba9493c7bdc11a49acf
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-21 00:00:09 +0600

    Update nstream.js

--- commands/nstream.js
+++ commands/nstream.js
@@ -8,7 +8,7 @@ module.exports = {
         {
           title: "Network Stream",
           description:
-            "Using this feature you can stream Youtube, m3u links, other online sources that isn't present in Cloudstream.",
+            "Using this feature you can stream Youtube, m3u links, other online sources that aren't present in Cloudstream.",
           url: "https://cs3docs.pages.dev/settings/stream/",
           color: 41983,
           thumbnail: {
