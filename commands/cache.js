3d84a423afa766e7118af160047531c7713d5ea6
anotherduckling <anotherduckling@users.noreply.github.com>
2023-01-20 18:43:39 +0000

    Prettified Code!

--- commands/cache.js
+++ commands/cache.js
@@ -1,17 +1,20 @@
 module.exports = {
-    name: "marl",
-    //onlyUsers: [""],
-    async execute(message) {
-        message.channel.send({
-            //files: [""],
-            embeds: [{
-"title": "Change Cache Settings",
-      "description": "Go to **Cloudstream → Settings → Player**, change the cache settings and press **apply**.",
-      "color": null,
-      "image": {
-        "url": "https://media.discordapp.net/attachments/1044322950725259274/1066062587600584704/v6SBUt6.png"
-      }
-            }]
-        })
-    },
+  name: "marl",
+  //onlyUsers: [""],
+  async execute(message) {
+    message.channel.send({
+      //files: [""],
+      embeds: [
+        {
+          title: "Change Cache Settings",
+          description:
+            "Go to **Cloudstream → Settings → Player**, change the cache settings and press **apply**.",
+          color: null,
+          image: {
+            url: "https://media.discordapp.net/attachments/1044322950725259274/1066062587600584704/v6SBUt6.png",
+          },
+        },
+      ],
+    });
+  },
 };
