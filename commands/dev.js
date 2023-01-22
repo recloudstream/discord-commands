8cd1d461da0be0e664250621fdca169052e00698
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-01-22 11:40:21 +0100

    fix urls

--- commands/dev.js
+++ commands/dev.js
@@ -6,8 +6,8 @@ module.exports = {
         {
           title: "For extension developers",
           description:
-            "This section will outline how to start developing your own extensions for Cloudstream.\n\n**Topics:**\n🔹[Using plugin template](https://cloudstream.cf/docs/devs/using-plugin-template.md)\n🔹[Creating your own providers](https://cloudstream.cf/docs/devs/create-your-own-providers.md)\n🔹[Scraping tutorial](https://cloudstream.cf/docs/devs/scraping/index.md)\n🔹[Creating your own JSON repository](https://cloudstream.cf/docs/devs/create-your-own-json-repository.md)",
-          url: "https://cloudstream.cf/docs/devs/index.md",
+            "This section will outline how to start developing your own extensions for Cloudstream.\n\n**Topics:**\n🔹[Using plugin template](https://docs.cloudstream.cf/devs/using-plugin-template)\n🔹[Creating your own providers](https://docs.cloudstream.cf/devs/create-your-own-providers)\n🔹[Scraping tutorial](https://docs.cloudstream.cf/devs/scraping/)\n🔹[Creating your own JSON repository](https://docs.cloudstream.cf/devs/create-your-own-json-repository)",
+          url: "https://docs.cloudstream.cf/devs/",
           color: 4587714,
           thumbnail: {
             url: "https://raw.githubusercontent.com/github/explore/4479d2a2c854198cb00160f8593519c14dc3b905/topics/kotlin/kotlin.png",
