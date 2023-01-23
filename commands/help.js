247a3a046cb80c6c471bf01493adef1a9a0baf18
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-24 00:20:53 +0600

    new docs

--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,17 @@
 module.exports = {
-  name: "cast",
-  async execute(message) {
-    message.channel.send({
-      embeds: [
-        {
-          title: "Cloudstream Help",
-          description:
-            "1. [**CS3 Links**](https://discord.com/channels/737724143126052974/1055344541558919229/1055344545329598514)\n2. [**CS3 Forks**](https://discord.com/channels/737724143126052974/1055345442495397938/1055467667449987142)\n3. [**CS3 Setup**](https://discord.com/channels/737724143126052974/1055345748822204487/1055345754555818024)\n4. [**TV Setup**](https://discord.com/channels/737724143126052974/1055347828794339378/1055347836201488404)\n5. [**WSA Setup**](https://discord.com/channels/737724143126052974/1055348198320902175/1055348205430259732)\n6. [**Subtitles FAQ**](https://discord.com/channels/737724143126052974/1055349163396706445/1055349167939141672)\n7. [**Opensubtitles FAQ**](https://discord.com/channels/737724143126052974/1055350005088653352/1055350008788033626)\n8. [**Anime Tracking**](https://discord.com/channels/737724143126052974/1055350390935269448/1055350398241746995)\n9. [**Downloader FAQ**](https://discord.com/channels/737724143126052974/1055351934430740530/1055351936968302692)\n10. [**Other Settings**](https://discord.com/channels/737724143126052974/1055354229126074390/1055354317181308958)\n11. [**Related Apps**](https://discord.com/channels/737724143126052974/1055355930318995476/1055355933225652235)\n12. [**CS3 Fixes**](https://discord.com/channels/737724143126052974/1055362005193740389/1055362007840325643)\n13. [**Command List**](https://discord.com/channels/737724143126052974/1055359189465178143/1055359192363450399)",
-          color: 623359,
-          thumbnail: {
-            url: "https://media.discordapp.net/attachments/1015131233824538624/1055368886406619157/game-bloons-arcane-mastery-and-staff-pack.png",
-          },
-        },
-      ],
-    });
-  },
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            //files: [""],
+            embeds: [{
+"title": "Cloudstream Help!",
+      "description": "1. [**Setup Process**](https://docs.cloudstream.cf/)\n2. [**TV Setup**](https://docs.cloudstream.cf/other-devices/tv/)\n3. [**Windows**](https://docs.cloudstream.cf/other-devices/windows/)\n4. [**Subtitles**](https://docs.cloudstream.cf/settings/subtitle/)\n5. [**Open Subtitles**](https://docs.cloudstream.cf/integrations/opensubtitles/)\n6. [**Anime Tracking**](https://docs.cloudstream.cf/integrations/animetracking/)\n7. [**Troubleshooting**](https://docs.cloudstream.cf/troubleshooting/)\n8. [**VPN**](https://docs.cloudstream.cf/more-stuff/vpn/)\n9. [**Developer**](https://docs.cloudstream.cf/devs/gettingstarted/)\n10 [**Bot cmds**](https://discord.com/channels/737724143126052974/1055359189465178143/)",
+      "color": 16777215,
+      "thumbnail": {
+        "url": "https://media.discordapp.net/attachments/1035159969332478035/1067146476775559190/jp8pcmU.png"
+      }
+            }]
+        })
+    },
 };
