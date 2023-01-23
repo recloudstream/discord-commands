3b546f90a9baaba2ec1a0782ada84fd4d95a2af8
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-24 00:04:16 +0600

    Update dev.js

--- commands/dev.js
+++ commands/dev.js
@@ -3,15 +3,13 @@ module.exports = {
   async execute(message) {
     message.channel.send({
       embeds: [
-        {
-          title: "For extension developers",
-          description:
-            "This section will outline how to start developing your own extensions for Cloudstream.\n\n**Topics:**\n🔹[Using plugin template](https://docs.cloudstream.cf/devs/using-plugin-template)\n🔹[Creating your own providers](https://docs.cloudstream.cf/devs/create-your-own-providers)\n🔹[Scraping tutorial](https://docs.cloudstream.cf/devs/scraping/)\n🔹[Creating your own JSON repository](https://docs.cloudstream.cf/devs/create-your-own-json-repository)",
-          url: "https://docs.cloudstream.cf/devs/",
-          color: 4587714,
-          thumbnail: {
-            url: "https://raw.githubusercontent.com/github/explore/4479d2a2c854198cb00160f8593519c14dc3b905/topics/kotlin/kotlin.png",
-          },
+"title": "For extension developers",
+      "description": "You can read the documentation to know how to develop extensions for cloudstream.",
+      "url": "https://docs.cloudstream.cf/devs/gettingstarted/",
+      "color": 12058879,
+      "thumbnail": {
+        "url": "https://images-ext-1.discordapp.net/external/CwYUlAxTS2m_Z1Anvr6hB-mZlPxUZtC5HMWuHxq9iW0/https/raw.githubusercontent.com/github/explore/4479d2a2c854198cb00160f8593519c14dc3b905/topics/kotlin/kotlin.png"
+      },
         },
       ],
     });
