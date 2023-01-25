443e016c11bc93e03dee42304c9b06124494c8b4
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-26 00:17:59 +0600

    meh

--- commands/setup.js
+++ commands/setup.js
@@ -9,10 +9,9 @@ module.exports = {
         {
           title: "Cloudstream setup process",
           description:
-            "**Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease-release.apk).\n**Step 2:** Install the [repositories](https://cloudstream.cf/repos).\n\nIf you need more guides, go to <#1031508285393748008>.\n\n **Don't install all the plugins from a repo without knowing**",
+            "**Step 1:** Download and install the [pre-release](https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease-release.apk).\n**Step 2:** Install the [repositories](https://cloudstream.cf/repos).",
           color: 16711680,
-          image: {
-            url: "https://cdn.discordapp.com/attachments/1036209801803333632/1036609641636962334/unknown.png",
+          
           },
         },
       ],
