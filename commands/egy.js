928831291bc38df3e051e41a8b309bccaabc311a
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-26 21:51:31 +0600

    Update egy.js

--- commands/egy.js
+++ commands/egy.js
@@ -4,7 +4,7 @@ module.exports = {
     message.channel.send({
       embeds: [
         {
-          title: "Egybest is always broken. Use other sources.",
+          title: "Egybest is DEAD. Use other sources.",
           color: 16711680,
           image: {
             url: "https://cdn.discordapp.com/attachments/1036144578690031726/1036241544799338506/sketch-1667043229468.png",
