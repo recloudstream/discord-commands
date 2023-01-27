b1b69727b57a3350c391a6fde97f426fb568179b
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-27 11:27:14 +0600

    Microsoft has approved cs3

--- commands/store.js
+++ commands/store.js
@@ -7,7 +7,7 @@ module.exports = {
           title: "Cloudstream Beta - Microsoft Store Apps",
           description:
             "Cloudstream is a Windows app for streaming and downloading Movies, TV-Series, Asian Dramas, Anime and IPTV. The app provides all the contents without any sign-up and subscription.\n\nFeatures include:\n- 4K movies\n- Anime tracking\n- Subtitle downloads\n- Chromecast support",
-          url: "https://rentry.org/wsa",
+          url: "https://docs.cloudstream.cf/other-devices/windows/",
           image: {
             url: "https://cdn.discordapp.com/attachments/1016716040648015972/1036639726255480853/cover.png",
           },
