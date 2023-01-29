c1db74e78edd8bff5eeba7a6756f789589606edf
anotherduckling <anotherduckling@users.noreply.github.com>
2023-01-29 07:02:44 +0000

    Prettified Code!

--- commands/disabled.js
+++ commands/disabled.js
@@ -1,17 +1,20 @@
 module.exports = {
-    name: "marl",
-    //onlyUsers: [""],
-    async execute(message) {
-        message.channel.send({
-            //files: [""],
-            embeds: [{
-"title": "Disabled Extension",
-      "description": "You can't enable a disabled extension. The extension is broken and there is no ETA for the fix. So, use the recommended sources.",
-      "color": 16745728,
-      "thumbnail": {
-        "url": "https://cdn-icons-png.flaticon.com/512/7878/7878631.png"
-      }
-            }]
-        })
-    },
+  name: "marl",
+  //onlyUsers: [""],
+  async execute(message) {
+    message.channel.send({
+      //files: [""],
+      embeds: [
+        {
+          title: "Disabled Extension",
+          description:
+            "You can't enable a disabled extension. The extension is broken and there is no ETA for the fix. So, use the recommended sources.",
+          color: 16745728,
+          thumbnail: {
+            url: "https://cdn-icons-png.flaticon.com/512/7878/7878631.png",
+          },
+        },
+      ],
+    });
+  },
 };
