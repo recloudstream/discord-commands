12ed3a4a774cce86c5fd22d7a7b796398bf04c2d
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-01-29 19:10:26 +0600

    Create lib.js

---
+++ commands/lib.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            files: ["https://media.discordapp.net/attachments/1015131233824538624/1069242834789601280/8XuEuxb.png"],
+            
+        })
+    },
+};
