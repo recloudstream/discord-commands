925b6a3d0f3e7b75983a9a131728562bfc804fc4
anotherduckling <anotherduckling@users.noreply.github.com>
2023-01-31 18:04:03 +0000

    Prettified Code!

--- commands/sl.js
+++ commands/sl.js
@@ -1,14 +1,17 @@
 module.exports = {
-    name: "marl",
-    //onlyUsers: [""],
-    async execute(message) {
-        message.channel.send({
-            //files: [""],
-            embeds: [{
-"title": "Sorastream Lite",
-      "description": "Sorastream lite is bundled w/ the Sorastream extension. It's a trimmed down version of sora as the name suggests. It only contains sites with stream options. No DDL sites (UHD, FDM, MoviezAdd etc.) will be there to reduce the link loading time.",
-      "color": 13311
-            }]
-        })
-    },
+  name: "marl",
+  //onlyUsers: [""],
+  async execute(message) {
+    message.channel.send({
+      //files: [""],
+      embeds: [
+        {
+          title: "Sorastream Lite",
+          description:
+            "Sorastream lite is bundled w/ the Sorastream extension. It's a trimmed down version of sora as the name suggests. It only contains sites with stream options. No DDL sites (UHD, FDM, MoviezAdd etc.) will be there to reduce the link loading time.",
+          color: 13311,
+        },
+      ],
+    });
+  },
 };
