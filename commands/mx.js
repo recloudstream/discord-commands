9304c41ed21a1f9452bfc306652fdd8f6d0ffadd
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-01 19:33:54 +0600

    daily commit

---
+++ commands/mx.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            files: ["https://cdn.discordapp.com/attachments/1008654622740729897/1070335858961559602/laplDZU.png"],
+            
+        })
+    },
+};
