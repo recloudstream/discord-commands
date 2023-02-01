bc4a0e1f00131e2db738a5d44a34321ab909899d
anotherduckling <anotherduckling@users.noreply.github.com>
2023-02-01 13:34:14 +0000

    Prettified Code!

--- commands/mx.js
+++ commands/mx.js
@@ -1,10 +1,11 @@
 module.exports = {
-    name: "marl",
-    //onlyUsers: [""],
-    async execute(message) {
-        message.channel.send({
-            files: ["https://cdn.discordapp.com/attachments/1008654622740729897/1070335858961559602/laplDZU.png"],
-            
-        })
-    },
+  name: "marl",
+  //onlyUsers: [""],
+  async execute(message) {
+    message.channel.send({
+      files: [
+        "https://cdn.discordapp.com/attachments/1008654622740729897/1070335858961559602/laplDZU.png",
+      ],
+    });
+  },
 };
