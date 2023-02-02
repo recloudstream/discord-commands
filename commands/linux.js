f9645b1d637512f865178ed3f48ac198fc471f8e
anotherduckling <anotherduckling@users.noreply.github.com>
2023-02-02 04:20:42 +0000

    Prettified Code!

--- commands/linux.js
+++ commands/linux.js
@@ -4,13 +4,14 @@ module.exports = {
     message.channel.send({
       embeds: [
         {
-          "title": "CS3 on Linux installation guide",
-      "description": "You can read this page to get an idea on how to install cs3 app and repos on Linux.",
-      "url": "https://docs.cloudstream.cf/other-devices/linux/",
-      "color": 16748288,
-      "thumbnail": {
-        "url": "https://media.discordapp.net/attachments/1008654622740729897/1039962777403543573/waydroid_white_tb.png"
-      },
+          title: "CS3 on Linux installation guide",
+          description:
+            "You can read this page to get an idea on how to install cs3 app and repos on Linux.",
+          url: "https://docs.cloudstream.cf/other-devices/linux/",
+          color: 16748288,
+          thumbnail: {
+            url: "https://media.discordapp.net/attachments/1008654622740729897/1039962777403543573/waydroid_white_tb.png",
+          },
         },
       ],
     });
