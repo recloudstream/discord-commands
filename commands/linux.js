a5d3282aa81b98759fb1cf2fa8143d20d2b21f95
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-02 10:20:29 +0600

    Update linux.js

--- commands/linux.js
+++ commands/linux.js
@@ -4,13 +4,13 @@ module.exports = {
     message.channel.send({
       embeds: [
         {
-          title: "CS3 on Linux installation guide",
-          description:
-            "You can use [**Waydroid**](https://waydro.id/) to run Android on Linux.\n🔹[Installing Waydroid](https://docs.waydro.id/usage/install-on-desktops)\n🔹[Install and Run Android Applications](https://docs.waydro.id/usage/install-and-run-android-applications)\n🔹[Full documentation](https://docs.waydro.id/)\n🔹[Video guide](https://www.youtube.com/watch?v=FKnkIf9jb7U)",
-          color: 16745728,
-          thumbnail: {
-            url: "https://cdn.discordapp.com/attachments/1008654622740729897/1039962777403543573/waydroid_white_tb.png",
-          },
+          "title": "CS3 on Linux installation guide",
+      "description": "You can read this page to get an idea on how to install cs3 app and repos on Linux.",
+      "url": "https://docs.cloudstream.cf/other-devices/linux/",
+      "color": 16748288,
+      "thumbnail": {
+        "url": "https://media.discordapp.net/attachments/1008654622740729897/1039962777403543573/waydroid_white_tb.png"
+      },
         },
       ],
     });
