c33147719ada44177aa4d05ed84f9d24d75619df
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-04 00:27:33 +0600

    Update qn.js

--- commands/qn.js
+++ commands/qn.js
@@ -8,7 +8,7 @@ module.exports = {
         {
           title: "Quicknovel",
           description:
-            "QN is a FOSS novel reader that supports sources for LN, WN and other eBooks. It also offers external reader. It can download and convert files to epub.",
+            "QuickNovel is a FOSS novel reader that supports sources for LN, WN and other eBooks. It also offers external reader. It can download and convert files to epub.",
           url: "https://github.com/LagradOst/QuickNovel/releases",
           color: 2247143,
           thumbnail: {
