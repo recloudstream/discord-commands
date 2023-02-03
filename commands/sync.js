8471525f86440e3df3f9b674f398e425643eff4f
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-03 11:36:00 +0600

    Update sync.js

--- commands/sync.js
+++ commands/sync.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "very soon",
   async execute(message) {
     message.channel.send({
-      content: "<:copium:1070939903724109866>",
+      content: "very soon <:copium:1070939903724109866>",
     });
   },
 };
