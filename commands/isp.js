6833ea7a3b3200ff9fbd8789229cbb94c7807f09
anotherduckling <anotherduckling@users.noreply.github.com>
2023-02-04 12:39:38 +0000

    Prettified Code!

--- commands/isp.js
+++ commands/isp.js
@@ -1,17 +1,20 @@
 module.exports = {
-    name: "marl",
-    //onlyUsers: [""],
-    async execute(message) {
-        message.channel.send({
-            //files: [""],
-            embeds: [{
-"title": "Use VPN to install repos",
-      "description": "Your ISP is blocking Github raw URLs. You have to use VPN to install the repositories. You can try [these VPNs](https://docs.cloudstream.cf/recapps/#vpn).",
-      "color": 9568470,
-      "thumbnail": {
-        "url": "https://cdn-icons-png.flaticon.com/512/188/188120.png"
-      }
-            }]
-        })
-    },
+  name: "marl",
+  //onlyUsers: [""],
+  async execute(message) {
+    message.channel.send({
+      //files: [""],
+      embeds: [
+        {
+          title: "Use VPN to install repos",
+          description:
+            "Your ISP is blocking Github raw URLs. You have to use VPN to install the repositories. You can try [these VPNs](https://docs.cloudstream.cf/recapps/#vpn).",
+          color: 9568470,
+          thumbnail: {
+            url: "https://cdn-icons-png.flaticon.com/512/188/188120.png",
+          },
+        },
+      ],
+    });
+  },
 };
