e5fdcef7f15fa674b395feb05325970b897be285
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-04 17:52:23 +0600

    Sheeeeeesh!!! Now everyone will PR!!! 🙏

--- commands/list.js
+++ commands/list.js
@@ -5,11 +5,25 @@ module.exports = {
     message.channel.send({
       embeds: [
         {
-          title: "Bot Commands",
+          title: "Commands",
           description: (await getFiles("commands"))
             .map((json) => "." + json.name.replace(".js", ""))
             .join("\n"),
           color: 3407616,
+        },
+      ],
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              label: "Edit on Github",
+              url: "https://github.com/recloudstream/discord-commands/tree/master/commands",
+              style: 5,
+              emoji: "✍️",
+            },
+          ],
         },
       ],
     });
