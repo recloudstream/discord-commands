31a16d5aa2fdb02d53aaa04bd71722d9bde8ba41
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-04 13:49:49 +0600

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -5,27 +5,14 @@ module.exports = {
     message.channel.send({
       embeds: [
         {
-          title: "Commands",
+          title: "Bot Commands",
           description: (await getFiles("commands"))
             .map((json) => "." + json.name.replace(".js", ""))
             .join("\n"),
           color: 3407616,
         },
       ],
-      components: [
-        {
-          type: 1,
-          components: [
-            {
-              type: 2,
-              label: "Edit on Github",
-              url: "https://github.com/ImZaw/discord-extensions/tree/master/embeds",
-              style: 5,
-              emoji: "✍️",
-            },
-          ],
-        },
-      ],
+
     });
   },
 };
