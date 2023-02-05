03989b437f9b644c1373ec34028ac6242b2376d1
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-05 20:48:05 +0600

    Add files via upload

---
+++ commands/eng.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            //files: [""],
+            embeds: [{
+                "description": "[Click here](https://l.cloudstream.cf/dir_eng) to install English repo or use code \"Eng\".",
+                "color": 16777215
+            }]
+        })
+    },
+};
---
+++ commands/hexa.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            //files: [""],
+            embeds: [{
+                "description": "[Click here](https://l.cloudstream.cf/dir_hexa) to install Hexated repo or use code \"Hexa\".",
+                "color": 16777215
+            }]
+        })
+    },
+};
