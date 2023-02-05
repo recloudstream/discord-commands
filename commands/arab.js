5f09a52bb6543fb9519be5e418715019f4e98427
anotherduckling <anotherduckling@users.noreply.github.com>
2023-02-05 14:48:22 +0000

    Prettified Code!

--- commands/arab.js
+++ commands/arab.js
@@ -1,13 +1,16 @@
 module.exports = {
-    name: "marl",
-    //onlyUsers: [""],
-    async execute(message) {
-        message.channel.send({
-            //files: [""],
-            embeds: [{
-                "description": "[Click here](https://l.cloudstream.cf/dir_arab) to install Arabic repo or use code \"Arab\".",
-                "color": 16777215
-            }]
-        })
-    },
+  name: "marl",
+  //onlyUsers: [""],
+  async execute(message) {
+    message.channel.send({
+      //files: [""],
+      embeds: [
+        {
+          description:
+            '[Click here](https://l.cloudstream.cf/dir_arab) to install Arabic repo or use code "Arab".',
+          color: 16777215,
+        },
+      ],
+    });
+  },
 };
--- commands/eng.js
+++ commands/eng.js
@@ -1,13 +1,16 @@
 module.exports = {
-    name: "marl",
-    //onlyUsers: [""],
-    async execute(message) {
-        message.channel.send({
-            //files: [""],
-            embeds: [{
-                "description": "[Click here](https://l.cloudstream.cf/dir_eng) to install English repo or use code \"Eng\".",
-                "color": 16777215
-            }]
-        })
-    },
+  name: "marl",
+  //onlyUsers: [""],
+  async execute(message) {
+    message.channel.send({
+      //files: [""],
+      embeds: [
+        {
+          description:
+            '[Click here](https://l.cloudstream.cf/dir_eng) to install English repo or use code "Eng".',
+          color: 16777215,
+        },
+      ],
+    });
+  },
 };
