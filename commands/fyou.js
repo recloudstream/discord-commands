6801b2d69ef6600b6159a53591cbf30853eb2385
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-02-07 19:43:31 +0100

    Create fyou.js

---
+++ commands/fyou.js
@@ -0,0 +1,10 @@
+  module.exports = {
+  name: "fyou",
+  async execute(message) {
+    message.channel.send({
+      files: [
+        "https://cdn.discordapp.com/attachments/929894915754848298/952614865116229642/video0-9-1.mov",
+      ],
+    });
+  },
+};
