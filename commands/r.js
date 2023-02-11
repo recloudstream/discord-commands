7f7f67e5b4e77b5c5223e7d496575725f4cfa6d4
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-11 22:36:41 +0600

    Create r.js

---
+++ commands/r.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            //files: [""],
+            embeds: [{
+"title": "Repositories",
+      "url": "https://docs.cloudstream.cf/repositories/",
+      "color": 16777215,
+      "image": {
+        "url": "https://cdn.discordapp.com/emojis/974924816698208267.webp"
+      }
+            }]
+        })
+    },
+};
