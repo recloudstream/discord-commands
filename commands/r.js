f9245e335a2a688743832bd895abad2fd6134cda
anotherduckling <anotherduckling@users.noreply.github.com>
2023-02-11 16:37:02 +0000

    Prettified Code!

--- commands/r.js
+++ commands/r.js
@@ -1,17 +1,19 @@
 module.exports = {
-    name: "marl",
-    //onlyUsers: [""],
-    async execute(message) {
-        message.channel.send({
-            //files: [""],
-            embeds: [{
-"title": "Repositories",
-      "url": "https://docs.cloudstream.cf/repositories/",
-      "color": 16777215,
-      "image": {
-        "url": "https://cdn.discordapp.com/emojis/974924816698208267.webp"
-      }
-            }]
-        })
-    },
+  name: "marl",
+  //onlyUsers: [""],
+  async execute(message) {
+    message.channel.send({
+      //files: [""],
+      embeds: [
+        {
+          title: "Repositories",
+          url: "https://docs.cloudstream.cf/repositories/",
+          color: 16777215,
+          image: {
+            url: "https://cdn.discordapp.com/emojis/974924816698208267.webp",
+          },
+        },
+      ],
+    });
+  },
 };
