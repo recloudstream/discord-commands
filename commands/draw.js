d1f2016c0bdce74ec760d67fcb24bcef95f7bdbc
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-02-11 20:59:33 +0100

    big troll

--- commands/draw.js
+++ commands/draw.js
@@ -1,16 +1,19 @@
-const { drawImage } = require("./src/utils/drawImage.js");
+//const { drawImage } = require("./src/utils/drawImage.js");
 module.exports = {
   name: "draw",
   cooldown: 60,
   onlyChannels: ["737729263221997619"],
   async execute(message, inputs) {
-    let prompt = inputs.join(" ");
-    let resp = await drawImage(prompt);
+    /*
     message.channel.send({
       content: resp?.content,
       files: resp?.files,
     });
     this.content = resp?.content;
     this.files = resp?.files;
+    */
+    message.channel.send({
+      content: `Please wait ${Math.ceil(Math.random() * 60)}s`
+    });
   },
 };
