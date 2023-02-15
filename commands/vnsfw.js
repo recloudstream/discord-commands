c58b828a5562a9dcc271b91a22b13ae7ba8dc043
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-15 23:18:44 +0600

    Create vnsfw.js

---
+++ commands/vnsfw.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            files: ["https://cdn.discordapp.com/attachments/737729263221997619/1035548185063591986/nsfw.mp"],
+        })
+    },
+};
