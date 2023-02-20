084fed89c9e429d83aa3939b8e30915d7ea4e166
C10udburst <C10udburst@users.noreply.github.com>
2023-02-20 13:39:57 +0000

    Prettified Code!

--- commands/pressthebellicon.js
+++ commands/pressthebellicon.js
@@ -1,9 +1,11 @@
 module.exports = {
-    name: "marl",
-    //onlyUsers: [""],
-    async execute(message) {
-        message.channel.send({
-            files: ["https://cdn.discordapp.com/attachments/1015131233824538624/1077115285426937946/OIk8SLk.mp4"],
-        })
-    },
+  name: "marl",
+  //onlyUsers: [""],
+  async execute(message) {
+    message.channel.send({
+      files: [
+        "https://cdn.discordapp.com/attachments/1015131233824538624/1077115285426937946/OIk8SLk.mp4",
+      ],
+    });
+  },
 };
