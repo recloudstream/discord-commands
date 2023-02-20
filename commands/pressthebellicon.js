77fb2ea0d3d777fa05e7bdd7dbce809fbc677957
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-20 12:32:38 +0600

    Create pressthebellicon.js

---
+++ commands/pressthebellicon.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            files: ["https://cdn.discordapp.com/attachments/1015131233824538624/1077115285426937946/OIk8SLk.mp4"],
+        })
+    },
+};
