a8250e9b67b5cb4266697aea82c3b48de45033c2
anotherduckling <anotherduckling@users.noreply.github.com>
2023-02-21 08:00:28 +0000

    Prettified Code!

--- commands/skip.js
+++ commands/skip.js
@@ -1,18 +1,22 @@
 module.exports = {
-    name: "marl",
-    //onlyUsers: [""],
-    async execute(message) {
-        message.channel.send({
-            //files: [""],
-            embeds: [{
-"title": "Skip Loading",
-      "description": "While scraping links in the background, it immediately plays the best resolution currently available, bypassing the scraping process.",
-      "color": null
-    },
-    {
-      "description": "Let's say Sora uses over 30 APIs to scrape a link. If the loading is not skipped, Sora will scrape all 30+ APIs and display the best available resolution for the video. However, if you skip the loading, the program will immediately start playing the video with the best available resolution, which may not necessarily be the highest quality (e.g. it could be a 240p video depending on when you skip the loading).",
-      "color": null
-            }]
-        })
-    },
+  name: "marl",
+  //onlyUsers: [""],
+  async execute(message) {
+    message.channel.send({
+      //files: [""],
+      embeds: [
+        {
+          title: "Skip Loading",
+          description:
+            "While scraping links in the background, it immediately plays the best resolution currently available, bypassing the scraping process.",
+          color: null,
+        },
+        {
+          description:
+            "Let's say Sora uses over 30 APIs to scrape a link. If the loading is not skipped, Sora will scrape all 30+ APIs and display the best available resolution for the video. However, if you skip the loading, the program will immediately start playing the video with the best available resolution, which may not necessarily be the highest quality (e.g. it could be a 240p video depending on when you skip the loading).",
+          color: null,
+        },
+      ],
+    });
+  },
 };
