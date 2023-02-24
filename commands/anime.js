e140ff4a5210728620d91346a5738669f4c37970
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-02-24 23:22:00 +0600

    Create anime.js

---
+++ commands/anime.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "marl",
+    //onlyUsers: [""],
+    async execute(message) {
+        message.channel.send({
+            //files: [""],
+            embeds: [{
+"title": "Recommend Anime Extensions",
+      "url": "https://docs.cloudstream.cf/recsrc/#anime",
+      "color": null,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1015131233824538624/1078727091459194910/iBjiWut.png"
+      }
+            }]
+        })
+    },
+};
