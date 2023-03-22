8d92ce25e193d7404088bf2fe7543b489bc6b9c3
Duckling <57977673+anotherduckling@users.noreply.github.com>
2023-03-23 00:28:06 +0600

    Update r.js

--- commands/r.js
+++ commands/r.js
@@ -7,7 +7,7 @@ module.exports = {
       embeds: [
         {
           title: "Repositories",
-          url: "https://docs.cloudstream.cf/repositories/",
+          url: "https://rentry.org/cs3-repos",
           color: 16777215,
           image: {
             url: "https://cdn.discordapp.com/emojis/974924816698208267.webp",
